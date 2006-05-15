c652bd80f705ef7a5fea3733f25c03c21e267288
Ingo Düppe <ingo.dueppe@crowdcode.io>
2006-05-15 14:13:35 +0000

    setup openuss 3.0 trunk
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@7 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ openuss/foundation/foundation-core/src/main/java/org/openuss/foundation/lecture/FacultyServiceImpl.java
@@ -0,0 +1,35 @@
+// license-header java merge-point
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package org.openuss.foundation.lecture;
+
+/**
+ * @see org.openuss.foundation.lecture.FacultyService
+ */
+public class FacultyServiceImpl
+    extends org.openuss.foundation.lecture.FacultyServiceBase
+{
+
+    /**
+     * @see org.openuss.foundation.lecture.FacultyService#createFaculty(org.openuss.foundation.lecture.Faculty)
+     */
+    protected void handleCreateFaculty(org.openuss.foundation.lecture.Faculty faculty)
+        throws java.lang.Exception
+    {
+        // @todo implement protected void handleCreateFaculty(org.openuss.foundation.lecture.Faculty faculty)
+        throw new java.lang.UnsupportedOperationException("org.openuss.foundation.lecture.FacultyService.handleCreateFaculty(org.openuss.foundation.lecture.Faculty faculty) Not implemented!");
+    }
+
+    /**
+     * @see org.openuss.foundation.lecture.FacultyService#getAllFaculties()
+     */
+    protected java.util.List handleGetAllFaculties()
+        throws java.lang.Exception
+    {
+        // @todo implement protected java.util.List handleGetAllFaculties()
+        return null;
+    }
+
+}
