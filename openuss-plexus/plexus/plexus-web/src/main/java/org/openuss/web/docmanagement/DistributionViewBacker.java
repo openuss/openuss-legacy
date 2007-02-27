e9f222e259e3e134b891a30b662d79057be3202b
roekens <roekens>
2007-02-27 12:15:33 +0000

    added first views for distribution part of docmanagement
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/docrabbit_3_x@145 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/docmanagement/DistributionViewBacker.java
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/docmanagement/DistributionViewBacker.java
@@ -1,5 +1,20 @@
 package org.openuss.web.docmanagement;
 
+import org.apache.myfaces.custom.tree2.TreeNode;
+import org.openuss.docmanagement.DistributionService;
+
 public class DistributionViewBacker{
+	public DistributionService distributionService;
+
+	public TreeNode getTreeNode(){
+		return null;
+	}
 	
+	public DistributionService getDistributionService() {
+		return distributionService;
+	}
+
+	public void setDistributionService(DistributionService distributionService) {
+		this.distributionService = distributionService;
+	}
 }
