7c0909dc53e0eafbb7f02a88c54b2f4d9b825220
davidullrich <davidullrich>
2007-03-01 21:32:36 +0000

    implemented DefaultIOManagerImpl
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/docrabbit_3_x@152 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/webdav/DefaultIOManagerImpl.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/webdav/DefaultIOManagerImpl.java
@@ -1,37 +1,172 @@
 package org.openuss.docmanagement.webdav;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.apache.jackrabbit.webdav.DavResource;
 
+
+/**
+ * Default implementation of interface IOManager
+ * @author David Ullrich
+ * @version 0.9
+ */
 public class DefaultIOManagerImpl implements IOManager {
 
-	public void addIOHandler(IOHandler ioHandler) {
-		// TODO Auto-generated method stub
-
+	// internal list of IOHandlers
+	private final List<IOHandler> ioHandlerList;
+	
+	/**
+	 * Constructor
+	 */
+	public DefaultIOManagerImpl() {
+		ioHandlerList = new ArrayList<IOHandler>();
 	}
 
-	public boolean exportContent(ExportContext context, boolean isCollection) {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * @see org.openuss.docmanagement.webdav.IOManager#addIOHandler(IOHandler)
+	 */
+	public void addIOHandler(IOHandler ioHandler) throws IllegalArgumentException {
+		// test parameters
+		if (ioHandler == null) {
+			throw new IllegalArgumentException("The parameter ioHandler must not be null!");
+		}
+		
+		// add handler, if not in list
+		if (ioHandlerList.contains(ioHandler)) {
+			// set the IOManager-property to this instance and append it to list of handlers
+			ioHandler.setIOManager(this);
+			ioHandlerList.add(ioHandler);
+		}
 	}
 
-	public boolean exportContent(ExportContext context, DavResource resource) {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * @see org.openuss.docmanagement.webdav.IOManager#exportContent(ExportContext, boolean)
+	 */
+	public boolean exportContent(ExportContext context, boolean isCollection) throws IOException {
+		// internal variable for export status
+		boolean success = false;
+		
+		if (context != null) {
+			// get an iterator for the list of IOHandlers
+			Iterator<IOHandler> iterator = ioHandlerList.iterator();
+			IOHandler handler;
+			
+			// try to export for every handler, if not already successful
+			while (!success && iterator.hasNext()) {
+				handler = iterator.next();
+				// check if handler can handle export
+				if (handler.canExport(context, isCollection)) {
+					// export and store export status
+					success = handler.exportContent(context, isCollection);
+				}
+			}
+			
+			// export successful or list of handlers iterated -> terminate context operation
+			context.informCompleted(success);
+		}
+		
+		// return final export status
+		return success;
 	}
 
+	/**
+	 * @see org.openuss.docmanagement.webdav.IOManager#exportContent(ExportContext, DavResource)
+	 */
+	public boolean exportContent(ExportContext context, DavResource resource) throws IOException {
+		// internal variable for export status
+		boolean success = false;
+		
+		if (context != null && resource != null) {
+			// get an iterator for the list of IOHandlers
+			Iterator<IOHandler> iterator = ioHandlerList.iterator();
+			IOHandler handler;
+			
+			// try to export for every handler, if not already successful
+			while (!success && iterator.hasNext()) {
+				handler = iterator.next();
+				// check if handler can handle export
+				if (handler.canExport(context, resource)) {
+					// export and store export status
+					success = handler.exportContent(context, resource);
+				}
+			}
+			
+			// export successful or list of handlers iterated -> terminate context operation
+			context.informCompleted(success);
+		}
+		
+		// return final export status
+		return success;
+	}
+
+	/**
+	 * @see org.openuss.docmanagement.webdav.IOManager#getIOHandlers()
+	 */
 	public IOHandler[] getIOHandlers() {
-		// TODO Auto-generated method stub
-		return null;
+		// return the list of handlers as an array
+		return (IOHandler[])ioHandlerList.toArray();
 	}
 
-	public boolean importContent(ExportContext context, boolean isCollection) {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * @see org.openuss.docmanagement.webdav.IOManager#importContent(ImportContext, boolean)
+	 */
+	public boolean importContent(ImportContext context, boolean isCollection) throws IOException {
+		// internal variable for import status
+		boolean success = false;
+		
+		if (context != null) {
+			// get an iterator for the list of IOHandlers
+			Iterator<IOHandler> iterator = ioHandlerList.iterator();
+			IOHandler handler;
+			
+			// try to export for every handler, if not already successful
+			while (!success && iterator.hasNext()) {
+				handler = iterator.next();
+				// check if handler can handle import
+				if (handler.canImport(context, isCollection)) {
+					// export and store import status
+					success = handler.importContent(context, isCollection);
+				}
+			}
+			
+			// import successful or list of handlers iterated -> terminate context operation
+			context.informCompleted(success);
+		}
+		
+		// return final import status
+		return success;
 	}
 
-	public boolean importContent(ExportContext context, DavResource resource) {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * @see org.openuss.docmanagement.webdav.IOManager#importContent(ImportContext, DavResource)
+	 */
+	public boolean importContent(ImportContext context, DavResource resource) throws IOException {
+		// internal variable for import status
+		boolean success = false;
+		
+		if (context != null) {
+			// get an iterator for the list of IOHandlers
+			Iterator<IOHandler> iterator = ioHandlerList.iterator();
+			IOHandler handler;
+			
+			// try to export for every handler, if not already successful
+			while (!success && iterator.hasNext()) {
+				handler = iterator.next();
+				// check if handler can handle import
+				if (handler.canImport(context, resource)) {
+					// export and store import status
+					success = handler.importContent(context, resource);
+				}
+			}
+			
+			// import successful or list of handlers iterated -> terminate context operation
+			context.informCompleted(success);
+		}
+		
+		// return final import status
+		return success;
 	}
-
 }
--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/webdav/IOManager.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/webdav/IOManager.java
@@ -1,12 +1,57 @@
 package org.openuss.docmanagement.webdav;
 
+import java.io.IOException;
+
 import org.apache.jackrabbit.webdav.DavResource;
 
+/**
+ * 
+ * @author David Ullrich
+ * @version 0.8
+ */
 public interface IOManager {
-	public void addIOHandler(IOHandler ioHandler);
+	/**
+	 * Appends the specified IOHandler to the list of managed handlers.
+	 * @param ioHandler IOHandler to append to the list.
+	 * @throws IllegalArgumentException
+	 */
+	public void addIOHandler(IOHandler ioHandler) throws IllegalArgumentException;
+
+	/**
+	 * Returns an array containing all of the IOHandlers in the correct order.
+	 * @return an array containing all of the IOHandlers in the correct order.
+	 */
 	public IOHandler[] getIOHandlers();
-	public boolean exportContent(ExportContext context, boolean isCollection);
-	public boolean exportContent(ExportContext context, DavResource resource);
-	public boolean importContent(ExportContext context, boolean isCollection);
-	public boolean importContent(ExportContext context, DavResource resource);
+
+	/**
+	 * 
+	 * @param context
+	 * @param isCollection
+	 * @return
+	 */
+	public boolean exportContent(ExportContext context, boolean isCollection) throws IOException;
+	
+	/**
+	 * 
+	 * @param context
+	 * @param resource
+	 * @return
+	 */
+	public boolean exportContent(ExportContext context, DavResource resource) throws IOException;
+	
+	/**
+	 * 
+	 * @param context
+	 * @param isCollection
+	 * @return
+	 */
+	public boolean importContent(ImportContext context, boolean isCollection) throws IOException;
+	
+	/**
+	 * 
+	 * @param context
+	 * @param resource
+	 * @return
+	 */
+	public boolean importContent(ImportContext context, DavResource resource) throws IOException;
 }
