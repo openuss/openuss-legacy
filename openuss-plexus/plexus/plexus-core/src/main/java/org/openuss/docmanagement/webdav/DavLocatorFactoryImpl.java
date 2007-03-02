bfbdcf0158d158df990bbd6605145ab1cfee48e6
davidullrich <davidullrich>
2007-03-02 03:16:23 +0000

    abecedarian implementation
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/docrabbit_3_x@156 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/webdav/DavLocatorFactoryImpl.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/webdav/DavLocatorFactoryImpl.java
@@ -3,23 +3,33 @@ package org.openuss.docmanagement.webdav;
 import org.apache.jackrabbit.webdav.DavLocatorFactory;
 import org.apache.jackrabbit.webdav.DavResourceLocator;
 
+/**
+ * @author David Ullrich
+ * @version 0.5
+ */
 public class DavLocatorFactoryImpl implements DavLocatorFactory {
 
-	public DavResourceLocator createResourceLocator(String arg0, String arg1) {
-		// TODO Auto-generated method stub
-		return null;
+	/* (non-Javadoc)
+	 * @see org.apache.jackrabbit.webdav.DavLocatorFactory#createResourceLocator(java.lang.String, java.lang.String, java.lang.String, boolean)
+	 */
+	public DavResourceLocator createResourceLocator(String prefix, String workspacePath, String path, boolean isResourcePath) {
+		// TODO workspacePath beachten
+		return new DavResourceLocatorImpl(prefix, path, this);
 	}
 
-	public DavResourceLocator createResourceLocator(String arg0, String arg1,
-			String arg2) {
-		// TODO Auto-generated method stub
-		return null;
+	/* (non-Javadoc)
+	 * @see org.apache.jackrabbit.webdav.DavLocatorFactory#createResourceLocator(java.lang.String, java.lang.String, java.lang.String)
+	 */
+	public DavResourceLocator createResourceLocator(String prefix, String workspacePath, String resourcePath) {
+		// redirect to other method
+		return createResourceLocator(prefix, workspacePath, resourcePath, true);
 	}
 
-	public DavResourceLocator createResourceLocator(String arg0, String arg1,
-			String arg2, boolean arg3) {
-		// TODO Auto-generated method stub
+	/* (non-Javadoc)
+	 * @see org.apache.jackrabbit.webdav.DavLocatorFactory#createResourceLocator(java.lang.String, java.lang.String)
+	 */
+	public DavResourceLocator createResourceLocator(String prefix, String href) {
+		// TODO prefix aus href entfernen, um resourcePath zu ermitteln
 		return null;
 	}
-
 }
--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/webdav/DavResourceLocatorImpl.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/webdav/DavResourceLocatorImpl.java
@@ -3,56 +3,119 @@ package org.openuss.docmanagement.webdav;
 import org.apache.jackrabbit.webdav.DavLocatorFactory;
 import org.apache.jackrabbit.webdav.DavResourceLocator;
 
+/**
+ * @author David Ullrich
+ * @version 0.5
+ */
 public class DavResourceLocatorImpl implements DavResourceLocator {
+	private final String prefix;
+	private final String resourcePath;
+	private final String workspaceName;
+	private final String workspacePath;
+	private final DavLocatorFactory factory;
+	private final String href;
 
+	/**
+	 * Constructor
+	 * @param prefix
+	 * @param resourcePath
+	 * @param factory
+	 */
+	public DavResourceLocatorImpl(String prefix, String resourcePath, DavLocatorFactory factory) {
+		// TODO
+		this.prefix = prefix;
+		this.workspaceName = "";
+		this.workspacePath = "";
+		// remove trailing slash for resource pathes, but NOT for root
+		if (resourcePath.endsWith("/") && (resourcePath.length() > 1)) {
+			this.resourcePath = resourcePath.substring(0, resourcePath.length() - 1);
+		} else {
+			this.resourcePath = resourcePath;
+		}
+		this.factory = factory;
+		// TODO escape resourcePath!!!
+		href = prefix + this.resourcePath;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.apache.jackrabbit.webdav.DavResourceLocator#getFactory()
+	 */
 	public DavLocatorFactory getFactory() {
-		// TODO Auto-generated method stub
-		return null;
+		return factory;
 	}
 
-	public String getHref(boolean arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	/* (non-Javadoc)
+	 * @see org.apache.jackrabbit.webdav.DavResourceLocator#getHref(boolean)
+	 */
+	public String getHref(boolean isCollection) {
+		// append trailing slash for collections, but NOT for root
+		String suffix = "";
+		if (isCollection && !isRootLocation()) {
+			suffix = "/";
+		}
+		return href + suffix;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.apache.jackrabbit.webdav.DavResourceLocator#getPrefix()
+	 */
 	public String getPrefix() {
-		// TODO Auto-generated method stub
-		return null;
+		return prefix;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.apache.jackrabbit.webdav.DavResourceLocator#getRepositoryPath()
+	 */
 	public String getRepositoryPath() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.apache.jackrabbit.webdav.DavResourceLocator#getResourcePath()
+	 */
 	public String getResourcePath() {
-		// TODO Auto-generated method stub
-		return null;
+		return resourcePath;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.apache.jackrabbit.webdav.DavResourceLocator#getWorkspaceName()
+	 */
 	public String getWorkspaceName() {
-		// TODO Auto-generated method stub
-		return null;
+		return workspaceName;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.apache.jackrabbit.webdav.DavResourceLocator#getWorkspacePath()
+	 */
 	public String getWorkspacePath() {
-		// TODO Auto-generated method stub
-		return null;
+		return workspacePath;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.apache.jackrabbit.webdav.DavResourceLocator#isRootLocation()
+	 */
 	public boolean isRootLocation() {
-		// TODO Auto-generated method stub
-		return false;
+		return getResourcePath().equals("/");
 	}
 
-	public boolean isSameWorkspace(DavResourceLocator arg0) {
-		// TODO Auto-generated method stub
-		return false;
+	/* (non-Javadoc)
+	 * @see org.apache.jackrabbit.webdav.DavResourceLocator#isSameWorkspace(org.apache.jackrabbit.webdav.DavResourceLocator)
+	 */
+	public boolean isSameWorkspace(DavResourceLocator locator) {
+		// test parameters
+		if (locator == null) {
+			return false;
+		}
+
+		// redirect to other method
+		return isSameWorkspace(locator.getWorkspaceName());
 	}
 
-	public boolean isSameWorkspace(String arg0) {
-		// TODO Auto-generated method stub
-		return false;
+	/* (non-Javadoc)
+	 * @see org.apache.jackrabbit.webdav.DavResourceLocator#isSameWorkspace(java.lang.String)
+	 */
+	public boolean isSameWorkspace(String workspaceName) {
+		return getWorkspaceName().equals(workspaceName);
 	}
-
 }
