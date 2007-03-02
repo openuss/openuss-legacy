7cd94b12832d2e034133a02563862f379a6f937c
davidullrich <davidullrich>
2007-03-02 02:03:41 +0000

    largely implemented DefaultIOHandlerImpl
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/docrabbit_3_x@154 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/webdav/IOHandler.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/webdav/IOHandler.java
@@ -1,16 +1,94 @@
 package org.openuss.docmanagement.webdav;
 
+import java.io.IOException;
+
 import org.apache.jackrabbit.webdav.DavResource;
 
+/**
+ * 
+ * @author David Ullrich
+ * @version 0.8
+ *
+ */
 public interface IOHandler {
+	/**
+	 * 
+	 * @param context
+	 * @param isCollection
+	 * @return
+	 */
 	public boolean canExport(ExportContext context, boolean isCollection);
+	
+	/**
+	 * 
+	 * @param context
+	 * @param resource
+	 * @return
+	 */
 	public boolean canExport(ExportContext context, DavResource resource);
-	public boolean canImport(ExportContext context, boolean isCollection);
-	public boolean canImport(ExportContext context, DavResource resource);
-	public boolean exportContent(ExportContext context, boolean isCollection);
-	public boolean exportContent(ExportContext context, DavResource resource);
-	public boolean importContent(ExportContext context, boolean isCollection);
-	public boolean importContent(ExportContext context, DavResource resource);
+	
+	/**
+	 * 
+	 * @param context
+	 * @param isCollection
+	 * @return
+	 */
+	public boolean canImport(ImportContext context, boolean isCollection);
+	
+	/**
+	 * 
+	 * @param context
+	 * @param resource
+	 * @return
+	 */
+	public boolean canImport(ImportContext context, DavResource resource);
+	
+	/**
+	 * 
+	 * @param context
+	 * @param isCollection
+	 * @return
+	 * @throws IOException
+	 */
+	public boolean exportContent(ExportContext context, boolean isCollection) throws IOException;
+	
+	/**
+	 * 
+	 * @param context
+	 * @param resource
+	 * @return
+	 * @throws IOException
+	 */
+	public boolean exportContent(ExportContext context, DavResource resource) throws IOException;
+
+	/**
+	 * @param context
+	 * @param isCollection
+	 * @return
+	 * @throws IOException
+	 */
+	public boolean importContent(ImportContext context, boolean isCollection) throws IOException;
+
+	/**
+	 * @param context
+	 * @param resource
+	 * @return
+	 * @throws IOException
+	 */
+	public boolean importContent(ImportContext context, DavResource resource) throws IOException;
+
+	/**
+	 * @return
+	 */
 	public IOManager getIOManager();
+
+	/**
+	 * @param manager
+	 */
+	public void setIOManager(IOManager manager);
+
+	/**
+	 * @return
+	 */
 	public String getName();
 }
