c3ce9b4ef39729082e5b15b091ea6d953dbc8601
davidullrich <davidullrich>
2007-03-02 02:22:37 +0000

    very simple implementation of DefaultItemFilterImpl
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/docrabbit_3_x@155 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/webdav/DefaultItemFilterImpl.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/webdav/DefaultItemFilterImpl.java
@@ -1,12 +1,31 @@
 package org.openuss.docmanagement.webdav;
 
 import javax.jcr.Item;
+import javax.jcr.RepositoryException;
 
+/**
+ * @author David Ullrich
+ * @version 0.5
+ */
 public class DefaultItemFilterImpl implements ItemFilter {
-
+	/* (non-Javadoc)
+	 * @see org.openuss.docmanagement.webdav.ItemFilter#isFilteredItem(javax.jcr.Item)
+	 */
 	public boolean isFilteredItem(Item item) {
-		// TODO Auto-generated method stub
+		// test parameter
+		if (item == null) {
+			return true;
+		}
+		
+		try {
+			String name = item.getName();
+			if (name.startsWith("jcr:") || name.startsWith("rep:")) {
+				return true;
+			}
+		} catch (RepositoryException ex) {
+			// ignore
+		}
+
 		return false;
 	}
-
 }
