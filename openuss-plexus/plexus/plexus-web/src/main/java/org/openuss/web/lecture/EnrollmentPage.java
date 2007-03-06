6e7c6eba6ab0e778f05c9b34caf524d244d81f72
roekens <roekens>
2007-03-06 18:51:41 +0000

    added first drafts for enrollment-main view, enrollment-aspirants-view and stubs for enrollment-participants view. added 2 tables to model for enrollmentmembers and aspirants
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@178 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/EnrollmentPage.java
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/EnrollmentPage.java
@@ -1,12 +1,18 @@
 package org.openuss.web.lecture;
 
+import java.util.ArrayList;
+
 import org.apache.shale.tiger.managed.Bean;
 import org.apache.shale.tiger.managed.Property;
 import org.apache.shale.tiger.managed.Scope;
 import org.apache.shale.tiger.view.Prerender;
 import org.apache.shale.tiger.view.View;
+import org.openuss.lecture.AccessType;
 import org.openuss.lecture.Enrollment;
+import org.openuss.lecture.Faculty;
 import org.openuss.lecture.LectureException;
+import org.openuss.lecture.LectureService;
+import org.openuss.lecture.Subject;
 import org.openuss.web.Constants;
 
 /**
@@ -24,17 +30,42 @@ public class EnrollmentPage extends AbstractLecturePage {
 
 	@Property(value = "#{param.enrollmentId}")
 	protected Long enrollmentId;
+	
 
 	@Override
 	@Prerender
 	public void prerender() throws LectureException {
+		Subject subject = Subject.Factory.newInstance();
+		subject.setName("TestSubject");
+		subject.setShortcut("TESTSHORT");
+		subject.setDescription("TestDescription");
+		
+		enrollment = Enrollment.Factory.newInstance();
+		enrollment.setAccessType(AccessType.OPEN);
+		enrollment.setDocuments(true);
+		enrollment.setDiscussion(true);
+		enrollment.setMailinglist(true);
+		enrollment.setSubject(subject);
+		enrollment.setShortcut("SHORTCUT");
+		enrollment.setDescription("description");
+		enrollment.setId(new Long(42347));
+		enrollmentId = enrollment.getId();
+		
+		Faculty faculty = lectureService.getFaculty(new Long(8441));
+		enrollment.setFaculty(faculty);
+		
+		ArrayList al = new ArrayList();
+		al.add(enrollment);
+		subject.setEnrollments(al);
+		
+		/*
 		if (enrollmentId != null) {
 			// fetch enrollment by id
 			enrollment = lectureService.getEnrollment(enrollmentId);
 		} else if (enrollment != null) {
 			// refresh session enrollment object
 			enrollment = lectureService.getEnrollment(enrollment.getId());
-		}
+		}*/
 		if (enrollment == null) {
 			addMessage(i18n("message_error_enrollment_not_exists"));
 			redirect(Constants.DESKTOP); // back to where he came from
