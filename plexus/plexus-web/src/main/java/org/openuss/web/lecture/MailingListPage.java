d0d4d95150b3c866a3082ba61e0fceeab6ba8e9c
roekens <roekens>
2007-03-07 22:26:25 +0000

    added views and beans for enrollment mailing
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@188 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/lecture/MailingListPage.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/lecture/MailingListPage.java
@@ -1,11 +1,7 @@
 package org.openuss.web.lecture;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.faces.event.ValueChangeEvent;
 
 import org.apache.log4j.Logger;
 import org.apache.shale.tiger.managed.Bean;
@@ -13,44 +9,83 @@ import org.apache.shale.tiger.managed.Scope;
 import org.apache.shale.tiger.view.View;
 import org.openuss.framework.web.jsf.model.AbstractPagedTable;
 import org.openuss.framework.web.jsf.model.DataPage;
-import org.openuss.security.User;
-import org.openuss.security.UserInfo;
+import org.openuss.mail.MailingJob;
+import org.openuss.mail.MailingStatus;
+
 import org.openuss.web.Constants;
 
 @Bean(name = "views$secured$enrollment$mailinglist", scope = Scope.REQUEST)
 @View
 public class MailingListPage extends AbstractEnrollmentPage {
-	private static final Logger logger = Logger.getLogger(EnrollmentAspirantsPage.class);
+	private static final Logger logger = Logger.getLogger(MailingListPage.class);
 	
 	private MailingListDataProvider data = new MailingListDataProvider();
 	
 	public String delete() {
+		logger.debug("MailingList delete Item");
 		return Constants.SUCCESS;
 	}
 
 	public String send() {
+		logger.debug("MailingList send Item");
 		return Constants.SUCCESS;
 	}
 
-	public String changeStatus(){
+	public String change(){
+		logger.debug("MailingList change Item");
 		return Constants.SUCCESS;
+	}
+	
+	public String changeStatus(){
+		logger.debug("MailingList status change");
+		return Constants.SUCCESS;
+		
 	}
 		
-	private class MailingListDataProvider extends AbstractPagedTable<UserInfo> {
+	private class MailingListDataProvider extends AbstractPagedTable<MailingJob> {
 
-		private DataPage<UserInfo> page; 
+		private DataPage<MailingJob> page; 
 		
 		@Override 
-		public DataPage<UserInfo> getDataPage(int startRow, int pageSize) {
-			ArrayList<UserInfo> al = new ArrayList<UserInfo>();			
+		public DataPage<MailingJob> getDataPage(int startRow, int pageSize) {
+			ArrayList<MailingJob> al = new ArrayList<MailingJob>();			
+			MailingJob mj1 = MailingJob.Factory.newInstance();
+			MailingJob mj2 = MailingJob.Factory.newInstance();
+			MailingJob mj3 = MailingJob.Factory.newInstance();
+			MailingJob mj4 = MailingJob.Factory.newInstance();
 			
-			UserInfo ui1 = new UserInfo(new Long(1234), "cag", "Sebastian", "Roekens", "abc123", "plexus@openuss-plexus.com", true, false, false, new Date(System.currentTimeMillis()));
-			UserInfo ui2 = new UserInfo(new Long(12345), "dueppe", "Ingo", "Düppe", "12345", "plexus@openuss-plexus.com", true, true, false, new Date(System.currentTimeMillis()));
-			UserInfo ui3 = new UserInfo(new Long(1111), "bundy", "Al", "Bundy", "dumpfbacke", "plexus@openuss-plexus.com", true, false, true, new Date(System.currentTimeMillis()));
-			al.add(ui1); al.add(ui2); al.add(ui3);
-			page = new DataPage<UserInfo>(al.size(),0,al);
+			mj1.setJobId(new Long(1));
+			mj1.setMailTitle("Lösungen zu Fallstudien");
+			mj1.setSendingTime(new Timestamp(System.currentTimeMillis()));
+			mj1.setStatus(MailingStatus.DRAFT);
+			
+			mj2.setJobId(new Long(2));
+			mj2.setMailTitle("RWC Seminar");
+			mj2.setSendingTime(new Timestamp(System.currentTimeMillis()));
+			mj2.setStatus(MailingStatus.INQUEUE);
+
+			mj3.setJobId(new Long(3));
+			mj3.setMailTitle("SAP Workshop");
+			mj3.setSendingTime(new Timestamp(System.currentTimeMillis()));
+			mj3.setStatus(MailingStatus.PLANNED);
+
+			mj4.setJobId(new Long(4));
+			mj4.setMailTitle("OpenUSS");
+			mj4.setSendingTime(new Timestamp(System.currentTimeMillis()));
+			mj4.setStatus(MailingStatus.SEND);
+
+			al.add(mj1); al.add(mj2); al.add(mj3); al.add(mj4);
+			page = new DataPage<MailingJob>(al.size(),0,al);
 			return page;
 		}
+	}
+
+	public MailingListDataProvider getData() {
+		return data;
+	}
+
+	public void setData(MailingListDataProvider data) {
+		this.data = data;
 	}
 	
 
