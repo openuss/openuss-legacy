ea1d67e710a2b618d75a2f58e262a3078d869341
roekens <roekens>
2007-03-08 14:42:16 +0000

    added accessrecorder package to store information which user viewed which objects
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@200 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/accessrecorder/AccessRecorderServiceImpl.java
@@ -0,0 +1,25 @@
+// OpenUSS - Open Source University Support System
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package org.openuss.accessrecorder;
+
+/**
+ * @see org.openuss.accessrecorder.AccessRecorderService
+ */
+public class AccessRecorderServiceImpl
+    extends org.openuss.accessrecorder.AccessRecorderServiceBase
+{
+
+    /**
+     * @see org.openuss.accessrecorder.AccessRecorderService#hasViewed(java.lang.Long, org.openuss.security.User, org.openuss.lecture.Enrollment)
+     */
+    protected boolean handleHasViewed(java.lang.Long objectId, org.openuss.security.User user, org.openuss.lecture.Enrollment enrollment)
+        throws java.lang.Exception
+    {
+        // @todo implement protected boolean handleHasViewed(java.lang.Long objectId, org.openuss.security.User user, org.openuss.lecture.Enrollment enrollment)
+        return false;
+    }
+
+}
