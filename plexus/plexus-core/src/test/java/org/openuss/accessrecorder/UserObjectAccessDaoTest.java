ebf89d6970e60185216d5992d4d0f43a18c407f0
roekens <roekens>
2007-03-08 14:42:16 +0000

    added accessrecorder package to store information which user viewed which objects
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@200 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-core/src/test/java/org/openuss/accessrecorder/UserObjectAccessDaoTest.java
@@ -0,0 +1,23 @@
+// OpenUSS - Open Source University Support System
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package org.openuss.accessrecorder;
+
+
+/**
+ * JUnit Test for Spring Hibernate UserObjectAccessDao class.
+ * @see org.openuss.accessrecorder.UserObjectAccessDao
+ */
+public class UserObjectAccessDaoTest extends UserObjectAccessDaoTestBase {
+	
+	public void testUserObjectAccessDaoCreate() {
+		/*UserObjectAccess userObjectAccess = new UserObjectAccessImpl();
+		userObjectAccess.setObjectId(" ");
+		userObjectAccess.setViewed(" ");
+		assertNull(userObjectAccess.getId());
+		userObjectAccessDao.create(userObjectAccess);
+		assertNotNull(userObjectAccess.getId());*/
+	}
+}
