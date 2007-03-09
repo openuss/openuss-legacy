a423179f04bc6a482ac407c5ff72c13b4fa0c387
davidullrich <davidullrich>
2007-03-09 14:13:27 +0000

    implemented authentication against OpenUSS
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/docrabbit_3_x@212 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/webdav/SessionProvider.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/webdav/SessionProvider.java
@@ -1,5 +1,8 @@
 package org.openuss.docmanagement.webdav;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import javax.jcr.Credentials;
 import javax.jcr.LoginException;
 import javax.jcr.NoSuchWorkspaceException;
@@ -7,9 +10,20 @@ import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.SimpleCredentials;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.acegisecurity.Authentication;
+import org.acegisecurity.AuthenticationException;
+import org.acegisecurity.AuthenticationManager;
+import org.acegisecurity.BadCredentialsException;
+import org.acegisecurity.DisabledException;
+import org.acegisecurity.LockedException;
+import org.acegisecurity.providers.UsernamePasswordAuthenticationToken;
+import org.acegisecurity.ui.WebAuthenticationDetails;
+import org.apache.jackrabbit.util.Base64;
+import org.apache.jackrabbit.webdav.DavConstants;
 import org.apache.jackrabbit.webdav.DavException;
 import org.apache.jackrabbit.webdav.DavServletResponse;
 import org.apache.log4j.Logger;
@@ -24,6 +38,7 @@ public class SessionProvider {
 	private final String REPOSITORY_USERNAME = "username";
 	private final String REPOSITORY_PASSWORD = "password";
 	private Repository repository;
+	private AuthenticationManager authenticationManager;
 
 	/**
 	 * @param request
@@ -34,14 +49,20 @@ public class SessionProvider {
 	public boolean attachSession(HttpServletRequest request, DavService davService) throws DavException {
 		try {
 			// retrieve user authentication information from request
-			Credentials userCredentials = getCredentials(request);
+			SimpleCredentials userCredentials = getCredentials(request);
 			if (userCredentials == null) {
 				// user authentication missing
 				throw new DavException(HttpServletResponse.SC_UNAUTHORIZED);
 			}
+			logger.debug("Authentication requested for user '" + userCredentials.getUserID() + "'");
 
 			// try to authenticate user to system
-			// TODO Benutzer gegen OpenUSS authentifizieren
+			Authentication authentication;
+			UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userCredentials.getUserID(), new String(userCredentials.getPassword()));
+			authentication = getAuthenticationManager().authenticate(authenticationToken);
+			logger.debug("Authentication successful: " + (authentication != null));
+
+			// TODO authentication object verwenden
 
 			// establish session with repository
 			Credentials repositoryCredentials = new SimpleCredentials(REPOSITORY_USERNAME, REPOSITORY_PASSWORD.toCharArray());
@@ -58,12 +79,18 @@ public class SessionProvider {
 			
 			// Session successfully attached
 			return true;
-		} catch (NoSuchWorkspaceException ex) {
-			// workspace not found
-			logger.debug("NoSuchWorkspace exception occurred.");
+		} catch (AuthenticationException ex) {
+			// authentication failed
+			logger.debug("Authentication exception occurred.");
 			logger.debug("Exception: " + ex.getMessage());
-			// rethrow exception as DavException
-			throw new DavException(DavServletResponse.SC_NOT_FOUND, ex.getMessage());
+			// rethrow exception as DavException -> request username password
+			throw new DavException(DavServletResponse.SC_UNAUTHORIZED, ex.getMessage());
+		} catch (ServletException ex) {
+			// invalid or not supported credentials
+			logger.debug("Servlet exception occurred.");
+			logger.debug("Exception: " + ex.getMessage());
+			// rethrow exception as DavException -> bad request
+			throw new DavException(DavServletResponse.SC_BAD_REQUEST, ex.getMessage());
 		} catch (LoginException ex) {
 			// login to repository failed -> service unavailable
 			logger.debug("Login exception occurred.");
@@ -107,18 +134,48 @@ public class SessionProvider {
 	 * @param request
 	 * @return
 	 */
-	private Credentials getCredentials(HttpServletRequest request) {
-		// TODO credentials aus Request extrahieren
-		return new SimpleCredentials("username", "geheim".toCharArray());
+	private SimpleCredentials getCredentials(HttpServletRequest request) throws ServletException {
+		// get value of authorization header field
+		String authorizationHeader = request.getHeader("Authorization");
+		
+		if (authorizationHeader != null) {
+			// field was set -> decode
+			String[] headerFields = authorizationHeader.split(" ");
+			// authorization header has to be like 'Basic credentials'
+			if ((headerFields.length >= 2) && headerFields[0].equalsIgnoreCase(HttpServletRequest.BASIC_AUTH)) {
+				// credentials are base64 encoded
+				ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+				try {
+					// decode credentials
+					Base64.decode(headerFields[1], outputStream);
+					String decodedCredentials = outputStream.toString("ISO-8859-1");
+					// decoded credentials should be like 'username:password'
+					int position = decodedCredentials.indexOf(":");
+					// minimum length for username and password is 1
+					if ((position < 1) || (position >= (decodedCredentials.length() - 1))) {
+						throw new ServletException("Missing username or password.");
+					}
+					// strip username and password from decoded credentials
+					String username = decodedCredentials.substring(0, position);
+					String password = decodedCredentials.substring(position + 1);
+					// create new instance of simple credentials and return it
+					return new SimpleCredentials(username, password.toCharArray());
+				} catch (IOException ex) {
+					// rethrow exception as servlet exception
+					throw new ServletException(ex.getMessage());
+				}
+			}
+			// invalid format of authorization header
+			throw new ServletException("Invalid or not supported value of authorization header.");
+		}
+		// no authorization information found in request
+		return null;
 	}
 	
 	/**
 	 * @return
 	 */
 	public Repository getRepository() {
-		if (repository == null) {
-			
-		}
 		return repository;
 	}
 	
@@ -127,5 +184,19 @@ public class SessionProvider {
 	 */
 	public void setRepository(Repository repository) {
 		this.repository = repository;
+	}
+
+	/**
+	 * @return the authenticationManager
+	 */
+	public AuthenticationManager getAuthenticationManager() {
+		return authenticationManager;
+	}
+
+	/**
+	 * @param authenticationManager the authenticationManager to set
+	 */
+	public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
 	}
 }
