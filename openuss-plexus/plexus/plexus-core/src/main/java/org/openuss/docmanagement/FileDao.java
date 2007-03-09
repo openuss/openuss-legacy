3adc32a21ed711d37a20ce08baf02f882dc375c0
davidullrich <davidullrich>
2007-03-09 03:32:54 +0000

    removed compilation errors
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/docrabbit_3_x@210 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/FileDao.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/FileDao.java
@@ -1,5 +1,7 @@
 package org.openuss.docmanagement;
 
+import org.apache.jackrabbit.webdav.io.OutputContext;
+
 /**
  * @author David Ullrich
  * @version 0.5
@@ -12,5 +14,11 @@ public class FileDao extends ResourceDao {
 	@Override
 	public boolean isCollection() {
 		return false;
+	}
+
+	@Override
+	public void spool(OutputContext context) {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/FolderDao.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/docmanagement/FolderDao.java
@@ -1,10 +1,18 @@
 package org.openuss.docmanagement;
 
+import org.apache.jackrabbit.webdav.io.OutputContext;
+
 /**
  * @author David Ullrich
  * @version 0.5
  */
 public class FolderDao extends ResourceDao {
+
+	@Override
+	public void spool(OutputContext context) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	/* (non-Javadoc)
 	 * @see org.openuss.docmanagement.ResourceDao#isCollection()
