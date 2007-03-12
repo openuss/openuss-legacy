d13b3d143a3cf4ad32e76f5e90c5b745650d8334
roekens <roekens>
2007-03-12 14:12:06 +0000

    added document views, backing beans, small update of RepositoryFile class
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@234 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/documents/DocumentNewPage.java
@@ -0,0 +1,35 @@
+package org.openuss.web.documents;
+
+import org.apache.log4j.Logger;
+import org.apache.shale.tiger.managed.Bean;
+import org.apache.shale.tiger.managed.Property;
+import org.apache.shale.tiger.managed.Scope;
+import org.apache.shale.tiger.view.View;
+import org.openuss.repository.RepositoryFile;
+import org.openuss.web.Constants;
+
+@Bean(name = "views$secured$documents$documentnew", scope = Scope.REQUEST)
+@View
+public class DocumentNewPage{
+	private static final Logger logger = Logger.getLogger(DocumentNewPage.class);
+	
+	@Property(value = "#{newDocument}")
+	private RepositoryFile newDocument;
+
+
+	public String save(){
+		logger.debug("new document saved");
+		return Constants.SUCCESS;
+	}
+
+
+	public RepositoryFile getNewDocument() {
+		return newDocument;
+	}
+
+
+	public void setNewDocument(RepositoryFile newDocument) {
+		this.newDocument = newDocument;
+	}
+	
+}
---
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/documents/DocumentTableEntry.java
@@ -0,0 +1,92 @@
+package org.openuss.web.documents;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+public class DocumentTableEntry implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3053386731692416783L;
+
+	public String filename;
+	
+	public String mimeType;
+	
+	public String size;
+	
+	public Date lastChange;
+	
+	public Date created;
+	
+	public Boolean viewed;
+	
+	public Boolean marked;
+	
+	public DocumentTableEntry(String filename, String mimeType, String size, Date lastChange, Date created, boolean viewed){
+		this.filename=filename;
+		this.mimeType=mimeType;
+		this.size=size;
+		this.lastChange=lastChange;
+		this.created=created;
+		this.viewed=viewed;		
+	}
+	
+	public Date getCreated() {	
+		return created;
+	}
+	
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	
+	public String getFilename() {
+		return filename;
+	}
+	
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	
+	public Date getLastChange() {
+		return lastChange;
+	}
+	
+	public void setLastChange(Date lastChange) {
+		this.lastChange = lastChange;
+	}
+	
+	public String getMimeType() {
+		return mimeType;
+	}
+	
+	public void setMimeType(String mimeType) {
+		this.mimeType = mimeType;
+	}
+	
+	public String getSize() {
+		return size;
+	}
+	
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public boolean isViewed() {
+		return viewed;
+	}
+
+	public void setViewed(boolean viewed) {
+		this.viewed = viewed;
+	}
+
+	public boolean isMarked() {
+		return marked;
+	}
+
+	public void setMarked(boolean marked) {
+		this.marked = marked;
+	}
+}
