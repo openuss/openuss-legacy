6b055e2a1de56d11af1640582ef6659bb7971d77
roekens <roekens>
2007-03-14 15:50:57 +0000

    added model, views and backing beans for wiki component
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@265 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-core/src/test/java/org/openuss/wiki/WikiServiceIntegrationTest.java
@@ -0,0 +1,14 @@
+// OpenUSS - Open Source University Support System
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package org.openuss.wiki;
+
+/**
+ * JUnit Test for Spring Hibernate WikiService class.
+ * @see org.openuss.wiki.WikiService
+ */
+public class WikiServiceIntegrationTest extends WikiServiceIntegrationTestBase {
+	
+}
