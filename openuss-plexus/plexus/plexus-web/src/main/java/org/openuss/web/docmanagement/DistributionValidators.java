921dd7933d560c52afa3989c7c14ca9793ffb7db
roekens <roekens>
2007-03-22 01:11:43 +0000

    added validator for file and foldernames
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/docrabbit_3_x@365 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/docmanagement/DistributionValidators.java
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/docmanagement/DistributionValidators.java
@@ -2,15 +2,36 @@ package org.openuss.web.docmanagement;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
+import org.apache.shale.tiger.register.FacesValidator;
 
-public class DistributionValidators{
-	//FIXME implement me	
-	public void checkUser(FacesContext context, UIComponent component, Object value) throws ValidatorException {
-		  // you are free to call this method at your choice, it has to be conform with your validator attribute
-		  // queue the message in this method, make sure to call component.setValid(false)
-		//FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, summary, details);
-		//getFacesContext().addMessage(clientId, msg);
+
+/**
+ * Validator checks wether the resourceName is valid
+ */
+@FacesValidator(value="checkResourceNameValidator")
+public class DistributionValidators extends ExceptionHandler implements Validator {
+	public static final String allowed ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789äöüßÄÖÜ-_.";	
+	
+	public boolean checkResource(String resource){		
+		if (resource==null) return false;
+		if (resource.length()==0) return false;
+		if (resource.endsWith(".")) return false;
+		for (int i = 0; i < resource.length(); i++){
+			if (allowed.indexOf(resource.charAt(i))==-1) return false;
+			if (resource.charAt(i)=='.') if (resource.charAt(i+1)=='.') return false;
+		}
+		return true;	
+	}
+	
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		String resourceName = (String) value;
+		if (!checkResource(resourceName))((UIInput)component).setValid(false);
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Resource name not valid!" ,null);
+		context.addMessage(null, msg);
+
 	}
 }
