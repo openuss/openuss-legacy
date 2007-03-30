7007607dff1b0c576e6f33a45806fe7ac6b4cac6
roekens <roekens>
2007-03-30 17:06:16 +0000

    added remove method to service, added methods to DomainViewStateDao
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@516 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-core/src/main/java/org/openuss/viewtracking/DomainViewStateDaoImpl.java
@@ -0,0 +1,31 @@
+// OpenUSS - Open Source University Support System
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package org.openuss.viewtracking;
+/**
+ * @see org.openuss.viewtracking.DomainViewState
+ */
+public class DomainViewStateDaoImpl
+    extends org.openuss.viewtracking.DomainViewStateDaoBase
+{
+    /**
+     * @see org.openuss.viewtracking.DomainViewStateDao#updateAllToModified(java.lang.Long)
+     */
+    protected void handleUpdateAllToModified(java.lang.Long domainIdentifier)
+    {
+        // @todo implement public void handleUpdateAllToModified(java.lang.Long domainIdentifier)
+        throw new java.lang.UnsupportedOperationException("org.openuss.viewtracking.DomainViewState.handleUpdateAllToModified(java.lang.Long domainIdentifier) Not implemented!");
+}
+
+    /**
+     * @see org.openuss.viewtracking.DomainViewStateDao#removeAllByDomain(java.lang.Long)
+     */
+    protected void handleRemoveAllByDomain(java.lang.Long domainIdentifier)
+    {
+        // @todo implement public void handleRemoveAllByDomain(java.lang.Long domainIdentifier)
+        throw new java.lang.UnsupportedOperationException("org.openuss.viewtracking.DomainViewState.handleRemoveAllByDomain(java.lang.Long domainIdentifier) Not implemented!");
+}
+
+}
