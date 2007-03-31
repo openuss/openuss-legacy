9d51698d5dee06cda37c15a212fdb4926eca0eff
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-03-31 08:50:50 +0000

    resolved viewtracking
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@526 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/viewtracking/DomainViewStateDaoImpl.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/viewtracking/DomainViewStateDaoImpl.java
@@ -4,28 +4,44 @@
  * You can (and have to!) safely modify it by hand.
  */
 package org.openuss.viewtracking;
+
+import org.hibernate.HibernateException;
+
 /**
  * @see org.openuss.viewtracking.DomainViewState
  */
-public class DomainViewStateDaoImpl
-    extends org.openuss.viewtracking.DomainViewStateDaoBase
-{
-    /**
-     * @see org.openuss.viewtracking.DomainViewStateDao#updateAllToModified(java.lang.Long)
-     */
-    protected void handleUpdateAllToModified(java.lang.Long domainIdentifier)
-    {
-        // @todo implement public void handleUpdateAllToModified(java.lang.Long domainIdentifier)
-        throw new java.lang.UnsupportedOperationException("org.openuss.viewtracking.DomainViewState.handleUpdateAllToModified(java.lang.Long domainIdentifier) Not implemented!");
-}
+public class DomainViewStateDaoImpl extends DomainViewStateDaoBase {
+	/**
+	 * @see org.openuss.viewtracking.DomainViewStateDao#updateAllToModified(java.lang.Long)
+	 */
+	protected void handleUpdateAllToModified(final Long domainIdentifier) {
+		final String hqlUpdate = "update DomainViewStateImpl as v set v.viewState = 2 where v.domainViewStatePk.domainIdentifier = :domainIdentifier";
+		getHibernateTemplate().execute(new org.springframework.orm.hibernate3.HibernateCallback() {
+			public Object doInHibernate(org.hibernate.Session session) throws HibernateException {
+				int count = session.createQuery(hqlUpdate)
+				.setLong("domainIdentifier", domainIdentifier)
+				.executeUpdate();
+				logger.debug("updated "+count+" records.");
+				return count;
+			}
+		}, true);
+	}
 
-    /**
-     * @see org.openuss.viewtracking.DomainViewStateDao#removeAllByDomain(java.lang.Long)
-     */
-    protected void handleRemoveAllByDomain(java.lang.Long domainIdentifier)
-    {
-        // @todo implement public void handleRemoveAllByDomain(java.lang.Long domainIdentifier)
-        throw new java.lang.UnsupportedOperationException("org.openuss.viewtracking.DomainViewState.handleRemoveAllByDomain(java.lang.Long domainIdentifier) Not implemented!");
-}
+	/**
+	 * @see org.openuss.viewtracking.DomainViewStateDao#removeAllByDomain(java.lang.Long)
+	 */
+	protected void handleRemoveAllByDomain(final Long domainIdentifier) {
+		final String hqlDelete = "delete DomainViewStateImpl v where v.domainViewStatePk.domainIdentifier = :domainIdentifier";
+		getHibernateTemplate().execute(new org.springframework.orm.hibernate3.HibernateCallback() {
+			public Object doInHibernate(org.hibernate.Session session) throws HibernateException {
+				int count = session.createQuery(hqlDelete)
+					.setLong("domainIdentifier", domainIdentifier)
+					.executeUpdate();
+				
+				logger.debug("deleted "+count+" records.");
+				return count;
+			}
+		}, true);
 
+	}
 }
--- openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/viewtracking/DomainViewStateDaoTest.java
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/viewtracking/DomainViewStateDaoTest.java
@@ -28,20 +28,71 @@ public class DomainViewStateDaoTest extends DomainViewStateDaoTestBase {
 	public void testViewStateDaoCreate() {
 		User user = testUtility.createSecureContext();
 		Long id = testUtility.unique();
-		DomainViewState domainViewState = new DomainViewStateImpl();
-		DomainViewStatePK pk = new DomainViewStatePK();
-		pk.setDomainIdentifier(id);
-		pk.setUserIdentifier(user.getId());
-		domainViewState.setDomainViewStatePk(pk);
-		domainViewState.setViewState(ViewState.NEW);
+		DomainViewState domainViewState = createDomainViewState(id, user.getId(), ViewState.NEW);
+		
 		assertEquals(id, domainViewState.getDomainViewStatePk().getDomainIdentifier());
 		domainViewStateDao.create(domainViewState);
 		assertEquals(id, domainViewState.getDomainViewStatePk().getDomainIdentifier());
 		assertEquals(user.getId(), domainViewState.getDomainViewStatePk().getUserIdentifier());
 		assertEquals(ViewState.NEW, domainViewState.getViewState());
 		commit();
-		domainViewState = domainViewStateDao.load(pk);
+		domainViewState = domainViewStateDao.load(domainViewState.getDomainViewStatePk());
 		assertEquals(id, domainViewState.getDomainViewStatePk().getDomainIdentifier());
+		
+	}
+	
+	public void testUpdateAllAndRemoveBy() {
+		Long domainId1 = testUtility.unique();
+		Long domainId2 = testUtility.unique();
+		Long user1 = testUtility.unique();
+		Long user2 = testUtility.unique();
+		
+		DomainViewState viewState1 = createDomainViewState(domainId1, user1, ViewState.NEW);
+		DomainViewState viewState2 = createDomainViewState(domainId1, user2, ViewState.NEW);
+		DomainViewState viewState3 = createDomainViewState(domainId2, user2, ViewState.NEW);
+		
+		domainViewStateDao.create(viewState1);
+		domainViewStateDao.create(viewState2);
+		domainViewStateDao.create(viewState3);
+		
+		commit();
+		
+		domainViewStateDao.updateAllToModified(domainId1);
+		
+		commit();
+		
+		DomainViewState loaded1 = domainViewStateDao.load(createPK(domainId1,user1));
+		assertEquals(ViewState.MODIFIED, loaded1.getViewState());
+		DomainViewState loaded2 = domainViewStateDao.load(createPK(domainId1,user2));
+		assertEquals(ViewState.MODIFIED, loaded2.getViewState());
+		DomainViewState loaded3 = domainViewStateDao.load(createPK(domainId2,user2));
+		assertEquals(ViewState.NEW, loaded3.getViewState());
+		
+		domainViewStateDao.removeAllByDomain(domainId1);
+		
+		commit();
+		
+		assertNull(domainViewStateDao.load(createPK(domainId1,user1)));
+		assertNull(domainViewStateDao.load(createPK(domainId1,user2)));
+		assertNotNull(domainViewStateDao.load(createPK(domainId2,user2)));
+		
+		domainViewStateDao.removeAllByDomain(domainId2);
+	}
+	
+	private DomainViewState createDomainViewState(Long domainId, Long userId, ViewState state) {
+		DomainViewStatePK pk = createPK(domainId, userId);
+		
+		DomainViewState viewstate = DomainViewState.Factory.newInstance();
+		viewstate.setDomainViewStatePk(pk);
+		viewstate.setViewState(state);
+		return viewstate;
+	}
+
+	private DomainViewStatePK createPK(Long domainId, Long userId) {
+		DomainViewStatePK pk = new DomainViewStatePK();
+		pk.setDomainIdentifier(domainId);
+		pk.setUserIdentifier(userId);
+		return pk;
 	}
 
 	private void commit() {
