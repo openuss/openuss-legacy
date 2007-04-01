50044548ac21b5b57f42046faf1356a6496edb40
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-04-01 11:22:41 +0000

    refactoring
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@537 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/upload/UploadFileManager.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/upload/UploadFileManager.java
@@ -1,7 +1,9 @@
 package org.openuss.web.upload;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
 import org.apache.commons.lang.Validate;
 import org.apache.log4j.Logger;
@@ -9,6 +11,7 @@ import org.apache.shale.tiger.managed.Bean;
 import org.apache.shale.tiger.managed.Scope;
 import org.apache.shale.tiger.view.Destroy;
 import org.apache.shale.tiger.view.View;
+import org.openuss.documents.FileInfo;
 import org.openuss.framework.web.jsf.controller.BaseBean;
 import org.openuss.web.Constants;
 
@@ -25,10 +28,13 @@ public class UploadFileManager extends BaseBean{
 
 	private static final long serialVersionUID = -842372090394813149L;
 	
-	private List<UploadedDocument> files;
+	private Stack<UploadedDocument> files;
+	
+	private Map<FileInfo, UploadedDocument> fileInfo2Documents;
 	
 	public UploadFileManager() {
-		files = new ArrayList<UploadedDocument>();
+		files = new Stack<UploadedDocument>();
+		fileInfo2Documents = new HashMap<FileInfo, UploadedDocument>();
 	}
 	
 	@Destroy
@@ -48,7 +54,7 @@ public class UploadFileManager extends BaseBean{
 		Validate.notNull(document, "Parameter document must not be null!");
 		
 		logger.debug("Adding document "+document.getFileName()+" to upload manager");
-		files.add(document);
+		files.push(document);
 	}
 	
 	/**
@@ -60,6 +66,52 @@ public class UploadFileManager extends BaseBean{
 
 		files.remove(document);
 		document.remove();
+	}
+	
+	public void remove(FileInfo fileInfo) {
+		Validate.notNull(fileInfo, "Parameter fileInfo must not be null!");
+		UploadedDocument documents = fileInfo2Documents.get(fileInfo);
+		if (documents != null) {
+			fileInfo2Documents.remove(fileInfo);
+			registerDocument(documents);
+		}
+	}
+	
+	public UploadedDocument getLastUploaded() {
+		if (files.isEmpty()) {
+			return null;
+		} else {
+			return files.peek();
+		}
+	}
+	
+	public FileInfo lastUploadAsFileInfo() throws IOException {
+		if (files.isEmpty()) {
+			return null;
+		}
+		UploadedDocument document = files.peek();
+		if (document != null) {
+			FileInfo fileInfo = new FileInfo();
+			fileInfo.setFileName(document.getFileName());
+			fileInfo.setExtension(extension(document.getFileName()));
+
+			fileInfo.setContentType(document.getContentType());
+			fileInfo.setFileSize(document.getFileSize());
+			fileInfo.setInputStream(document.getInputStream());
+			fileInfo2Documents.put(fileInfo, document);
+			return fileInfo;
+		} else {
+			return null;
+		}
+	}
+	
+	private String extension(String fileName) {
+		if (fileName != null) {
+			int index = fileName.lastIndexOf('.');
+			return fileName.substring(index+1);
+		} else {
+			return "";
+		}
 	}
 	
 }
