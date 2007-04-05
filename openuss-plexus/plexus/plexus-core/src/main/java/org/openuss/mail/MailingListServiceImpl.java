8cf7ff27353361b7a3e8eda26a803304142980e1
roekens <roekens>
2007-04-05 21:03:30 +0000

    updated model for mail package: - added mailinglist services and entities - added valueobject for mails in db
    added service stubs
    
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@595 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/mail/MailingListServiceImpl.java
@@ -0,0 +1,55 @@
+// OpenUSS - Open Source University Support System
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package org.openuss.mail;
+
+/**
+ * @see org.openuss.mail.MailingListService
+ */
+public class MailingListServiceImpl
+    extends org.openuss.mail.MailingListServiceBase
+{
+
+    /**
+     * @see org.openuss.mail.MailingListService#getMailingList(java.lang.Long)
+     */
+    protected org.openuss.mail.MailingListInfo handleGetMailingList(java.lang.Long domainIdentitfier)
+        throws java.lang.Exception
+    {
+        // @todo implement protected org.openuss.mail.MailingListInfo handleGetMailingList(java.lang.Long domainIdentitfier)
+        return null;
+    }
+
+    /**
+     * @see org.openuss.mail.MailingListService#addUserToMailingList(org.openuss.security.UserInfo, org.openuss.mail.MailingListInfo)
+     */
+    protected void handleAddUserToMailingList(org.openuss.security.UserInfo user, org.openuss.mail.MailingListInfo mailingList)
+        throws java.lang.Exception
+    {
+        // @todo implement protected void handleAddUserToMailingList(org.openuss.security.UserInfo user, org.openuss.mail.MailingListInfo mailingList)
+        throw new java.lang.UnsupportedOperationException("org.openuss.mail.MailingListService.handleAddUserToMailingList(org.openuss.security.UserInfo user, org.openuss.mail.MailingListInfo mailingList) Not implemented!");
+    }
+
+    /**
+     * @see org.openuss.mail.MailingListService#removeUserFromMailingList(org.openuss.security.UserInfo, org.openuss.mail.MailingListInfo)
+     */
+    protected void handleRemoveUserFromMailingList(org.openuss.security.UserInfo user, org.openuss.mail.MailingListInfo mailingList)
+        throws java.lang.Exception
+    {
+        // @todo implement protected void handleRemoveUserFromMailingList(org.openuss.security.UserInfo user, org.openuss.mail.MailingListInfo mailingList)
+        throw new java.lang.UnsupportedOperationException("org.openuss.mail.MailingListService.handleRemoveUserFromMailingList(org.openuss.security.UserInfo user, org.openuss.mail.MailingListInfo mailingList) Not implemented!");
+    }
+
+    /**
+     * @see org.openuss.mail.MailingListService#sendMail(org.openuss.mail.MailInfo, org.openuss.mail.MailingListInfo)
+     */
+    protected void handleSendMail(org.openuss.mail.MailInfo mail, org.openuss.mail.MailingListInfo mailingList)
+        throws java.lang.Exception
+    {
+        // @todo implement protected void handleSendMail(org.openuss.mail.MailInfo mail, org.openuss.mail.MailingListInfo mailingList)
+        throw new java.lang.UnsupportedOperationException("org.openuss.mail.MailingListService.handleSendMail(org.openuss.mail.MailInfo mail, org.openuss.mail.MailingListInfo mailingList) Not implemented!");
+    }
+
+}
