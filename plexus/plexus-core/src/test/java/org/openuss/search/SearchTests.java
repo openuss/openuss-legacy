6fd3d3bd5b8bb9795df094d3230884aff72d93f6
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-04-09 17:44:16 +0000

    some first search tests
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@614 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-core/src/test/java/org/openuss/search/SearchTests.java
@@ -0,0 +1,17 @@
+package org.openuss.search;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class SearchTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for org.openuss.system");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(SearchEngineTest.class);
+		suite.addTestSuite(SearchEngineTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
