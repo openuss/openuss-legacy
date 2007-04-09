4156bdf598705189272832dc91d19f1ad5dc2a95
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-04-09 17:41:57 +0000

    add mailinglistserviceintegrationtest
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@610 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/mail/MailTests.java
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/mail/MailTests.java
@@ -9,6 +9,7 @@ public class MailTests {
 		TestSuite suite = new TestSuite("Test for org.openuss.system");
 		//$JUnit-BEGIN$
 		suite.addTestSuite(MailingJobDaoTest.class);
+		suite.addTestSuite(MailingListServiceIntegrationTest.class);
 		suite.addTestSuite(MailServiceIntegrationTest.class);
 		suite.addTestSuite(MailToSendDaoTest.class);
 		suite.addTestSuite(MailMessageDaoTest.class);
