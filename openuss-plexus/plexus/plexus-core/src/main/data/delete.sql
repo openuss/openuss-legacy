b5f434c00d091195558a3efd6b3a295208f6a9cc
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-04-09 17:01:26 +0000

    improved sql delete script
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@605 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/main/data/delete.sql
+++ openuss-plexus/plexus/plexus-core/src/main/data/delete.sql
@@ -1,32 +1,54 @@
+/* BrainContest */
 delete from braincontest_answer;
 delete from braincontest;
-
+/* Desktop */
+delete from desktop_desktop_enrollment;
 delete from desktop_desktop_subject;
 delete from desktop_desktop_faculty;
 delete from desktop_desktop;
+/* Discussion */
+delete from discussion_formula;
+delete from discussion_forumwatch;
+delete from discussion_topicwatch;
+update discussion_topic  set FIRST_FK = null, LAST_FK = null;
+delete from discussion_post;
+delete from discussion_topic;
+delete from discussion_forum;
+/* Documents */
+update documents_folderentry set PARENT_FK = null;
+delete from documents_fileentry;
+delete from documents_folder;
+delete from documents_folderentry;
+/* Repository */
+delete from repository_file;
+/* News */
 delete from news_newsitem;
-delete from lecture_faculty_aspirant;
-delete from lecture_faculty_group;
-delete from lecture_faculty_member;
-
+/* Enrollment */
+delete from enrollment_member;
+/* Mailinglist */
+delete from mailinglist;
+delete from mailinglist_recipients;
+delete from mailing_job;
+delete from mail_to_send;
+delete from mail_templatemodel;
+delete from mail_template;
+delete from mail_body;
+/* Lecture */
 update lecture_enrollment e set e.faculty_fk = null;
 update lecture_period e set e.faculty_fk = null;
 update lecture_subject e set e.faculty_fk = null;
-
-update discussion_post d set d.submitter_fk = null;
-update discussion_post d set d.topic_fk = null;
-delete from discussion_topic;
-
-delete from enrollment_member;
+delete from lecture_faculty_aspirant;
+delete from lecture_faculty_group;
+delete from lecture_faculty_member;
 delete from lecture_faculty;
 delete from lecture_enrollment;
 delete from lecture_period;
 delete from lecture_subject;
+/* Security */
+update security_object_identity set parent_fk = null;
 delete from security_group2authority;
 delete from security_group;
 delete from security_permission;
-
-update security_object_identity set parent_fk = null;
 delete from security_object_identity;
 delete from security_activationcode;
 delete from security_user;
@@ -34,4 +56,5 @@ delete from security_user_contact;
 delete from security_user_preferences;
 delete from security_user_profile;
 delete from security_authority;
+/* system */
 delete from system_property;
