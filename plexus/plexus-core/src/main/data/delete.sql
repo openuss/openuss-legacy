2d3a300b91136a30d84f982582f6d1c2d7ab802b
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-04-12 14:20:13 +0000

    bugfix
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@635 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-core/src/main/data/delete.sql
+++ plexus/plexus-core/src/main/data/delete.sql
@@ -1,12 +1,9 @@
-/* BrainContest */
 delete from braincontest_answer;
 delete from braincontest;
-/* Desktop */
 delete from desktop_desktop_enrollment;
 delete from desktop_desktop_subject;
 delete from desktop_desktop_faculty;
 delete from desktop_desktop;
-/* Discussion */
 delete from discussion_formula;
 delete from discussion_forumwatch;
 delete from discussion_topicwatch;
@@ -14,26 +11,20 @@ update discussion_topic  set FIRST_FK = null, LAST_FK = null;
 delete from discussion_post;
 delete from discussion_topic;
 delete from discussion_forum;
-/* Documents */
 update documents_folderentry set PARENT_FK = null;
 delete from documents_fileentry;
 delete from documents_folder;
 delete from documents_folderentry;
-/* Repository */
 delete from repository_file;
-/* News */
 delete from news_newsitem;
-/* Enrollment */
 delete from enrollment_member;
-/* Mailinglist */
-delete from mailinglist;
-delete from mailinglist_recipients;
-delete from mailing_job;
-delete from mail_to_send;
-delete from mail_templatemodel;
-delete from mail_template;
-delete from mail_body;
-/* Lecture */
+delete from mailinglist_subcriber;
+delete from mailinglist_mail;
+delete from mailinglist_mailinglist;
+delete from message_templateparameter;
+delete from message_templatemessage;
+delete from message_recipient;
+delete from message_message;
 update lecture_enrollment e set e.faculty_fk = null;
 update lecture_period e set e.faculty_fk = null;
 update lecture_subject e set e.faculty_fk = null;
@@ -44,7 +35,6 @@ delete from lecture_faculty;
 delete from lecture_enrollment;
 delete from lecture_period;
 delete from lecture_subject;
-/* Security */
 update security_object_identity set parent_fk = null;
 delete from security_group2authority;
 delete from security_group;
@@ -56,5 +46,4 @@ delete from security_user_contact;
 delete from security_user_preferences;
 delete from security_user_profile;
 delete from security_authority;
-/* system */
 delete from system_property;
