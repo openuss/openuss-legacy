e0d77788a499ae6213f71f2c6ec6e9e858806733
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-04-26 07:04:28 +0000

    add sql import.sql script for hibernate
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@729 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-core/src/main/resources/import.sql
@@ -0,0 +1,19 @@
+insert into SECURITY_AUTHORITY (ID) values (-1);
+insert into SECURITY_AUTHORITY (ID) values (-2);
+insert into SECURITY_AUTHORITY (ID) values (-3);
+insert into SECURITY_AUTHORITY (ID) values (-4);
+insert into SECURITY_GROUP (ID, NAME, LABEL, GROUP_TYPE) values (-1, 'ROLE_ANONYMOUS', 'ANONYMOUS', 1);
+insert into SECURITY_GROUP (ID, NAME, LABEL, GROUP_TYPE) values (-2, 'ROLE_USER', 'USER', 1);
+insert into SECURITY_GROUP (ID, NAME, LABEL, GROUP_TYPE) values (-3, 'ROLE_ASSISTANT', 'ASSISTANT', 1);
+insert into SECURITY_GROUP (ID, NAME, LABEL, GROUP_TYPE) values (-4, 'ROLE_ADMIN', 'ADMINISTRATOR', 1);
+
+insert into SECURITY_OBJECT_IDENTITY (ID, PARENT_FK) VALUES (0, null);
+insert into SECURITY_PERMISSION (ID, MASK, ACL_OBJECT_IDENTITY_FK, RECIPIENT_FK) values (1, 32767, 0, -4);
+
+insert into SYSTEM_PROPERTY (ID, PROP_NAME, PROP_VALUE) values (0, 'openuss.url', 'localhost:8080');
+insert into SYSTEM_PROPERTY (ID, PROP_NAME, PROP_VALUE) values (1, 'repository.path', 'd:/temp');
+insert into SYSTEM_PROPERTY (ID, PROP_NAME, PROP_VALUE) values (2, 'mail.from.address', 'plexus@openuss-plexus.org');
+insert into SYSTEM_PROPERTY (ID, PROP_NAME, PROP_VALUE) values (3, 'mail.from.name', 'plexus@openuss-plexus.org');
+insert into SYSTEM_PROPERTY (ID, PROP_NAME, PROP_VALUE) values (4, 'mail.host.name', 'localhost');
+insert into SYSTEM_PROPERTY (ID, PROP_NAME, PROP_VALUE) values (5, 'mail.host.user', 'plexus@openuss-plexus.org');
+insert into SYSTEM_PROPERTY (ID, PROP_NAME, PROP_VALUE) values (6, 'mail.host.password', 'plexus');
