60a0047df5fb1e7aaddcb6752cdff792b996b6d9
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-05-05 09:35:35 +0000

    code polishing
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@755 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-core/src/main/java/org/openuss/commands/LastProcessedCommandImpl.java
+++ plexus/plexus-core/src/main/java/org/openuss/commands/LastProcessedCommandImpl.java
@@ -8,13 +8,10 @@ package org.openuss.commands;
 /**
  * @see org.openuss.commands.LastProcessedCommand
  */
-public class LastProcessedCommandImpl
-    extends org.openuss.commands.LastProcessedCommandBase
-	implements org.openuss.commands.LastProcessedCommand
-{
-    /**
-     * The serial version UID of this class. Needed for serialization.
-     */
-    private static final long serialVersionUID = -5969753665225915140L;
+public class LastProcessedCommandImpl extends LastProcessedCommandBase implements LastProcessedCommand {
+	/**
+	 * The serial version UID of this class. Needed for serialization.
+	 */
+	private static final long serialVersionUID = -5969753665225915140L;
 
 }
