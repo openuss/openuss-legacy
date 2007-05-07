b15f93658550d42773d62b4bbf3c418102843f19
roekens <roekens>
2007-05-07 14:21:12 +0000

    updated MailingListServiceIntegrationTest: added tests for subscribing and unsubscribing and bugfixed MailingListService and SubscriberDao
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@767 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/mailinglist/MailingListServiceIntegrationTest.java
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/mailinglist/MailingListServiceIntegrationTest.java
@@ -5,7 +5,13 @@
  */
 package org.openuss.mailinglist;
 
+import java.util.List;
+
+import org.openuss.TestUtility;
+import org.openuss.foundation.DefaultDomainObject;
+import org.openuss.foundation.DomainObject;
 import org.openuss.security.SecurityService;
+import org.openuss.security.User;
 
 /**
  * JUnit Test for Spring Hibernate MailingListService class.
@@ -15,10 +21,53 @@ public class MailingListServiceIntegrationTest extends MailingListServiceIntegra
 	
 	public SecurityService securityService;
 	
+	public TestUtility testUtilty;
 	
-	public void testMailingListAddingAndRemoving(){
+	public void testMailingListSubscription(){
+		DomainObject domainObject = generateDomainObject();
+		User user = testUtility.createDefaultUserInDB();
+		commit();
+		//init list of subscribers has to be empty
+		List subscribers = getMailingListService().getSubscribers(domainObject);
+		assertEquals(0, subscribers.size());
+		//add user to mailinglist
+		getMailingListService().subscribe(domainObject, user);
+		subscribers = getMailingListService().getSubscribers(domainObject);
+		assertEquals(1, subscribers.size());
+		SubscriberInfo si = (SubscriberInfo) subscribers.get(0);
+		assertEquals(false, si.isBlocked());
+		assertEquals(user.getDisplayName(), si.getDisplayName());
+		assertEquals(user.getEmail(), si.getEmail());
+		//delete user from mailingList using first unsubscribe method
+		getMailingListService().unsubscribe(si);
+		subscribers = getMailingListService().getSubscribers(domainObject);
+		assertEquals(0, subscribers.size());
+		//add user to mailingList
+		getMailingListService().subscribe(domainObject, user);
+		subscribers = getMailingListService().getSubscribers(domainObject);
+		assertEquals(1, subscribers.size());
+		si = (SubscriberInfo) subscribers.get(0);
+		assertEquals(false, si.isBlocked());
+		assertEquals(user.getDisplayName(), si.getDisplayName());
+		assertEquals(user.getEmail(), si.getEmail());
+		//delete user from mailingList using second unsubscribe method
+		getMailingListService().unsubscribe(domainObject, user);
+		subscribers = getMailingListService().getSubscribers(domainObject);
+		assertEquals(0, subscribers.size());
+		//test unsubscribe methods if user not in mailinglist
+		getMailingListService().unsubscribe(domainObject, user);
+		getMailingListService().unsubscribe(si);
+		subscribers = getMailingListService().getSubscribers(domainObject);
+		assertEquals(0, subscribers.size());
+
 	}
 
+	private DomainObject generateDomainObject(){
+		DomainObject domainObject = new DefaultDomainObject(testUtility.unique());
+		securityService.createObjectIdentity(domainObject, null);
+		//securityService.setPermissions(user, domainId, LectureAclEntry.ASSIST);		
+		return domainObject;
+	}
 
 	public SecurityService getSecurityService() {
 		return securityService;
@@ -27,5 +76,13 @@ public class MailingListServiceIntegrationTest extends MailingListServiceIntegra
 
 	public void setSecurityService(SecurityService securityService) {
 		this.securityService = securityService;
+	}
+
+	public TestUtility getTestUtilty() {
+		return testUtilty;
+	}
+
+	public void setTestUtilty(TestUtility testUtilty) {
+		this.testUtilty = testUtilty;
 	}
 }
