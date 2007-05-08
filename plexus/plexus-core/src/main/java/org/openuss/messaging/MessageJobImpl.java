be014288dd7a9e29894fec4be1f982636d8ea501
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-05-08 16:41:05 +0000

    improved messaging service
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@785 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-core/src/main/java/org/openuss/messaging/MessageJobImpl.java
+++ plexus/plexus-core/src/main/java/org/openuss/messaging/MessageJobImpl.java
@@ -5,6 +5,12 @@
  */
 package org.openuss.messaging;
 
+import java.util.Collection;
+import java.util.Date;
+
+import org.apache.commons.lang.Validate;
+import org.openuss.security.User;
+
 /**
  * @author Ingo Dueppe
  * @see org.openuss.messaging.MessageJob
@@ -14,6 +20,11 @@ public class MessageJobImpl extends MessageJobBase implements MessageJob {
 	 * The serial version UID of this class. Needed for serialization.
 	 */
 	private static final long serialVersionUID = 7356518302537926968L;
+	
+	
+	public MessageJobImpl() {
+		setCreated(new Date());
+	}
 
 	/**
 	 * @see org.openuss.messaging.MessageJob#getToSend()
@@ -22,6 +33,19 @@ public class MessageJobImpl extends MessageJobBase implements MessageJob {
 		int send = 0;
 		// TODO everything
 		return send;
+	}
+
+	public void addRecipient(User user) {
+		Validate.notNull(user, "Parameter recipient must not be null.");
+		Recipient recipient = Recipient.Factory.newInstance();
+		recipient.setUser(user);
+		getRecipients().add(recipient);
+		
+	}
+
+	public void addRecipients(Collection users) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
