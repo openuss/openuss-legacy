f6a1f8ae83d393c34e57a76c8d056147fdd91cf3
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-05-08 16:41:05 +0000

    improved messaging service
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@785 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/messaging/MessageServiceImpl.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/messaging/MessageServiceImpl.java
@@ -8,6 +8,9 @@ package org.openuss.messaging;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.Validate;
+import org.openuss.security.User;
+
 /**
  * @see org.openuss.messaging.MessageService
  */
@@ -17,7 +20,9 @@ public class MessageServiceImpl extends MessageServiceBase {
 	 * @see org.openuss.messaging.MessageService#sendMessage(java.lang.String,
 	 *      java.lang.String, boolean, java.util.List)
 	 */
-	protected Long handleSendMessage(String subject, String text, boolean sms, List recipients) throws Exception {
+	protected Long handleSendMessage(String sender, String subject, String text, boolean sms, List recipients) throws Exception {
+		Validate.notEmpty(subject, "Parameter subject must not be empty");
+		validateRecipients(recipients);
 		// @todo implement protected org.openuss.messaging.MessageJob
 		// handleSendMessage(java.lang.String subject, java.lang.String text,
 		// boolean sms, java.util.List recipients)
@@ -28,12 +33,24 @@ public class MessageServiceImpl extends MessageServiceBase {
 	 * @see org.openuss.messaging.MessageService#sendMessage(java.lang.String,
 	 *      java.lang.String, java.util.Map, java.util.List)
 	 */
-	protected Long handleSendMessage(String subject, String templateName, Map parameters, List recipients)
-			throws Exception {
-		// @todo implement protected org.openuss.messaging.MessageJob
-		// handleSendMessage(java.lang.String subject, java.lang.String
-		// templateName, java.util.Map parameters, java.util.List recipients)
+	protected Long handleSendMessage(String sender, String subject, String templateName, Map parameters, List recipients) throws Exception {
+		Validate.notEmpty(subject, "Parameter subject must not be empty");
+		Validate.notEmpty(templateName,"Parameter templateName must not be empty");
+		validateRecipients(recipients);
+		
+		MessageJob job = MessageJob.Factory.newInstance();
+		job.setSendAsSms(false);
+		job.addRecipients(recipients);
+		
+		Message message = TemplateMessage.Factory.newInstance();
+		message.setSenderName(sender);
+		
 		return null;
+	}
+
+	private void validateRecipients(List recipients) {
+		Validate.notNull(recipients,"Parameter recipient must not be empty");
+		Validate.allElementsOfType(recipients, org.openuss.security.User.class,"Parameter recipient must only contain User objects.");
 	}
 
 	@Override
