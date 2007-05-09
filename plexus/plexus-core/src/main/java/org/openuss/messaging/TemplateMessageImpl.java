2d7d3f91028c3c509764e5cf0f1839028ca89a11
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-05-09 11:04:36 +0000

    in progress: messaging
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@792 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-core/src/main/java/org/openuss/messaging/TemplateMessageImpl.java
+++ plexus/plexus-core/src/main/java/org/openuss/messaging/TemplateMessageImpl.java
@@ -5,10 +5,14 @@
  */
 package org.openuss.messaging;
 
+import java.util.Map;
+import java.util.Set;
+
 import org.apache.commons.lang.Validate;
 
 /**
  * @see org.openuss.messaging.TemplateMessage
+ * @author Ingo Dueppe
  */
 public class TemplateMessageImpl extends TemplateMessageBase implements TemplateMessage {
 	/**
@@ -21,9 +25,11 @@ public class TemplateMessageImpl extends TemplateMessageBase implements Template
 	 *      java.lang.String)
 	 */
 	public void addParameter(String name, String value) {
+		Validate.notEmpty(name, "Parameter name must not be null");
+		Validate.notEmpty(value, "Parameter value must not be null");
 		TemplateParameter parameter = TemplateParameter.Factory.newInstance();
-		parameter.setModelName(name);
-		parameter.setModelValue(value);
+		parameter.setName(name);
+		parameter.setValue(value);
 		addParameter(parameter);
 	}
 
@@ -42,6 +48,17 @@ public class TemplateMessageImpl extends TemplateMessageBase implements Template
 	public void removeParameter(TemplateParameter parameter) {
 		if (parameter != null) {
 			getParameters().remove(parameter);
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void addParameters(Map parameters) {
+		Validate.notNull(parameters, "Parameter parameters must not be null");
+		Validate.allElementsOfType(parameters.keySet(), String.class, "Parameter parameters must contain Map<String,String>");
+		for(Map.Entry<String, String> entry : (Set<Map.Entry<String,String>>)parameters.entrySet()) {
+			addParameter(entry.getKey(),entry.getValue());
 		}
 	}
 
