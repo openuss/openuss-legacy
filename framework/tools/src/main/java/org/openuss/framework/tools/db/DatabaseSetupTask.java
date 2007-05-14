c39b47d02b3d1e6c85ea8a39787b8188a2e797bc
ronhaus <ronhaus>
2007-05-14 10:01:23 +0000

    Updated the database creator ant task
    - the db openuss30 is longer created automatically
    - the ant task expects properties now
    
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@829 60046477-0c10-0410-bf4b-e552a4ff1768

--- framework/tools/src/main/java/org/openuss/framework/tools/db/DatabaseSetupTask.java
+++ framework/tools/src/main/java/org/openuss/framework/tools/db/DatabaseSetupTask.java
@@ -5,67 +5,96 @@ import org.apache.tools.ant.Task;
 import org.firebirdsql.management.FBManager;
 
 /**
+ * 	Dropes and creates a Firebird database. 
  * 
+ * @version 1.0
  * @author Ron Haus
  */
 public class DatabaseSetupTask extends Task {
 
-	public void execute() throws BuildException {
+	private String DB_SERVER_URL = "";
 
-		String DB_SERVER_URL = "localhost";
-		int DB_SERVER_PORT = 3050;
-		String DB_NAME1 = "openuss30";
-		String DB_NAME2 = "openuss30-test";
-		String DB_USER = "sysdba";
-		String DB_PASSWORD = "masterkey";
+	private int DB_SERVER_PORT = 0;
+
+	private String DB_NAME = "";
+
+	private String DB_USER = "";
+
+	private String DB_PASSWORD = "";
+
+	public void setDB_NAME(String db_name) {
+		DB_NAME = db_name;
+	}
+
+	public void setDB_PASSWORD(String db_password) {
+		DB_PASSWORD = db_password;
+	}
+
+	public void setDB_SERVER_PORT(int db_server_port) {
+		DB_SERVER_PORT = db_server_port;
+	}
+
+	public void setDB_SERVER_URL(String db_server_url) {
+		DB_SERVER_URL = db_server_url;
+	}
+
+	public void setDB_USER(String db_user) {
+		DB_USER = db_user;
+	}
+
+	public void execute() throws BuildException {
+		
+		if (DB_SERVER_URL == "") {
+			throw new BuildException("ERROR: The property DB_SERVER_URL is undefined.");
+		}
+		if (DB_SERVER_PORT == 0) {
+			throw new BuildException("ERROR: The property DB_SERVER_PORT is undefined.");
+		}
+		if (DB_NAME == "") {
+			throw new BuildException("ERROR: The property DB_NAME is undefined.");
+		}
+		if (DB_USER == "") {
+			throw new BuildException("ERROR: The property DB_USER is undefined.");
+		}
+		if (DB_PASSWORD == "") {
+			throw new BuildException("ERROR: The property DB_PASSWORD is undefined.");
+		}
 		
 		FBManager fbManager = new FBManager();
 		fbManager.setForceCreate(true);
 		fbManager.setServer(DB_SERVER_URL);
 		fbManager.setPort(DB_SERVER_PORT);
-		
+
 		try {
-			System.out.println("INFO: Starting Firebird Database Manager...");
+			log("INFO: Starting Firebird Database Manager...");
 			fbManager.start();
-			System.out.println("INFO: Firebird Database Manager started");
+			log("INFO: Firebird Database Manager started");
 			try {
-				System.out.println("INFO: Droping database "+DB_NAME1+"...");
-				fbManager.dropDatabase(DB_NAME1, DB_USER, DB_PASSWORD);
-				System.out.println("INFO: Database dropped");
-			} catch(Exception e) {
-				System.out.println("INFO: Database "+DB_NAME1+" couldn't be dropped. May have not existed.");
+				log("INFO: Droping database " + DB_NAME + "...");
+				fbManager.dropDatabase(DB_NAME, DB_USER, DB_PASSWORD);
+				log("INFO: Database dropped");
+			} catch (Exception e) {
+				log("INFO: Database " + DB_NAME + " couldn't be dropped. May have not existed.");
 			}
 			try {
-				System.out.println("INFO: Droping database "+DB_NAME2+"...");
-				fbManager.dropDatabase(DB_NAME2, DB_USER, DB_PASSWORD);
-				System.out.println("INFO: Database dropped");
-			} catch(Exception e) {
-				System.out.println("INFO: Database "+DB_NAME2+" couldn't be dropped. May have not existed.");
+				log("INFO: Creating database " + DB_NAME + "...");
+				fbManager.createDatabase(DB_NAME, DB_USER, DB_PASSWORD);
+				log("INFO: Database created");
+			} catch (Exception e) {
+				log("ERROR: Couldn't create new " + DB_NAME + " database.");
+				throw new BuildException(e);
 			}
-			try {
-				System.out.println("INFO: Creating database "+DB_NAME1+"...");
-				fbManager.createDatabase(DB_NAME1, DB_USER, DB_PASSWORD);
-				System.out.println("INFO: Database created");
-			} catch(Exception e) {
-				System.out.println("ERROR: Couldn't create new "+DB_NAME1+" database.");
-				//throw new BuildException(e);
-			}
-			try {
-				System.out.println("INFO: Creating database "+DB_NAME2+"...");
-				fbManager.createDatabase(DB_NAME2, DB_USER, DB_PASSWORD);
-				System.out.println("INFO: Database created");
-			} catch(Exception e) {
-				System.out.println("ERROR: Couldn't create new "+DB_NAME2+" database.");
-				//throw new BuildException(e);
-			}
-		} catch(Exception e) {
-			System.out.println("ERROR: Couldn't initialize Firebird Database Manager. No database was dropped or created.");
-			//throw new BuildException(e);
+		} catch (Exception e) {
+			log("ERROR: Couldn't initialize Firebird Database Manager. No database was dropped or created.");
+			throw new BuildException(e);
 		} finally {
 			try {
 				fbManager.stop();
-				System.out.println("INFO: Firebird Database Manager stopped");
-			} catch(Exception e) {;}
+				log("INFO: Firebird Database Manager stopped");
+			} catch (Exception e) {
+				;
+			}
 		}
 	}
+
 }
