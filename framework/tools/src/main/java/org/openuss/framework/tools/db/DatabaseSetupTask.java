289959a5a4aa50fdea1a8606b7877d6bc3d6f538
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-05-16 22:16:37 +0000

    code review, fixed some issue and polished the java code
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@845 60046477-0c10-0410-bf4b-e552a4ff1768

--- framework/tools/src/main/java/org/openuss/framework/tools/db/DatabaseSetupTask.java
+++ framework/tools/src/main/java/org/openuss/framework/tools/db/DatabaseSetupTask.java
@@ -12,76 +12,76 @@ import org.firebirdsql.management.FBManager;
  */
 public class DatabaseSetupTask extends Task {
 
-	private String DB_SERVER_URL = "";
+	private String dbServerUrl = "localhost";
 
-	private int DB_SERVER_PORT = 0;
+	private int dbServerPort = 3050;
 
-	private String DB_NAME = "";
+	private String dbName = "";
 
-	private String DB_USER = "";
+	private String dbUSer = "sysdba";
 
-	private String DB_PASSWORD = "";
+	private String dbPassword = "masterkey";
 
-	public void setDB_NAME(String db_name) {
-		DB_NAME = db_name;
+	public void setDbName(String dbName) {
+		this.dbName = dbName;
 	}
 
-	public void setDB_PASSWORD(String db_password) {
-		DB_PASSWORD = db_password;
+	public void setDbPassword(String dbPassword) {
+		this.dbPassword = dbPassword;
 	}
 
-	public void setDB_SERVER_PORT(int db_server_port) {
-		DB_SERVER_PORT = db_server_port;
+	public void setDbServerPort(int dbServerPort) {
+		this.dbServerPort = dbServerPort;
 	}
 
-	public void setDB_SERVER_URL(String db_server_url) {
-		DB_SERVER_URL = db_server_url;
+	public void setDbServerUrl(String dbServerUrl) {
+		this.dbServerUrl = dbServerUrl;
 	}
 
-	public void setDB_USER(String db_user) {
-		DB_USER = db_user;
+	public void setDbUSer(String dbUser) {
+		this.dbUSer = dbUser;
 	}
 
 	public void execute() throws BuildException {
 		
-		if (DB_SERVER_URL == "") {
+		if (dbServerUrl == null || dbServerUrl.trim().length() == 0) {
 			throw new BuildException("ERROR: The property DB_SERVER_URL is undefined.");
 		}
-		if (DB_SERVER_PORT == 0) {
+		if (dbServerPort == 0) {
 			throw new BuildException("ERROR: The property DB_SERVER_PORT is undefined.");
 		}
-		if (DB_NAME == "") {
+		if (dbName == null || dbName.trim().length() == 0) {
 			throw new BuildException("ERROR: The property DB_NAME is undefined.");
 		}
-		if (DB_USER == "") {
+		if (dbUSer == null || dbName.trim().length() == 0) {
 			throw new BuildException("ERROR: The property DB_USER is undefined.");
 		}
-		if (DB_PASSWORD == "") {
+		if (dbPassword == null || dbName.trim().length() == 0) {
 			throw new BuildException("ERROR: The property DB_PASSWORD is undefined.");
 		}
 		
 		FBManager fbManager = new FBManager();
 		fbManager.setForceCreate(true);
-		fbManager.setServer(DB_SERVER_URL);
-		fbManager.setPort(DB_SERVER_PORT);
+		fbManager.setServer(dbServerUrl);
+		fbManager.setPort(dbServerPort);
 
 		try {
 			log("INFO: Starting Firebird Database Manager...");
 			fbManager.start();
 			log("INFO: Firebird Database Manager started");
 			try {
-				log("INFO: Droping database " + DB_NAME + "...");
-				fbManager.dropDatabase(DB_NAME, DB_USER, DB_PASSWORD);
+				log("INFO: Droping database " + dbName + "...");
+				fbManager.dropDatabase(dbName, dbUSer, dbPassword);
 				log("INFO: Database dropped");
 			} catch (Exception e) {
-				log("INFO: Database " + DB_NAME + " couldn't be dropped. May have not existed.");
+				log("INFO: Database " + dbName + " couldn't be dropped. May have not existed.");
 			}
 			try {
-				log("INFO: Creating database " + DB_NAME + "...");
-				fbManager.createDatabase(DB_NAME, DB_USER, DB_PASSWORD);
+				log("INFO: Creating database " + dbName + "...");
+				fbManager.createDatabase(dbName, dbUSer, dbPassword);
 				log("INFO: Database created");
 			} catch (Exception e) {
-				log("ERROR: Couldn't create new " + DB_NAME + " database.");
+				log("ERROR: Couldn't create new " + dbName + " database.");
 				throw new BuildException(e);
 			}
 		} catch (Exception e) {
