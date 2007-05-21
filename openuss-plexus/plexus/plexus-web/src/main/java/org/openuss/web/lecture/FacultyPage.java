272dc3bef9d333e421dc3be676b4b3e5cd754095
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-05-21 13:40:45 +0000

    faculty mainpage polishing
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@910 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/FacultyPage.java
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/FacultyPage.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import javax.faces.event.ValueChangeEvent;
+
+import org.apache.log4j.Logger;
 import org.apache.shale.tiger.managed.Bean;
 import org.apache.shale.tiger.managed.Property;
 import org.apache.shale.tiger.managed.Scope;
+import org.apache.shale.tiger.view.Prerender;
 import org.apache.shale.tiger.view.View;
 import org.openuss.desktop.DesktopException;
 import org.openuss.framework.web.jsf.model.AbstractPagedTable;
 import org.openuss.framework.web.jsf.model.DataPage;
 import org.openuss.lecture.Enrollment;
 import org.openuss.lecture.LectureException;
+import org.openuss.lecture.Period;
 import org.openuss.news.NewsService;
 import org.openuss.web.Constants;
 
@@ -22,42 +27,81 @@ import org.openuss.web.Constants;
 @View
 @Bean(name = "views$secured$lecture$faculty", scope = Scope.REQUEST)
 public class FacultyPage extends AbstractLecturePage {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = Logger.getLogger(FacultyPage.class);
+
 	private static final long serialVersionUID = -1982354759705300093L;
 
 	@Property(value = "#{newsService}")
 	private NewsService newsService;
-	
+
+	@Property(value = "#{sessionScope.period}")
+	private Period period;
+
 	private EnrollmentDataModel enrollmentData = new EnrollmentDataModel();
-	
+
+	/**
+	 * Refreshing faculty entity
+	 * 
+	 * @throws Exception
+	 */
+	@Prerender
+	public void prerender() throws LectureException {
+		super.prerender();
+		if (period == null && faculty != null) {
+			period = faculty.getActivePeriod();
+		} else {
+			period = lectureService.getPeriod(period.getId());
+		}
+		setSessionBean(Constants.PERIOD, period);
+	}
+
+	/**
+	 * Value Change Listener to swithc password input text on and off.
+	 * 
+	 * @param event
+	 */
+	public void processPeriodSelectChanged(ValueChangeEvent event) {
+		final Long periodId = (Long) event.getNewValue();
+		period = lectureService.getPeriod(periodId);
+		setSessionBean(Constants.PERIOD, period);
+	}
+
 	private class EnrollmentDataModel extends AbstractPagedTable<Enrollment> {
-		
+
 		private static final long serialVersionUID = 3682383483634321520L;
-		
+
 		private DataPage<Enrollment> page;
 
 		@Override
 		public DataPage<Enrollment> getDataPage(int startRow, int pageSize) {
 			if (page == null) {
-				List<Enrollment> enrollments = new ArrayList(); 
-				if (faculty != null) { 
-					enrollments.addAll(faculty.getActiveEnrollments());
+				List<Enrollment> enrollments = new ArrayList();
+				if (period != null) {
+					period = lectureService.getPeriod(period.getId());
+					enrollments.addAll(period.getEnrollments());
+				} else {
+					logger.error("faculty page - no period selected!");
 				}
 				sort(enrollments);
-				page = new DataPage<Enrollment>(enrollments.size(),0,enrollments);
+				page = new DataPage<Enrollment>(enrollments.size(), 0, enrollments);
 			}
 			return page;
 		}
 	}
-	
+
 	/**
 	 * Current news items of faculty
+	 * 
 	 * @return
 	 * @throws LectureException
 	 */
 	public Collection getCurrentNewsItems() {
 		return newsService.getCurrentNewsItems(faculty, 10);
 	}
-	
+
 	public NewsService getNewsService() {
 		return newsService;
 	}
@@ -65,7 +109,7 @@ public class FacultyPage extends AbstractLecturePage {
 	public void setNewsService(NewsService newsService) {
 		this.newsService = newsService;
 	}
-	
+
 	public String showApplication() {
 		return Constants.DESKTOP;
 	}
@@ -77,11 +121,19 @@ public class FacultyPage extends AbstractLecturePage {
 	public void setEnrollmentData(EnrollmentDataModel enrollmentData) {
 		this.enrollmentData = enrollmentData;
 	}
-	
+
 	public String shortcutEnrollment() throws DesktopException {
 		Enrollment enrollment = enrollmentData.getRowData();
 		desktopService.linkEnrollment(desktop, enrollment);
 		addMessage(i18n("message_enrollment_shortcut_created"));
-		return Constants.FACULTY_PAGE; 
+		return Constants.FACULTY_PAGE;
+	}
+
+	public Period getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(Period period) {
+		this.period = period;
 	}
 }
