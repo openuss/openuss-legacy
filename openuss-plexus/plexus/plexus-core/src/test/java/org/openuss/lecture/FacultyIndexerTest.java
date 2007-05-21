50e2e30d92c45d9fcf6d0ea355d3d08b5ae58eb1
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-05-21 11:34:47 +0000

    fixed application context
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@904 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/FacultyIndexerTest.java
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/FacultyIndexerTest.java
@@ -35,16 +35,17 @@ public class FacultyIndexerTest extends AbstractDependencyInjectionSpringContext
 	
 	protected String[] getConfigLocations() {
 		return new String[] { 
-			"classpath*:applicationContext.xml",
-			"classpath*:applicationContext-beans.xml", 
-			"classpath*:applicationContext-tests.xml",
+			"classpath*:applicationContext.xml", 
+			"classpath*:applicationContext-beans.xml",
+			"classpath*:applicationContext-tests.xml", 
 			"classpath*:applicationContext-lucene.xml",
-			"classpath*:applicationContext-cache.xml",
-			"classpath*:beanRefFactory",
-			"classpath*:testSecurity.xml",
+			"classpath*:applicationContext-cache.xml", 
+			"classpath*:applicationContext-messaging.xml",
+			"classpath*:applicationContext-resources.xml",
+			"classpath*:beanRefFactory", 
+			"classpath*:testSecurity.xml", 
 			"classpath*:testDataSource.xml"};
 	}
-
 	public FacultyIndexer getFacultyIndexer() {
 		return facultyIndexer;
 	}
