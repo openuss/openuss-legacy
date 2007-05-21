adb4e952a9843fe8e59f22bcef85e88932754a71
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-05-21 11:50:41 +0000

    reinsert renamed classes
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@909 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-core/src/main/java/org/openuss/search/lucene/WorkAroundIndexReader.java
@@ -0,0 +1,31 @@
+package org.openuss.search.lucene;
+
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.Searcher;
+import org.springmodules.lucene.index.factory.SimpleLuceneIndexReader;
+import org.springmodules.lucene.search.factory.LuceneSearcher;
+
+/**
+ * Workaround class until spring modules will implement the SimpleLuceneIndexReader. 
+ * @author Ingo Dueppe
+ *
+ */
+public class WorkAroundIndexReader extends SimpleLuceneIndexReader {
+	 
+	private LuceneSearcher luceneSearcher;
+
+	public WorkAroundIndexReader(IndexReader indexReader, LuceneSearcher luceneSearcher) {
+		super(indexReader);
+		this.luceneSearcher = luceneSearcher;
+	}
+
+	@Override
+	public LuceneSearcher createSearcher() {
+		return luceneSearcher;
+	}
+
+	@Override
+	public Searcher createNativeSearcher() {
+		return super.createNativeSearcher();
+	}
+}
