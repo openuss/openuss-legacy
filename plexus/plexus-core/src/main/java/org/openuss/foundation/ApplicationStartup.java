e7c7bf7e3eb77f782889a6ba87275c13964f2871
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-05-24 17:00:11 +0000

    define a startup applicationContext
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@983 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-core/src/main/java/org/openuss/foundation/ApplicationStartup.java
@@ -0,0 +1,47 @@
+package org.openuss.foundation;
+
+import org.apache.log4j.Logger;
+
+import org.openuss.lecture.LectureIndex;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+/**
+ * This bean will initialize openuss plexus application after startup. 
+ * 
+ * @author Ingo Dueppe
+ */
+public class ApplicationStartup implements ApplicationListener {
+
+	private static final Logger logger = Logger.getLogger(ApplicationStartup.class);
+	
+	private LectureIndex lectureIndex;
+
+	public void onApplicationEvent(ApplicationEvent event) {
+		if (event instanceof ContextRefreshedEvent) {
+			try {
+				performInitialization();
+			} catch (Exception e) {
+				logger.error("initialization failse "+e);
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	private void performInitialization() throws Exception {
+		logger.info("===================> starting to initialize openuss plexus application! <=====================");
+		lectureIndex.recreate();
+		logger.info("===================> finished to initialize openuss plexus application!  <=====================");
+	}
+
+	public LectureIndex getLectureIndex() {
+		return lectureIndex;
+	}
+
+	public void setLectureIndex(LectureIndex recreateLectureIndex) {
+		this.lectureIndex = recreateLectureIndex;
+	}
+
+
+}
