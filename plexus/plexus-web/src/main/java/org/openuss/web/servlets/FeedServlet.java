9bf9424d02786d5b2e0c36235fe49daf8baebcca
roekens <roekens>
2007-06-01 11:46:42 +0000

    implementing feed feature - IN PROGRESS
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@1047 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/servlets/FeedServlet.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/servlets/FeedServlet.java
@@ -1,5 +1,11 @@
 package org.openuss.web.servlets;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.openuss.feed.FeedService;
+import org.openuss.feed.FeedWrapper;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -35,11 +42,24 @@ public class FeedServlet extends HttpServlet{
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws java.io.IOException {
 		Long enrollmentId = Long.parseLong(req.getParameter("enrollment"));
+		String modifiedSince = req.getParameter("If-Modified-Since");
 		if (enrollmentId!=null) {
-			String xmlFeed = feedService.getRssFeedForEnrollment(enrollmentId);
-			res.setContentType("application/xml; charset=utf-8");
-			res.getWriter().write(xmlFeed);
-			logger.debug(xmlFeed);
+			FeedWrapper  feedWrapper = feedService.getRssFeedForEnrollment(enrollmentId);
+			if (modifiedSince!=null&&modifiedSince!=""){
+				try {
+					if (DateFormat.getDateTimeInstance().parse(modifiedSince).getTime()<feedWrapper.getLastModified().getTime()){
+						res.sendError(HttpServletResponse.SC_NOT_MODIFIED);
+						return;
+					}
+				} catch (ParseException e) {
+					logger.debug("Malformed header information");
+				}
+				Calendar c = new GregorianCalendar();
+			}
+			//res.setContentType("application/xml");
+			res.setHeader("ETag", "46a963-1316-70caa880");
+			res.getWriter().write(feedWrapper.getWriter().toString());
+			res.setHeader("Last-Modified", feedWrapper.getLastModified().toString());
 		}
 	}
 	
