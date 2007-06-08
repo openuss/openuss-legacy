3a8f187d819be8005d0da7ee030444f10e1a96b6
roekens <roekens>
2007-06-08 07:45:31 +0000

    working on deep linking in mailinglist package
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@1109 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/mail/ShowMailPage.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/mail/ShowMailPage.java
@@ -1,11 +1,16 @@
 package org.openuss.web.mail;
 
 import org.apache.shale.tiger.managed.Bean;
+import org.apache.shale.tiger.managed.Property;
 import org.apache.shale.tiger.managed.Scope;
 import org.apache.shale.tiger.view.Prerender;
 import org.apache.shale.tiger.view.View;
+import org.openuss.enrollment.mailinglist.EnrollmentMailingListService;
+import org.openuss.mailinglist.MailDetail;
 import org.openuss.mailinglist.MailInfo;
+import org.openuss.mailinglist.MailingListInfo;
 import org.openuss.mailinglist.MailingStatus;
+import org.openuss.web.BasePage;
 import org.openuss.web.Constants;
 
 /**
@@ -15,16 +20,30 @@ import org.openuss.web.Constants;
  */
 @Bean(name = "views$secured$mailinglist$showmail", scope = Scope.REQUEST)
 @View
-public class ShowMailPage extends AbstractMailingListPage{
+public class ShowMailPage extends BasePage{
+	
+	@Property(value = "#{enrollmentMailingListService}")
+	protected EnrollmentMailingListService enrollmentMailingListService;
+	
+	@Property(value = "#{" + Constants.MAILINGLIST_MAIL + "}")
+	protected MailDetail mail;
+	
+	@Property(value = "#{" + Constants.MAILINGLIST_MAILINGLIST + "}")
+	protected MailingListInfo mailingList;
+
+	
 	@Prerender
 	public void prerender() throws Exception {
-		super.prerender();
 		if (mail==null){
 			addError(i18n("mailinglist_mailaccess_impossible"));
 			redirect(Constants.MAILINGLIST_MAIN);
 			return;
 		}			
-		if (mail.getId() == null) setMail(null);
+		if (mail.getId() == null) {
+			addError(i18n("mailinglist_mailaccess_impossible"));
+			redirect(Constants.MAILINGLIST_MAIN);
+			return;
+		}
 		if (mail.getId()!=null){
 			MailInfo mi = new MailInfo(); 
 			mi.setId(mail.getId());
@@ -42,5 +61,36 @@ public class ShowMailPage extends AbstractMailingListPage{
 			}
 			setSessionBean(Constants.MAILINGLIST_MAIL, mail);
 		}
+	}
+
+
+	public EnrollmentMailingListService getEnrollmentMailingListService() {
+		return enrollmentMailingListService;
+	}
+
+
+	public void setEnrollmentMailingListService(
+			EnrollmentMailingListService enrollmentMailingListService) {
+		this.enrollmentMailingListService = enrollmentMailingListService;
+	}
+
+
+	public MailDetail getMail() {
+		return mail;
+	}
+
+
+	public void setMail(MailDetail mail) {
+		this.mail = mail;
+	}
+
+
+	public MailingListInfo getMailingList() {
+		return mailingList;
+	}
+
+
+	public void setMailingList(MailingListInfo mailingList) {
+		this.mailingList = mailingList;
 	}	
 }
