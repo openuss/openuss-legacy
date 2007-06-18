72266173e73b3f2f384949917319998f856f5330
roekens <roekens>
2007-06-18 13:48:55 +0000

    added first integration for bread crumbs - defined new jsf tag for breadcrumbs, based on tomahawk datatable - added automatic setting of breadcrumbs for faculty and enrollment to AbstractEnrollmentPage - integrated breadcrumb for enrollment main page - introduced PageLinks as constant list of links to pages relative to server path
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@1158 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ framework/jsfcontrols/src/main/java/org/openuss/framework/jsfcontrols/breadcrumbs/BreadCrumb.java
@@ -0,0 +1,35 @@
+package org.openuss.framework.jsfcontrols.breadcrumbs;
+
+public class BreadCrumb{
+	
+	private String link;
+	
+	private String name;
+	
+	private String hint;
+
+	public String getHint() {
+		return hint;
+	}
+
+	public void setHint(String hint) {
+		this.hint = hint;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
