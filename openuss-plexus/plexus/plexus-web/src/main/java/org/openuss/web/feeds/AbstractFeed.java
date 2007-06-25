d293d68242c2dcb6c2df4eb4e0425a7722160509
roekens <roekens>
2007-06-25 12:53:48 +0000

    resolved issue USS-290 http://teamopenuss.uni-muenster.de/jira/browse/USS-290
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@1237 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/feeds/AbstractFeed.java
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/feeds/AbstractFeed.java
@@ -5,9 +5,12 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import org.apache.log4j.Logger;
-import org.openuss.web.BasePage;
+import org.openuss.security.SecurityService;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceAware;
 
 import com.sun.syndication.feed.synd.SyndCategory;
 import com.sun.syndication.feed.synd.SyndCategoryImpl;
@@ -19,7 +22,7 @@ import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.feed.synd.SyndFeedImpl;
 import com.sun.syndication.io.SyndFeedOutput;
 
-public abstract class AbstractFeed extends BasePage {
+public abstract class AbstractFeed implements MessageSourceAware {
 
 	public static final String RSS_2_0 = "rss_2.0";
 
@@ -28,6 +31,16 @@ public abstract class AbstractFeed extends BasePage {
 	public static final String TEXT_HTML = "text/html";
 	
 	public static final Logger logger = Logger.getLogger(AbstractFeed.class);
+	
+	private MessageSource messageSource;
+	
+	private MessageSource i18nMessageSource;
+	
+	protected SecurityService securityService;
+	
+	public String i18n(String code, Object[] args, Locale locale){
+		return messageSource.getMessage(code, args, locale);
+	}
 
 	@SuppressWarnings("unchecked")
 	public void addEntry(List entries, String title, String link, Date date, String blogContent, String cat, String author) {
@@ -85,6 +98,27 @@ public abstract class AbstractFeed extends BasePage {
             	logger.error("Unknown error: ", ex);
             }
             return null;
-	}	
-	
+	}
+
+	public void setMessageSource(MessageSource messageSource) {
+		//this.messageSource = messageSource;
+	}
+
+	public SecurityService getSecurityService() {
+		return securityService;
+	}
+
+	public void setSecurityService(SecurityService securityService) {
+		this.securityService = securityService;
+	}
+
+	public MessageSource getI18nMessageSource() {
+		return i18nMessageSource;
+	}
+
+	public void setI18nMessageSource(MessageSource messageSource) {
+		i18nMessageSource = messageSource;
+		this.messageSource = messageSource;
+	}
+
 }
