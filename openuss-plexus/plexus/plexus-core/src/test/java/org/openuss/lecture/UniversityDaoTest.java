61f074d3948149582fc83ca3265ef86d5ba4846e
ronhaus <ronhaus>
2007-07-03 19:31:33 +0000

    added basic DAO tests
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1262 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/UniversityDaoTest.java
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/UniversityDaoTest.java
@@ -5,15 +5,38 @@
  */
 package org.openuss.lecture;
 
+import org.openuss.TestUtility;
+import org.openuss.security.Membership;
+
 
 /**
  * JUnit Test for Spring Hibernate UniversityDao class.
  * @see org.openuss.lecture.UniversityDao
+ * @author Ron Haus
  */
 public class UniversityDaoTest extends UniversityDaoTestBase {
 	
+private TestUtility testUtility;
+	
+	public TestUtility getTestUtility() {
+		return testUtility;
+	}
+
+	public void setTestUtility(TestUtility testUtility) {
+		this.testUtility = testUtility;
+	}
+	
 	public void testUniversityDaoCreate() {
-		University university = new UniversityImpl();
+		University university = University.Factory.newInstance();
+		university.setName("testUniversity");
+		university.setShortcut("testU");
+		
+		Membership membership = Membership.Factory.newInstance();
+		membership.setOwner(testUtility.createUserInDB());
+		membership.getAspirants().add(testUtility.createUserInDB());
+		membership.getMembers().add(testUtility.createUserInDB());
+		university.setMembership(membership);
+		
 		assertNull(university.getId());
 		universityDao.create(university);
 		assertNotNull(university.getId());
--- openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/security/MembershipDaoTest.java
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/security/MembershipDaoTest.java
@@ -5,15 +5,32 @@
  */
 package org.openuss.security;
 
+import org.openuss.TestUtility;
+
 
 /**
  * JUnit Test for Spring Hibernate MembershipDao class.
  * @see org.openuss.security.MembershipDao
+ * @author Ron Haus
  */
 public class MembershipDaoTest extends MembershipDaoTestBase {
 	
+	private TestUtility testUtility;
+	
+	public TestUtility getTestUtility() {
+		return testUtility;
+	}
+
+	public void setTestUtility(TestUtility testUtility) {
+		this.testUtility = testUtility;
+	}
+	
 	public void testMembershipDaoCreate() {
-		Membership membership = new MembershipImpl();
+		Membership membership = Membership.Factory.newInstance();
+		membership.setOwner(testUtility.createUserInDB());
+		membership.getAspirants().add(testUtility.createUserInDB());
+		membership.getMembers().add(testUtility.createUserInDB());
+		
 		assertNull(membership.getId());
 		membershipDao.create(membership);
 		assertNotNull(membership.getId());
