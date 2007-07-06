a36ea19eee3766b805867e171f1105fc3ce3f36e
ronhaus <ronhaus>
2007-07-06 14:06:55 +0000

    Implemented methods "acceptAspirant" and "addAspirant"
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1269 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/security/MembershipServiceImpl.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/security/MembershipServiceImpl.java
@@ -18,12 +18,10 @@ import org.openuss.lecture.Organisation;
 public class MembershipServiceImpl extends org.openuss.security.MembershipServiceBase {
 
 	/**
-	 * @see org.openuss.security.MembershipService#setOwner(java.lang.Long,
-	 *      java.lang.Long)
+	 * @see org.openuss.security.MembershipService#setOwner(java.lang.Long, java.lang.Long)
 	 */
 	protected void handleSetOwner(java.lang.Long organisationId, java.lang.Long userId) throws java.lang.Exception {
-		// TODO Check Security - only admin and owner must be allowed to change
-		// the owner
+		// TODO Check Security - only admin and owner must be allowed to change the owner
 		User owner = this.getUserDao().load(userId);
 		if (owner == null) {
 			throw new IllegalArgumentException(
@@ -55,17 +53,46 @@ public class MembershipServiceImpl extends org.openuss.security.MembershipServic
 	}
 
 	/**
-	 * @see org.openuss.security.MembershipService#acceptAspirant(java.lang.Long,
-	 *      java.lang.Long)
+	 * @see org.openuss.security.MembershipService#acceptAspirant(java.lang.Long, java.lang.Long)
 	 */
 	protected void handleAcceptAspirant(java.lang.Long organisationId, java.lang.Long userId)
 			throws java.lang.Exception {
+		// TODO Check Security - only admin, owner and members must be allowed to accept aspirants
+
+		User aspirant = this.getUserDao().load(userId);
+		if (aspirant == null) {
+			throw new IllegalArgumentException(
+					"MembershipService.handleAcceptAspirant - no user found corresponding to the id " + userId);
+		}
+
+		Organisation organisation = this.getOrganisationDao().load(organisationId);
+		if (organisation == null) {
+			throw new IllegalArgumentException(
+					"MembershipService.handleAcceptAspirant - no organisation found corresponding to the id "
+							+ organisationId);
+		}
+
+		// Remove Aspirant from List of Aspirants
+		List aspirants = organisation.getAspirants();
+		boolean wasFound = aspirants.remove(aspirant);
+		if (!wasFound) {
+			throw new IllegalArgumentException("MembershipService.handleAcceptAspirant - the user "
+					+ aspirant.getUsername() + " has not been an Aspirant");
+		}
+
+		// There is no need to check whether the Aspirant is already a Member or Owner, since the method addAspirant
+		// is taken care of that
+		
+		// Add Aspirant to List of Members
+		List members = organisation.getMembers();
+		members.add(aspirant);
+		
+		// To inform the aspirant of his "upgrade" is the responibility of the web layer
 
 	}
 
 	/**
-	 * @see org.openuss.security.MembershipService#rejectAspirant(java.lang.Long,
-	 *      java.lang.Long)
+	 * @see org.openuss.security.MembershipService#rejectAspirant(java.lang.Long, java.lang.Long)
 	 */
 	protected void handleRejectAspirant(java.lang.Long organisationId, java.lang.Long userId)
 			throws java.lang.Exception {
@@ -73,17 +100,49 @@ public class MembershipServiceImpl extends org.openuss.security.MembershipServic
 	}
 
 	/**
-	 * @see org.openuss.security.MembershipService#addMember(java.lang.Long,
-	 *      java.lang.Long)
+	 * @see org.openuss.security.MembershipService#addMember(java.lang.Long, java.lang.Long)
 	 */
 	protected void handleAddMember(java.lang.Long organisationId, java.lang.Long userId) throws java.lang.Exception {
+		// TODO Check Security - only admin, owner and members must be allowed to add members
 	}
 
 	/**
-	 * @see org.openuss.security.MembershipService#addAspirant(java.lang.Long,
-	 *      java.lang.Long)
+	 * @see org.openuss.security.MembershipService#addAspirant(java.lang.Long, java.lang.Long)
 	 */
 	protected void handleAddAspirant(java.lang.Long organisationId, java.lang.Long userId) throws java.lang.Exception {
+		// TODO Check Security - only admin, owner and members must be allowed to add aspirants
+
+		User user = this.getUserDao().load(userId);
+		if (user == null) {
+			throw new IllegalArgumentException(
+					"MembershipService.handleAddAspirant - no user found corresponding to the id " + userId);
+		}
+
+		Organisation organisation = this.getOrganisationDao().load(organisationId);
+		if (organisation == null) {
+			throw new IllegalArgumentException(
+					"MembershipService.handleAddAspirant - no organisation found corresponding to the id "
+							+ organisationId);
+		}
+
+		// Check whether the User is already a Member, Aspirant or the Owner
+		if (organisation.getOwner().equals(user)) {
+			throw new IllegalArgumentException("MembershipService.handleAddAspirant - the User " + user.getUsername()
+					+ " is already the Owner");
+		}
+		List members = organisation.getMembers();
+		if (members.contains(user)) {
+			throw new IllegalArgumentException("MembershipService.handleAddAspirant - the User " + user.getUsername()
+					+ " is already a Member");
+		}
+		List aspirants = organisation.getAspirants();
+		if (aspirants.contains(user)) {
+			throw new IllegalArgumentException("MembershipService.handleAddAspirant - the User " + user.getUsername()
+					+ " is already an Aspirant");
+		}
+
+		// Add User to the List of Aspirants
+		aspirants.add(user);
 
 	}
 
--- openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/security/MembershipServiceIntegrationTest.java
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/security/MembershipServiceIntegrationTest.java
@@ -78,7 +78,7 @@ public class MembershipServiceIntegrationTest extends MembershipServiceIntegrati
 		// Get List of Members
 		List members = membershipService.findAllMembers(university.getId());
 		assertNotNull(members);
-		assertEquals(members.size(),0);
+		int sizeBefore = members.size();
 
 		// Add a user using DAO object
 		university.getMembers().add(user1);
@@ -86,7 +86,7 @@ public class MembershipServiceIntegrationTest extends MembershipServiceIntegrati
 		// Get List of Members again
 		List members2 = membershipService.findAllMembers(university.getId());
 		assertNotNull(members2);
-		assertEquals(members2.size(),1);
+		assertEquals(members2.size(),sizeBefore+1);
 		
 		logger.info("----> END access to findAllMembers test");
 	}
@@ -103,7 +103,7 @@ public class MembershipServiceIntegrationTest extends MembershipServiceIntegrati
 		// Get List of Aspirants
 		List aspirants = membershipService.findAllAspirants(university.getId());
 		assertNotNull(aspirants);
-		assertEquals(aspirants.size(),0);
+		int sizeBefore = aspirants.size();
 		
 		// Add a user using DAO object
 		university.getAspirants().add(user1);
@@ -111,9 +111,86 @@ public class MembershipServiceIntegrationTest extends MembershipServiceIntegrati
 		// Get List of Aspirants again
 		List aspirants2 = membershipService.findAllAspirants(university.getId());
 		assertNotNull(aspirants2);
-		assertEquals(aspirants2.size(),1);
+		assertEquals(aspirants2.size(),sizeBefore+1);
 		
 		logger.info("----> END access to findAllAspirants test");
+	}
+	
+	public void testAddAspirant() {
+		logger.info("----> BEGIN access to addAspirant test");
+		
+		// Create University with DefaultUser as Owner
+		University university = testUtility.createPersistUniversityWithDefaultUser();
+		
+		// Try to add Owner as an Aspirant
+		try {
+			membershipService.addAspirant(university.getId(), university.getOwner().getId());
+			fail("Exception should have been thrown");
+		} catch (MembershipServiceException mse) {};
+		
+		// Create a 2nd User
+		User user1 = testUtility.createUserInDB();
+		
+		// Try to add Member as an Aspirant
+		try {
+			university.getMembers().add(user1);
+			membershipService.addAspirant(university.getId(), user1.getId());
+			fail("Exception should have been thrown");
+		} catch (MembershipServiceException mse) {};
+
+		//	Get List of Aspirants
+		List aspirants = university.getAspirants();
+		assertNotNull(aspirants);
+		int sizeBefore = aspirants.size();
+		
+		// Create a 3rd User
+		User user2 = testUtility.createUserInDB();
+		
+		// Add a user
+		membershipService.addAspirant(university.getId(), user2.getId());
+		
+		// Get List of Aspirants again
+		List aspirants2 = university.getAspirants();
+		assertNotNull(aspirants2);
+		assertEquals(aspirants2.size(),sizeBefore+1);
+		
+		// Try to add Owner as an Aspirant
+		try {
+			membershipService.addAspirant(university.getId(), user2.getId());
+			fail("Exception should have been thrown");
+		} catch (MembershipServiceException mse) {};
+		
+		logger.info("----> END access to addAspirant test");
+	}
+	
+	public void testAcceptAspirant() {
+		logger.info("----> BEGIN access to acceptAspirant test");
+		
+		// Create University with DefaultUser as Owner
+		University university = testUtility.createPersistUniversityWithDefaultUser();
+		
+		// Create a 2nd User
+		User user1 = testUtility.createUserInDB();
+		
+		// Try to accept the User
+		try {
+			membershipService.acceptAspirant(university.getId(), user1.getId());
+			fail("Exception should have been thrown");
+		} catch (MembershipServiceException mse) {};
+		
+		//Add User to Aspirants and accept after that
+		university.getAspirants().add(user1);
+		int sizeAspirantsBefore = university.getAspirants().size();
+		int sizeMembersBefore = university.getMembers().size();
+		membershipService.acceptAspirant(university.getId(), user1.getId());
+		List aspirants = university.getAspirants();
+		assertNotNull(aspirants);
+		assertEquals(aspirants.size(),sizeAspirantsBefore-1);
+		List members = university.getMembers();
+		assertNotNull(members);
+		assertEquals(members.size(),sizeMembersBefore+1);
+		
+		logger.info("----> END access to acceptAspirant test");
 	}
 
 	private static void createSecureContext(String roleName) {
