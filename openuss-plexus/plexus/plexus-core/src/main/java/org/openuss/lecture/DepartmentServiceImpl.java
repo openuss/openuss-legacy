bbd0654de2de3931afde39c2ef53719bc17f71a4
ronhaus <ronhaus>
2007-07-12 09:53:47 +0000

    added DepartmentService stub
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1304 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/lecture/DepartmentServiceImpl.java
@@ -0,0 +1,95 @@
+// OpenUSS - Open Source University Support System
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package org.openuss.lecture;
+
+/**
+ * @see org.openuss.lecture.DepartmentService
+ */
+public class DepartmentServiceImpl
+    extends org.openuss.lecture.DepartmentServiceBase
+{
+
+    /**
+     * @see org.openuss.lecture.DepartmentService#create(org.openuss.lecture.DepartmentInfo, java.lang.Long)
+     */
+    protected java.lang.Long handleCreate(org.openuss.lecture.DepartmentInfo department, java.lang.Long ownerId)
+        throws java.lang.Exception
+    {
+        // @todo implement protected java.lang.Long handleCreate(org.openuss.lecture.DepartmentInfo department, java.lang.Long ownerId)
+        return null;
+    }
+
+    /**
+     * @see org.openuss.lecture.DepartmentService#update(org.openuss.lecture.DepartmentInfo)
+     */
+    protected void handleUpdate(org.openuss.lecture.DepartmentInfo department)
+        throws java.lang.Exception
+    {
+        // @todo implement protected void handleUpdate(org.openuss.lecture.DepartmentInfo department)
+        throw new java.lang.UnsupportedOperationException("org.openuss.lecture.DepartmentService.handleUpdate(org.openuss.lecture.DepartmentInfo department) Not implemented!");
+    }
+
+    /**
+     * @see org.openuss.lecture.DepartmentService#removeDepartment(java.lang.Long)
+     */
+    protected void handleRemoveDepartment(java.lang.Long departmentId)
+        throws java.lang.Exception
+    {
+        // @todo implement protected void handleRemoveDepartment(java.lang.Long departmentId)
+        throw new java.lang.UnsupportedOperationException("org.openuss.lecture.DepartmentService.handleRemoveDepartment(java.lang.Long departmentId) Not implemented!");
+    }
+
+    /**
+     * @see org.openuss.lecture.DepartmentService#findDepartment(java.lang.Long)
+     */
+    protected org.openuss.lecture.DepartmentInfo handleFindDepartment(java.lang.Long departmentId)
+        throws java.lang.Exception
+    {
+        // @todo implement protected org.openuss.lecture.DepartmentInfo handleFindDepartment(java.lang.Long departmentId)
+        return null;
+    }
+
+    /**
+     * @see org.openuss.lecture.DepartmentService#findDepartmentsByUniversity(java.lang.Long)
+     */
+    protected java.util.List handleFindDepartmentsByUniversity(java.lang.Long universityId)
+        throws java.lang.Exception
+    {
+        // @todo implement protected java.util.List handleFindDepartmentsByUniversity(java.lang.Long universityId)
+        return null;
+    }
+
+    /**
+     * @see org.openuss.lecture.DepartmentService#acceptApplication(java.lang.Long)
+     */
+    protected void handleAcceptApplication(java.lang.Long applicationId)
+        throws java.lang.Exception
+    {
+        // @todo implement protected void handleAcceptApplication(java.lang.Long applicationId)
+        throw new java.lang.UnsupportedOperationException("org.openuss.lecture.DepartmentService.handleAcceptApplication(java.lang.Long applicationId) Not implemented!");
+    }
+
+    /**
+     * @see org.openuss.lecture.DepartmentService#rejectApplication(java.lang.Long)
+     */
+    protected void handleRejectApplication(java.lang.Long applicationId)
+        throws java.lang.Exception
+    {
+        // @todo implement protected void handleRejectApplication(java.lang.Long applicationId)
+        throw new java.lang.UnsupportedOperationException("org.openuss.lecture.DepartmentService.handleRejectApplication(java.lang.Long applicationId) Not implemented!");
+    }
+
+    /**
+     * @see org.openuss.lecture.DepartmentService#findByUserAndUniversity(java.lang.Long, java.lang.Long)
+     */
+    protected java.util.List handleFindByUserAndUniversity(java.lang.Long userId, java.lang.Long universityId)
+        throws java.lang.Exception
+    {
+        // @todo implement protected java.util.List handleFindByUserAndUniversity(java.lang.Long userId, java.lang.Long universityId)
+        return null;
+    }
+
+}
---
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/DepartmentServiceIntegrationTest.java
@@ -0,0 +1,14 @@
+// OpenUSS - Open Source University Support System
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package org.openuss.lecture;
+
+/**
+ * JUnit Test for Spring Hibernate DepartmentService class.
+ * @see org.openuss.lecture.DepartmentService
+ */
+public class DepartmentServiceIntegrationTest extends DepartmentServiceIntegrationTestBase {
+	
+}
