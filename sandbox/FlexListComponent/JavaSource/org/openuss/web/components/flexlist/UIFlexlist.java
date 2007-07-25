74f08ac0b0a5d43dbdc2c8ad7fc569028fd2625e
julian_reimann <julian_reimann>
2007-07-25 11:14:33 +0000

    Fixed static use of ids in html output, now uses dynamic ids based on ids from the component tree
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@1392 60046477-0c10-0410-bf4b-e552a4ff1768

--- sandbox/FlexListComponent/JavaSource/org/openuss/web/components/flexlist/UIFlexlist.java
+++ sandbox/FlexListComponent/JavaSource/org/openuss/web/components/flexlist/UIFlexlist.java
@@ -14,20 +14,30 @@ public class UIFlexlist extends UIOutput {
 		ResponseWriter writer = context.getResponseWriter();
 		
 		writer.startElement("div", this);
-		writer.writeAttribute("class", "flexList", null);
+			writer.writeAttribute("class", "flexList", null);
+			
 			writer.startElement("ul", this);
 			writer.writeAttribute("class", "flexListItems", null);
-				writer.startElement("li", this);
-				writer.writeAttribute("class", "header", null);
-					writer.startElement("div", this);
-					writer.writeAttribute("class", "flexListItemRight", null);
-					writer.endElement("div");
-					writer.startElement("div", this);
-					writer.writeAttribute("class", "flexListItemLeft", null);
-					writer.write( getAttributeString("title") );
-					writer.endElement("div");
-				writer.endElement("li");
+			
+				// render title if specified as attribute
+				String title = (String)getAttributes().get("title");
+				if(title != null) {
+					writer.startElement("li", this);
+					writer.writeAttribute("class", "header", null);
+					
+						writer.startElement("div", this);
+							writer.writeAttribute("class", "flexListItemRight", null);
+						writer.endElement("div");
+						
+						writer.startElement("div", this);
+							writer.writeAttribute("class", "flexListItemLeft", null);
+							writer.write( title );
+						writer.endElement("div");
+						
+					writer.endElement("li");
+				}
 				
+				// render list of visible items
 				i = ((ArrayList)(getAttributes().get("visibleItems"))).iterator();
 				while(i.hasNext()) {
 					listItem = (ListItemDAO)i.next();
@@ -44,60 +54,80 @@ public class UIFlexlist extends UIOutput {
 				}
 			writer.endElement("ul");
 
+			// render list of hidden items
 			writer.startElement("ul", this);
-			writer.writeAttribute("class", "flexListItems", null);
-			writer.writeAttribute("id", "hidden_flexListItems1", null);
-			writer.writeAttribute("style", "display:none;", null);
+				writer.writeAttribute("class", "flexListItems", null);
+				writer.writeAttribute("id", "hidden_items" + this.getId(), null);
+				writer.writeAttribute("style", "display:none;", null);
 			
+				// attribute "hiddenItems" is set by the backingbean and contains a list of items of type ArrayList
 				i = ((ArrayList)(getAttributes().get("hiddenItems"))).iterator();
-				
 				while(i.hasNext()) {
 					listItem = (ListItemDAO)i.next();
 					writer.startElement("li", this);
+					
 						writer.startElement("div", this);
-						writer.writeAttribute("class", "flexListItemRight", null);
+							writer.writeAttribute("class", "flexListItemRight", null);
 							writer.write( listItem.getTitle());
 						writer.endElement("div");
+						
 						writer.startElement("div", this);
-						writer.writeAttribute("class", "flexListItemLeft", null);
+							writer.writeAttribute("class", "flexListItemLeft", null);
 							writer.write( listItem.getInformation());
 						writer.endElement("div");
+						
 					writer.endElement("li");
 				}
 			writer.endElement("ul");
 
+			// render the list footer containing the buttons for showing and hiding the hidden items
 			writer.startElement("ul", this);
-			writer.writeAttribute("class", "flexListBottom", null);
+				writer.writeAttribute("class", "flexListBottom", null);
+				
 				writer.startElement("li", this);
+				
 					writer.startElement("div", this);
-					writer.writeAttribute("class", "flexListItemRight", null);
+						writer.writeAttribute("class", "flexListItemRight", null);
 					writer.endElement("div");
+					
 					writer.startElement("div", this);
-					writer.writeAttribute("class", "flexListItemLeft", null);
-					writer.startElement("a", this);
-						writer.writeAttribute("href", "javascript:void(0)", null);
-						writer.writeAttribute("style", "color: #5493D4;", null);
-						writer.writeAttribute("onclick", "new Effect.toggle('show_hidden_flexlist1', 'blind', {duration:0.001}); new Effect.toggle('hide_hidden_flexlist1', 'blind', {duration:0.001}); new Effect.toggle('hidden_flexListItems1', 'blind', {duration:0.001});", null);
-						writer.startElement("span", this);
-							writer.writeAttribute("id", "show_hidden_flexlist1", null);
-							writer.write( getAttributeString("showButtonTitle") );
-						writer.endElement("span");
-						writer.startElement("span", this);
-							writer.writeAttribute("id", "hide_hidden_flexlist1", null);
-							writer.writeAttribute("style", "display:none;", null);
-							writer.write( getAttributeString("hideButtonTitle") );
-						writer.endElement("span");						
-					writer.endElement("a");
+						writer.writeAttribute("class", "flexListItemLeft", null);
+						writer.startElement("a", this);
+							writer.writeAttribute("href", "javascript:void(0)", null);
+							writer.writeAttribute("style", "color: #5493D4;", null);
+							String javascript = "" +
+							"new Effect.toggle('show_hidden_items" + this.getId() + "', 'blind', {duration:0.001}); " +
+							"new Effect.toggle('hide_hidden_items" + this.getId() + "', 'blind', {duration:0.001}); " +
+							"new Effect.toggle('hidden_items" + this.getId() + "', 'blind', {duration:0.001});";	
+							writer.writeAttribute("onclick", javascript, null);
+							
+							writer.startElement("span", this);
+								writer.writeAttribute("id", "show_hidden_items" + this.getId(), null);
+								
+								String showButtonTitle = (String)getAttributes().get("showButtonTitle");
+								if(showButtonTitle != null)
+									writer.write( showButtonTitle );
+								else
+									writer.write( "More Items...");
+							writer.endElement("span");
+							
+							writer.startElement("span", this);
+								writer.writeAttribute("id", "hide_hidden_items" + this.getId(), null);
+								writer.writeAttribute("style", "display:none;", null);
+								
+								String hideButtonTitle = (String)getAttributes().get("hideButtonTitle");
+								if(hideButtonTitle != null)
+									writer.write( hideButtonTitle );
+								else
+									writer.write("Fewer Items...");
+							writer.endElement("span");
+							
+						writer.endElement("a");
 					writer.endElement("div");
 				writer.endElement("li");
 			writer.endElement("ul");
 		writer.endElement("div");
 	}
 	public void encodeEnd(FacesContext context) throws IOException {
-	}
-	
-	public String getAttributeString(String attributeName)
-	{
-		return (String)(getAttributes().get(attributeName));
 	}
 }
