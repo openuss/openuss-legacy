3945719d81e69329b02dddffd8c13792a9171199
ronhaus <ronhaus>
2007-07-25 13:38:58 +0000

    Reverted to initial version
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1398 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/lecture/DepartmentDaoImpl.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/lecture/DepartmentDaoImpl.java
@@ -4,64 +4,77 @@
  * You can (and have to!) safely modify it by hand.
  */
 package org.openuss.lecture;
-
 /**
  * @see org.openuss.lecture.Department
- * @author Ron Haus
  */
-public class DepartmentDaoImpl extends org.openuss.lecture.DepartmentDaoBase {
-	/**
-	 * @see org.openuss.lecture.DepartmentDao#toDepartmentInfo(org.openuss.lecture.Department,
-	 *      org.openuss.lecture.DepartmentInfo)
-	 */
-	public void toDepartmentInfo(org.openuss.lecture.Department sourceEntity,
-			org.openuss.lecture.DepartmentInfo targetVO) {
-		// @todo verify behavior of toDepartmentInfo
-		super.toDepartmentInfo(sourceEntity, targetVO);
-	}
+public class DepartmentDaoImpl
+    extends org.openuss.lecture.DepartmentDaoBase
+{
+    /**
+     * @see org.openuss.lecture.DepartmentDao#toDepartmentInfo(org.openuss.lecture.Department, org.openuss.lecture.DepartmentInfo)
+     */
+    public void toDepartmentInfo(
+        org.openuss.lecture.Department sourceEntity,
+        org.openuss.lecture.DepartmentInfo targetVO)
+    {
+        // @todo verify behavior of toDepartmentInfo
+        super.toDepartmentInfo(sourceEntity, targetVO);
+    }
 
-	/**
-	 * @see org.openuss.lecture.DepartmentDao#toDepartmentInfo(org.openuss.lecture.Department)
-	 */
-	public org.openuss.lecture.DepartmentInfo toDepartmentInfo(final org.openuss.lecture.Department entity) {
-		DepartmentInfo departmentInfo = super.toDepartmentInfo(entity);
-		departmentInfo.setUniversityId(entity.getUniversity().getId());
-		return departmentInfo;
-	}
 
-	/**
-	 * Retrieves the entity object that is associated with the specified value object from the object store. If no such
-	 * entity object exists in the object store, a new, blank entity is created
-	 */
-	private org.openuss.lecture.Department loadDepartmentFromDepartmentInfo(
-			org.openuss.lecture.DepartmentInfo departmentInfo) {
+    /**
+     * @see org.openuss.lecture.DepartmentDao#toDepartmentInfo(org.openuss.lecture.Department)
+     */
+    public org.openuss.lecture.DepartmentInfo toDepartmentInfo(final org.openuss.lecture.Department entity)
+    {
+        // @todo verify behavior of toDepartmentInfo
+        return super.toDepartmentInfo(entity);
+    }
 
-		org.openuss.lecture.Department department = this.load(departmentInfo.getId());
-		if (department == null) {
-			department = org.openuss.lecture.Department.Factory.newInstance();
-		}
-		return department;
 
-	}
+    /**
+     * Retrieves the entity object that is associated with the specified value object
+     * from the object store. If no such entity object exists in the object store,
+     * a new, blank entity is created
+     */
+    private org.openuss.lecture.Department loadDepartmentFromDepartmentInfo(org.openuss.lecture.DepartmentInfo departmentInfo)
+    {
+        // @todo implement loadDepartmentFromDepartmentInfo
+        throw new java.lang.UnsupportedOperationException("org.openuss.lecture.loadDepartmentFromDepartmentInfo(org.openuss.lecture.DepartmentInfo) not yet implemented.");
 
-	/**
-	 * @see org.openuss.lecture.DepartmentDao#departmentInfoToEntity(org.openuss.lecture.DepartmentInfo)
-	 */
-	public org.openuss.lecture.Department departmentInfoToEntity(org.openuss.lecture.DepartmentInfo departmentInfo) {
-		// @todo verify behavior of departmentInfoToEntity
-		org.openuss.lecture.Department entity = this.loadDepartmentFromDepartmentInfo(departmentInfo);
-		this.departmentInfoToEntity(departmentInfo, entity, true);
-		return entity;
-	}
+        /* A typical implementation looks like this:
+        org.openuss.lecture.Department department = this.load(departmentInfo.getId());
+        if (department == null)
+        {
+            department = org.openuss.lecture.Department.Factory.newInstance();
+        }
+        return department;
+        */
+    }
 
-	/**
-	 * @see org.openuss.lecture.DepartmentDao#departmentInfoToEntity(org.openuss.lecture.DepartmentInfo,
-	 *      org.openuss.lecture.Department)
-	 */
-	public void departmentInfoToEntity(org.openuss.lecture.DepartmentInfo sourceVO,
-			org.openuss.lecture.Department targetEntity, boolean copyIfNull) {
-		// @todo verify behavior of departmentInfoToEntity
-		super.departmentInfoToEntity(sourceVO, targetEntity, copyIfNull);
-	}
+    
+    /**
+     * @see org.openuss.lecture.DepartmentDao#departmentInfoToEntity(org.openuss.lecture.DepartmentInfo)
+     */
+    public org.openuss.lecture.Department departmentInfoToEntity(org.openuss.lecture.DepartmentInfo departmentInfo)
+    {
+        // @todo verify behavior of departmentInfoToEntity
+        org.openuss.lecture.Department entity = this.loadDepartmentFromDepartmentInfo(departmentInfo);
+        this.departmentInfoToEntity(departmentInfo, entity, true);
+        return entity;
+    }
+
+
+    /**
+     * @see org.openuss.lecture.DepartmentDao#departmentInfoToEntity(org.openuss.lecture.DepartmentInfo, org.openuss.lecture.Department)
+     */
+    public void departmentInfoToEntity(
+        org.openuss.lecture.DepartmentInfo sourceVO,
+        org.openuss.lecture.Department targetEntity,
+        boolean copyIfNull)
+    {
+        // @todo verify behavior of departmentInfoToEntity
+        super.departmentInfoToEntity(sourceVO, targetEntity, copyIfNull);
+    }
 
 }
--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/lecture/UniversityDaoImpl.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/lecture/UniversityDaoImpl.java
@@ -4,60 +4,77 @@
  * You can (and have to!) safely modify it by hand.
  */
 package org.openuss.lecture;
-
 /**
  * @see org.openuss.lecture.University
- * @author Ron Haus
  */
-public class UniversityDaoImpl extends org.openuss.lecture.UniversityDaoBase {
-	/**
-	 * @see org.openuss.lecture.UniversityDao#toUniversityInfo(org.openuss.lecture.University,
-	 *      org.openuss.lecture.UniversityInfo)
-	 */
-	public void toUniversityInfo(org.openuss.lecture.University sourceEntity,
-			org.openuss.lecture.UniversityInfo targetVO) {
-		// @todo verify behavior of toUniversityInfo
-		super.toUniversityInfo(sourceEntity, targetVO);
-	}
+public class UniversityDaoImpl
+    extends org.openuss.lecture.UniversityDaoBase
+{
+    /**
+     * @see org.openuss.lecture.UniversityDao#toUniversityInfo(org.openuss.lecture.University, org.openuss.lecture.UniversityInfo)
+     */
+    public void toUniversityInfo(
+        org.openuss.lecture.University sourceEntity,
+        org.openuss.lecture.UniversityInfo targetVO)
+    {
+        // @todo verify behavior of toUniversityInfo
+        super.toUniversityInfo(sourceEntity, targetVO);
+    }
 
-	/**
-	 * @see org.openuss.lecture.UniversityDao#toUniversityInfo(org.openuss.lecture.University)
-	 */
-	public org.openuss.lecture.UniversityInfo toUniversityInfo(final org.openuss.lecture.University entity) {
-		UniversityInfo universityInfo = super.toUniversityInfo(entity);
-		return universityInfo;
-	}
 
-	/**
-	 * Retrieves the entity object that is associated with the specified value object from the object store. If no such
-	 * entity object exists in the object store, a new, blank entity is created
-	 */
-	private org.openuss.lecture.University loadUniversityFromUniversityInfo(
-			org.openuss.lecture.UniversityInfo universityInfo) {
-		
-		org.openuss.lecture.University university = this.load(universityInfo.getId());
-		if (university == null) {
-			university = org.openuss.lecture.University.Factory.newInstance();
-		} return university;
-		
-	}
+    /**
+     * @see org.openuss.lecture.UniversityDao#toUniversityInfo(org.openuss.lecture.University)
+     */
+    public org.openuss.lecture.UniversityInfo toUniversityInfo(final org.openuss.lecture.University entity)
+    {
+        // @todo verify behavior of toUniversityInfo
+        return super.toUniversityInfo(entity);
+    }
 
-	/**
-	 * @see org.openuss.lecture.UniversityDao#universityInfoToEntity(org.openuss.lecture.UniversityInfo)
-	 */
-	public org.openuss.lecture.University universityInfoToEntity(org.openuss.lecture.UniversityInfo universityInfo) {
-		org.openuss.lecture.University entity = this.loadUniversityFromUniversityInfo(universityInfo);
-		this.universityInfoToEntity(universityInfo, entity, true);
-		return entity;
-	}
 
-	/**
-	 * @see org.openuss.lecture.UniversityDao#universityInfoToEntity(org.openuss.lecture.UniversityInfo,
-	 *      org.openuss.lecture.University)
-	 */
-	public void universityInfoToEntity(org.openuss.lecture.UniversityInfo sourceVO,
-			org.openuss.lecture.University targetEntity, boolean copyIfNull) {
+    /**
+     * Retrieves the entity object that is associated with the specified value object
+     * from the object store. If no such entity object exists in the object store,
+     * a new, blank entity is created
+     */
+    private org.openuss.lecture.University loadUniversityFromUniversityInfo(org.openuss.lecture.UniversityInfo universityInfo)
+    {
+        // @todo implement loadUniversityFromUniversityInfo
+        throw new java.lang.UnsupportedOperationException("org.openuss.lecture.loadUniversityFromUniversityInfo(org.openuss.lecture.UniversityInfo) not yet implemented.");
 
-		super.universityInfoToEntity(sourceVO, targetEntity, copyIfNull);
-	}
+        /* A typical implementation looks like this:
+        org.openuss.lecture.University university = this.load(universityInfo.getId());
+        if (university == null)
+        {
+            university = org.openuss.lecture.University.Factory.newInstance();
+        }
+        return university;
+        */
+    }
+
+    
+    /**
+     * @see org.openuss.lecture.UniversityDao#universityInfoToEntity(org.openuss.lecture.UniversityInfo)
+     */
+    public org.openuss.lecture.University universityInfoToEntity(org.openuss.lecture.UniversityInfo universityInfo)
+    {
+        // @todo verify behavior of universityInfoToEntity
+        org.openuss.lecture.University entity = this.loadUniversityFromUniversityInfo(universityInfo);
+        this.universityInfoToEntity(universityInfo, entity, true);
+        return entity;
+    }
+
+
+    /**
+     * @see org.openuss.lecture.UniversityDao#universityInfoToEntity(org.openuss.lecture.UniversityInfo, org.openuss.lecture.University)
+     */
+    public void universityInfoToEntity(
+        org.openuss.lecture.UniversityInfo sourceVO,
+        org.openuss.lecture.University targetEntity,
+        boolean copyIfNull)
+    {
+        // @todo verify behavior of universityInfoToEntity
+        super.universityInfoToEntity(sourceVO, targetEntity, copyIfNull);
+    }
+
 }
