50fbfcec74859329ab1964545e3ba8c4fcf764ba
flodon <flodon>
2007-07-26 23:19:26 +0000

    Implemented all testmethods.
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1413 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/DepartmentDaoTest.java
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/DepartmentDaoTest.java
@@ -12,7 +12,7 @@ import org.openuss.security.Membership;
  * JUnit Test for Spring Hibernate DepartmentDao class.
  * 
  * @see org.openuss.lecture.DepartmentDao
- * @author Ron Haus
+ * @author Florian Dondorf
  */
 public class DepartmentDaoTest extends DepartmentDaoTestBase {
 
@@ -28,6 +28,9 @@ public class DepartmentDaoTest extends DepartmentDaoTestBase {
 
 	public void testDepartmentDaoCreate() {
 
+		//TODO: Change cardinality of departments from 0..1 to 1
+		//		Error was insert, update = false.
+		
 		//Create University
 		University university = testUtility.createDefaultUniversityWithDefaultUser();
 		flush();
@@ -38,20 +41,21 @@ public class DepartmentDaoTest extends DepartmentDaoTestBase {
 		department1.setShortcut("FB3");
 		department1.setDescription("Testdescription1");
 		department1.setDepartmentType(DepartmentType.OFFICIAL);
-		//department1.setUniversity(university);
+		department1.setUniversity(university);
 		department1.setMembership(Membership.Factory.newInstance());
+		university.getDepartments().add(department1);
 		
 		Department department2 = Department.Factory.newInstance();
 		department2.setName("Wirtschaftswissenschaften - FB 4");
 		department2.setShortcut("FB4");
 		department2.setDescription("Testdescription2");
 		department2.setDepartmentType(DepartmentType.NONOFFICIAL);
-		//department2.setUniversity(university);
+		department2.setUniversity(university);
 		department2.setMembership(Membership.Factory.newInstance());
-		
-		university.getDepartments().add(department1);
 		university.getDepartments().add(department2);
 		
+		//Association between departments and a university 
+		//must be maintained by the university. 
 		departmentDao.create(department1);
 		departmentDao.create(department2);
 		flush();
@@ -59,13 +63,59 @@ public class DepartmentDaoTest extends DepartmentDaoTestBase {
 	}
 
 	public void testDepartmentDaotoDepartmentInfo() {
-
+		
+		//Create university
+		University university = testUtility.createDefaultUniversityWithDefaultUser();
+		flush();
+		
+		//Create Entity
+		Department departmentEntity = Department.Factory.newInstance();
+		departmentEntity.setName("testname");
+		departmentEntity.setDescription("testdescription");
+		departmentEntity.setDepartmentType(DepartmentType.OFFICIAL);
+		departmentEntity.setMembership(Membership.Factory.newInstance());
+		departmentEntity.setShortcut("test");
+		departmentEntity.setUniversity(university);
+		
+		//Create Info-object
+		DepartmentInfo departmentInfo =
+			departmentDao.toDepartmentInfo(departmentEntity);
+		
+		//Test
+		assertEquals(departmentEntity.getId(), departmentInfo.getId());
+		assertEquals(departmentEntity.getName(), departmentInfo.getName());
+		assertEquals(departmentEntity.getDescription(), departmentInfo.getDescription());
+		assertEquals(departmentEntity.getDepartmentType(), departmentInfo.getDepartmentType());
+		assertEquals(departmentEntity.getShortcut(), departmentInfo.getShortcut());
+		assertEquals(departmentEntity.getUniversity().getId(), departmentInfo.getUniversityId());
 
 	}
 
 	public void testDepartmentDaoDepartmentInfoToEntity() {
 
-
+		//Create university
+		University university = testUtility.createDefaultUniversityWithDefaultUser();
+		flush();
+		
+		//Create Info-Object
+		DepartmentInfo departmentInfo = new DepartmentInfo();
+		departmentInfo.setName("testname");
+		departmentInfo.setDescription("testdescription");
+		departmentInfo.setShortcut("test");
+		departmentInfo.setDepartmentType(DepartmentType.OFFICIAL);
+		departmentInfo.setUniversityId(university.getId());
+		
+		//Create Entity
+		Department departmentEntity =
+			departmentDao.departmentInfoToEntity(departmentInfo);
+		
+		//Test
+		assertEquals(departmentInfo.getId(), departmentEntity.getId());
+		assertEquals(departmentInfo.getName(), departmentEntity.getName());
+		assertEquals(departmentInfo.getDescription(), departmentEntity.getDescription());
+		assertEquals(departmentInfo.getDepartmentType(), departmentEntity.getDepartmentType());
+		assertEquals(departmentInfo.getShortcut(), departmentEntity.getShortcut());
+		assertEquals(departmentInfo.getUniversityId(), departmentEntity.getUniversity().getId());
 
 	}
 }
