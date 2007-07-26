95e78ac2bc596d466a5f0cd8ecfedafd1e8e165b
flodon <flodon>
2007-07-26 21:07:39 +0000

    In progress.
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1409 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/DepartmentDaoTest.java
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/DepartmentDaoTest.java
@@ -5,12 +5,8 @@
  */
 package org.openuss.lecture;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.openuss.TestUtility;
 import org.openuss.security.Membership;
-import org.openuss.security.User;
 
 /**
  * JUnit Test for Spring Hibernate DepartmentDao class.
@@ -32,6 +28,34 @@ public class DepartmentDaoTest extends DepartmentDaoTestBase {
 
 	public void testDepartmentDaoCreate() {
 
+		//Create University
+		University university = testUtility.createDefaultUniversityWithDefaultUser();
+		flush();
+		
+		//Create Departments
+		Department department1 = Department.Factory.newInstance();
+		department1.setName("Rechtswissenschaften - FB 3");
+		department1.setShortcut("FB3");
+		department1.setDescription("Testdescription1");
+		department1.setDepartmentType(DepartmentType.OFFICIAL);
+		//department1.setUniversity(university);
+		department1.setMembership(Membership.Factory.newInstance());
+		
+		Department department2 = Department.Factory.newInstance();
+		department2.setName("Wirtschaftswissenschaften - FB 4");
+		department2.setShortcut("FB4");
+		department2.setDescription("Testdescription2");
+		department2.setDepartmentType(DepartmentType.NONOFFICIAL);
+		//department2.setUniversity(university);
+		department2.setMembership(Membership.Factory.newInstance());
+		
+		university.getDepartments().add(department1);
+		university.getDepartments().add(department2);
+		
+		departmentDao.create(department1);
+		departmentDao.create(department2);
+		flush();
+		
 	}
 
 	public void testDepartmentDaotoDepartmentInfo() {
