2b136fb48550bd6e448ed7bf3b6797a45e4baf1e
ronhaus <ronhaus>
2007-07-29 18:48:04 +0000

    Added tests
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1441 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/desktop/DesktopService2IntegrationTest.java
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/desktop/DesktopService2IntegrationTest.java
@@ -5,10 +5,114 @@
  */
 package org.openuss.desktop;
 
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+import org.openuss.lecture.University;
+import org.openuss.security.User;
+
 /**
  * JUnit Test for Spring Hibernate DesktopService2 class.
  * @see org.openuss.desktop.DesktopService2
+ * @author Ron Haus
  */
-public class DesktopService2IntegrationTest extends DesktopService2IntegrationTestBase {
+public class DesktopService2IntegrationTest extends DesktopService2IntegrationTestBase {	
+
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = Logger.getLogger(DesktopService2IntegrationTest.class);
 	
+	private DesktopDao desktopDao;
+	
+	public DesktopDao getDesktopDao() {
+		return desktopDao;
+	}
+
+	public void setDesktopDao(DesktopDao desktopDao) {
+		this.desktopDao = desktopDao;
+	}
+
+	private Desktop createDesktop() {
+		Desktop desktop = Desktop.Factory.newInstance();
+		desktop.setUser(testUtility.createUserInDB());
+		this.desktopDao.create(desktop);
+		return desktop;
+	}
+	
+	
+	
+	public void testFindDesktop() throws Exception {
+		logger.info("----> BEGIN access to findDesktop test");
+		
+		//Create Desktop
+		Desktop desktop = this.createDesktop();
+		
+		//Find Desktop
+		DesktopInfo desktopInfo = this.getDesktopService2().findDesktop(desktop.getId());
+		assertNotNull(desktopInfo);		
+		assertEquals(desktop.getId(), desktopInfo.getId());		
+		
+		logger.info("----> END access to findDesktop test");
+	}
+	
+	public void testFindDesktopByUser() throws Exception {
+		logger.info("----> BEGIN access to findDesktopByUser test");
+		
+		//Create Desktop
+		Desktop desktop = this.createDesktop();
+		
+		//Find Desktop
+		DesktopInfo desktopInfo = this.getDesktopService2().findDesktopByUser(desktop.getUser().getId());
+		assertNotNull(desktopInfo);		
+		assertEquals(desktop.getId(), desktopInfo.getId());		
+		
+		logger.info("----> END access to findDesktopByUser test");
+	}
+	
+	public void testCreateDesktop() throws Exception {
+		logger.info("----> BEGIN access to createDesktop test");
+		
+		//Create User
+		User user = testUtility.createUniqueUserInDB();
+		
+		//Create Desktop
+		Long desktopId = this.getDesktopService2().createDesktop(user.getId());
+		assertNotNull(desktopId);
+		
+		//Synchronize with Database
+		flush();
+		
+		//Find just created Desktop
+		Desktop desktop = this.getDesktopDao().load(desktopId);
+		assertNotNull(desktop);		
+		assertEquals(desktopId, desktop.getId());	
+		
+		logger.info("----> END access to createDesktop test");
+	}
+	
+	@SuppressWarnings( { "unchecked" })
+	public void testUpdateDesktop() throws Exception {
+		logger.info("----> BEGIN access to updateDesktop test");
+		
+		//Create Desktop
+		Desktop desktop = this.createDesktop();
+		
+		//Create DesktopInfo
+		DesktopInfo desktopInfo = this.getDesktopDao().toDesktopInfo(desktop);
+		University university = testUtility.createUniqueUniversityInDB();	
+		desktopInfo.setUniversityIds(new ArrayList());
+		desktopInfo.getUniversityIds().add(university.getId());
+		
+		//Update Desktop
+		this.getDesktopService2().updateDesktop(desktopInfo);
+		
+		//Synchronize with Database
+		flush();
+		
+		assertNotNull(desktop.getUniversities());
+		assertTrue(desktop.getUniversities().contains(university));		
+		
+		logger.info("----> END access to updateDesktop test");
+	}
 }
