39346fb24430ac24535465df8496f913340edafd
flodon <flodon>
2007-08-01 17:42:45 +0000

    Implemented testPeriodDaoCreate, testPeriodInfoToEntity and testEntityToPeriodInfo.
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1453 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/PeriodDaoTest.java
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/PeriodDaoTest.java
@@ -5,8 +5,11 @@
  */
 package org.openuss.lecture;
 
-import org.openuss.security.SecurityTestUtils;
-import org.openuss.security.User;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.openuss.TestUtility;
 import org.openuss.security.UserDao;
 
 
@@ -18,26 +21,105 @@ public class PeriodDaoTest extends PeriodDaoTestBase {
 	
 	private InstituteDao instituteDao;
 	private UserDao userDao;
+	private TestUtility testUtility;
 	
 	public void testPeriodDaoCreate() {
-		User user = SecurityTestUtils.createDefaultUser();
-		userDao.create(user);
 		
-		Institute institute = Institute.Factory.newInstance();
-		institute.setName("name");
-		institute.setOwnername("ownername");
-		institute.setOwner(user);
-		institute.setShortcut("shortcut");
-		assertNull(institute.getId());
-		instituteDao.create(institute);
-		assertNotNull(institute.getId());
+		//Create university
+		University university = testUtility.createUniqueUniversityInDB();
+		flush();
 		
+		//Create Startdate
+		Calendar cal = new GregorianCalendar();
+		cal.set(2007, 10, 1);
+		Date startdate = new Date(cal.getTimeInMillis());
+		
+		//Create Enddate
+		cal = new GregorianCalendar();
+		cal.set(2008, 3, 31);
+		Date enddate = new Date(cal.getTimeInMillis());
+		
+		//Create Period
 		Period period = new PeriodImpl();
-		period.setInstitute(institute);
-		period.setName(" ");
+		period.setUniversity(university);
+		period.setName("WS 07/08");
+		period.setDescription("A new period");
+		period.setStartdate(startdate);
+		period.setEnddate(enddate);
+		
 		assertNull(period.getId());
 		periodDao.create(period);
 		assertNotNull(period.getId());
+	}
+	
+	public void testPeriodInfoToEntity () {
+		
+		//Create university
+		University university = testUtility.createUniqueUniversityInDB();
+		flush();
+		
+		//Create Startdate
+		Calendar cal = new GregorianCalendar();
+		cal.set(2007, 10, 1);
+		Date startdate = new Date(cal.getTimeInMillis());
+		
+		//Create Enddate
+		cal = new GregorianCalendar();
+		cal.set(2008, 3, 31);
+		Date enddate = new Date(cal.getTimeInMillis());
+		
+		//Create Period
+		Period period = Period.Factory.newInstance();
+		period.setUniversity(university);
+		period.setName("WS 07/08");
+		period.setDescription("A new period");
+		period.setStartdate(startdate);
+		period.setEnddate(enddate);
+		
+		//Test
+		PeriodInfo periodInfo = this.getPeriodDao().toPeriodInfo(period);
+		assertNotNull(periodInfo);
+		assertEquals(period.getId(), periodInfo.getId());
+		assertEquals(period.getName(), periodInfo.getName());
+		assertEquals(period.getDescription(), periodInfo.getDescription());
+		assertEquals(period.getStartdate().getTime(), periodInfo.getStartdate().getTime());
+		assertEquals(period.getEnddate().getTime(), periodInfo.getEnddate().getTime());
+		assertEquals(period.getUniversity().getId(), periodInfo.getUniversityId());
+	}
+	
+	public void testEntityToPeriodInfo () {
+		
+		//Create university
+		University university = testUtility.createUniqueUniversityInDB();
+		flush();
+		
+		//Create Startdate
+		Calendar cal = new GregorianCalendar();
+		cal.set(2007, 10, 1);
+		Date startdate = new Date(cal.getTimeInMillis());
+		
+		//Create Enddate
+		cal = new GregorianCalendar();
+		cal.set(2008, 3, 31);
+		Date enddate = new Date(cal.getTimeInMillis());
+		
+		//Create PeriodInfo
+		PeriodInfo periodInfo = new PeriodInfo();
+		periodInfo.setUniversityId(university.getId());
+		periodInfo.setName("WS 07/08");
+		periodInfo.setDescription("A new period");
+		periodInfo.setStartdate(startdate);
+		periodInfo.setEnddate(enddate);
+		
+		//Test
+		Period period = this.getPeriodDao().periodInfoToEntity(periodInfo);
+		assertNotNull(period);
+		assertEquals(periodInfo.getId(), period.getId());
+		assertEquals(periodInfo.getName(), period.getName());
+		assertEquals(periodInfo.getDescription(), period.getDescription());
+		assertEquals(periodInfo.getStartdate().getTime(), period.getStartdate().getTime());
+		assertEquals(periodInfo.getEnddate().getTime(), period.getEnddate().getTime());
+		assertEquals(periodInfo.getUniversityId(), period.getUniversity().getId());
 	}
 	
 	public void testPeriodFactory() {
@@ -64,4 +146,13 @@ public class PeriodDaoTest extends PeriodDaoTestBase {
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
 	}
+
+	public TestUtility getTestUtility() {
+		return testUtility;
+	}
+
+	public void setTestUtility(TestUtility testUtility) {
+		this.testUtility = testUtility;
+	}
+	
 }
