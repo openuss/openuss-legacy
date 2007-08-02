6c04429e3c7a42fbd9323b5970f3436e62d16cbb
ronhaus <ronhaus>
2007-08-02 20:49:35 +0000

    Implemented friend methods
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1467 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/lecture/UniversityImpl.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/lecture/UniversityImpl.java
@@ -5,8 +5,11 @@
  */
 package org.openuss.lecture;
 
+import org.apache.commons.lang.Validate;
+
 /**
  * @see org.openuss.lecture.University
+ * @author Ron Haus
  */
 public class UniversityImpl
     extends org.openuss.lecture.UniversityBase
@@ -22,8 +25,15 @@ public class UniversityImpl
      */
     public void add(org.openuss.lecture.Department department)
     {
-        // @todo implement public void add(org.openuss.lecture.Department department)
-        throw new java.lang.UnsupportedOperationException("org.openuss.lecture.University.add(org.openuss.lecture.Department department) Not implemented!");
+        Validate.notNull(department, "University.add(Department) - department cannot be null");
+        
+        if (!this.getDepartments().contains(department)) {
+        	this.getDepartments().add(department);
+        	department.setUniversity(this);
+        } else {
+        	department.setUniversity(this);
+        	throw new IllegalArgumentException("University.add(Department) - the Department has already been in the List");
+        }
     }
 
     /**
@@ -31,8 +41,15 @@ public class UniversityImpl
      */
     public void remove(org.openuss.lecture.Department department)
     {
-        // @todo implement public void remove(org.openuss.lecture.Department department)
-        throw new java.lang.UnsupportedOperationException("org.openuss.lecture.University.remove(org.openuss.lecture.Department department) Not implemented!");
+    	Validate.notNull(department, "University.remove(Department) - department cannot be null");
+    	
+    	if (!this.getDepartments().remove(department)) {
+    		if (department.getUniversity().equals(this)) {
+    			department.setUniversity(null);
+    		}
+        	throw new IllegalArgumentException("University.remove(Department) - the Department has not been in the List");
+        }
+    	department.setUniversity(null);
     }
 
     /**
@@ -40,8 +57,15 @@ public class UniversityImpl
      */
     public void add(org.openuss.lecture.Period period)
     {
-        // @todo implement public void add(org.openuss.lecture.Period period)
-        throw new java.lang.UnsupportedOperationException("org.openuss.lecture.University.add(org.openuss.lecture.Period period) Not implemented!");
+        Validate.notNull(period, "University.add(Period) - period cannot be null");
+        
+        if (!this.getPeriods().contains(period)) {
+        	this.getPeriods().add(period);
+        	period.setUniversity(this);
+        } else {
+        	period.setUniversity(this);
+        	throw new IllegalArgumentException("University.add(Period) - the Period has already been in the List");
+        }
     }
 
     /**
@@ -49,8 +73,15 @@ public class UniversityImpl
      */
     public void remove(org.openuss.lecture.Period period)
     {
-        // @todo implement public void remove(org.openuss.lecture.Period period)
-        throw new java.lang.UnsupportedOperationException("org.openuss.lecture.University.remove(org.openuss.lecture.Period period) Not implemented!");
+    	Validate.notNull(period, "University.remove(Period) - period cannot be null");
+    	
+    	if (!this.getPeriods().remove(period)) {
+    		if (period.getUniversity().equals(this)) {
+    			period.setUniversity(null);
+    		}
+        	throw new IllegalArgumentException("University.remove(Period) - the Period has not been in the List");
+        }
+    	period.setUniversity(null);
     }
 
 }
