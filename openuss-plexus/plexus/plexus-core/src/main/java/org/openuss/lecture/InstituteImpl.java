e38d9f2d1cfdb53dab72d3b164b3a25645458cca
ronhaus <ronhaus>
2007-08-03 16:33:21 +0000

    Introduced method "getAllCourses" Implemented method "getActiveCourses"
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1475 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/lecture/InstituteImpl.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/lecture/InstituteImpl.java
@@ -5,10 +5,16 @@
  */
 package org.openuss.lecture;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @see org.openuss.lecture.Institute
+ * @author Ron Haus
  */
 public class InstituteImpl extends org.openuss.lecture.InstituteBase implements org.openuss.lecture.Institute {
+
 	/**
 	 * The serial version UID of this class. Needed for serialization.
 	 */
@@ -33,28 +39,38 @@ public class InstituteImpl extends org.openuss.lecture.InstituteBase implements 
 	}
 
 	/**
-	 * @see org.openuss.lecture.Institute#add(org.openuss.lecture.Period)
-	 */
-	public void add(org.openuss.lecture.Period period) {
-		// @todo implement public void add(org.openuss.lecture.Period period)
-		throw new java.lang.UnsupportedOperationException(
-				"org.openuss.lecture.Institute.add(org.openuss.lecture.Period period) Not implemented!");
-	}
-
-	/**
-	 * @see org.openuss.lecture.Institute#remove(org.openuss.lecture.Period)
-	 */
-	public void remove(org.openuss.lecture.Period period) {
-		// @todo implement public void remove(org.openuss.lecture.Period period)
-		throw new java.lang.UnsupportedOperationException(
-				"org.openuss.lecture.Institute.remove(org.openuss.lecture.Period period) Not implemented!");
-	}
-
-	/**
 	 * @see org.openuss.lecture.Institute#getActiveCourses()
 	 */
+	@SuppressWarnings( { "unchecked" })
 	public java.util.List getActiveCourses() {
-		// @todo implement public java.util.List getActiveCourses()
+		List courses = new ArrayList();
+		Iterator iterCourseTypes = this.getCourseTypes().iterator();
+		while (iterCourseTypes.hasNext()) {
+			Iterator iterCourses = ((CourseType) iterCourseTypes.next()).getCourses().iterator();
+			while (iterCourses.hasNext()) {
+				Course course = (Course) iterCourses.next();
+				if (course.isActive()) {
+					courses.add(course);
+				}
+			}
+		}
+		return null;
+
+	}
+
+	/**
+	 * @see org.openuss.lecture.Institute#getAllCourses()
+	 */
+	@SuppressWarnings( { "unchecked" })
+	public List getAllCourses() {
+		List courses = new ArrayList();
+		Iterator iterCourseTypes = this.getCourseTypes().iterator();
+		while (iterCourseTypes.hasNext()) {
+			Iterator iterCourses = ((CourseType) iterCourseTypes.next()).getCourses().iterator();
+			while (iterCourses.hasNext()) {
+				courses.add((Course) iterCourses.next());
+			}
+		}
 		return null;
 	}
 
