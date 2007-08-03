f393224a5af90615158261e568ec39bbeea4ada9
flodon <flodon>
2007-08-03 12:38:24 +0000

    bugfix in testUniqueShortcut and testCourseDaoCreate and CreateInstitute. Implemented testCourseToCourseInfo and testCourseInfoToEntity.
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1471 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/CourseDaoTest.java
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/CourseDaoTest.java
@@ -5,7 +5,11 @@
  */
 package org.openuss.lecture;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import org.openuss.TestUtility;
+import org.openuss.security.Membership;
 import org.openuss.security.User;
 import org.openuss.security.UserDao;
 import org.springframework.dao.DataAccessException;
@@ -63,6 +67,11 @@ public class CourseDaoTest extends CourseDaoTestBase {
 	
 	public void testCourseUniqueShortcut() {
 		String shortcut = testUtility.unique("shortcut");
+		
+		// create university
+		University university = testUtility.createUniqueUniversityInDB();
+		assertNotNull(university);
+		
 		// create institute
 		Institute institute = createInstitute();
 		instituteDao.create(institute);
@@ -75,14 +84,11 @@ public class CourseDaoTest extends CourseDaoTestBase {
 		assertNotNull(courseTypeDao);
 		
 		//create period
-		Period period = createPeriod();
-		period.setInstitute(institute);
-		periodDao.create(period);
-		assertNotNull(periodDao);
+		Period period = testUtility.createUniquePeriodInDB();
+		assertNotNull(period);
 		
 		// create first course
 		Course course = new CourseImpl();
-		course.setInstitute(institute);
 		course.setCourseType(courseType);
 		course.setPeriod(period);
 		course.setShortcut(shortcut);
@@ -92,9 +98,8 @@ public class CourseDaoTest extends CourseDaoTestBase {
 
 		commit();
 		
-		// create secord course
+		// create second course
 		Course course2 = new CourseImpl();
-		course2.setInstitute(institute);
 		course2.setCourseType(courseType);
 		course2.setPeriod(period);
 		course2.setShortcut(shortcut);
@@ -111,6 +116,11 @@ public class CourseDaoTest extends CourseDaoTestBase {
 	}
 	
 	public void testCourseDaoCreate() {
+		
+		// create university
+		University university = testUtility.createUniqueUniversityInDB();
+		assertNotNull(university);
+		
 		// create institute
 		Institute institute = createInstitute();
 		instituteDao.create(institute);
@@ -124,13 +134,12 @@ public class CourseDaoTest extends CourseDaoTestBase {
 		
 		//create period
 		Period period = createPeriod();
-		period.setInstitute(institute);
+		period.setUniversity(university);
 		assertNull(period.getId());
 		periodDao.create(period);
 		assertNotNull(period.getId());
 		
 		Course course = new CourseImpl();
-		course.setInstitute(institute);
 		course.setCourseType(courseType);
 		course.setPeriod(period);
 		course.setShortcut("shortcut");
@@ -139,10 +148,70 @@ public class CourseDaoTest extends CourseDaoTestBase {
 		courseDao.create(course);
 		assertNotNull(course.getId());
 	}
+	
+	public void testCourseToCourseInfo () {
+		
+		// Create Course Entity
+		Course courseEntity = testUtility.createUniqueCourseInDB();
+		assertNotNull(courseEntity);
+		
+		// Test
+		CourseInfo courseInfo = this.getCourseDao().toCourseInfo(courseEntity);
+		assertNotNull(courseInfo);
+		assertEquals(courseEntity.getName(), courseInfo.getName());
+		assertEquals(courseEntity.getDescription(), courseInfo.getDescription());
+		assertEquals(courseEntity.getAccessType(), courseInfo.getAccessType());
+		assertEquals(courseEntity.getCourseType().getId(), courseInfo.getCourseTypeId());
+		assertEquals(courseEntity.getCourseType().getDescription(), courseInfo.getCourseTypeDescription());
+		assertEquals(courseEntity.getPassword(), courseInfo.getPassword());
+		assertEquals(courseEntity.getPeriod().getId(), courseInfo.getPeriodId());
+		assertEquals(courseEntity.getPeriod().getName(), courseInfo.getPeriodName());
+		assertEquals(courseEntity.getShortcut(), courseInfo.getShortcut());
+	}
+	
+	public void testCourseInfoToEntity () {
+	
+		// Create a complete Course
+		Course course = Course.Factory.newInstance();
+		course.setShortcut(testUtility.unique("testC"));
+		course.setDescription("This is a test Course");
+		course.setAccessType(AccessType.OPEN);
+		course.setCourseType(testUtility.createUniqueCourseTypeInDB());
+		course.setPeriod(testUtility.createUniquePeriodInDB());
+		
+		courseDao.create(course);
+		assertNotNull(course.getId());
+		
+		// Create the corresponding ValueObject
+		CourseInfo courseInfo = new CourseInfo();
+		courseInfo.setId(course.getId());
+		courseInfo.setName(course.getName());
+		courseInfo.setDescription(course.getDescription());
+		courseInfo.setAccessType(course.getAccessType());
+		courseInfo.setCourseTypeId(course.getCourseType().getId());
+		courseInfo.setCourseTypeDescription(course.getCourseType().getDescription());
+		courseInfo.setPeriodId(course.getPeriod().getId());
+		courseInfo.setPeriodName(course.getPeriod().getName());
+		
+		// Test toEntity
+		Course course2 = courseDao.courseInfoToEntity(courseInfo);
+		
+		assertEquals(course2.getId(), courseInfo.getId());
+		assertEquals(course2.getName(), courseInfo.getName());
+		assertEquals(course2.getShortcut(), courseInfo.getShortcut());
+		assertEquals(course2.getDescription(), courseInfo.getDescription());
+		assertEquals(course2.getAccessType(), courseInfo.getAccessType());
+		assertEquals(course2.getCourseType().getId(), courseInfo.getCourseTypeId());
+		assertEquals(course2.getCourseType().getDescription(), courseInfo.getCourseTypeDescription());
+		assertEquals(course2.getPeriod().getId(), courseInfo.getPeriodId());
+		assertEquals(course2.getPeriod().getName(), courseInfo.getPeriodName());
+	}
 
 	private Period createPeriod() {
 		Period period = Period.Factory.newInstance();
-		period.setName("name");		
+		period.setName("name");
+		period.setStartdate(new Date(new GregorianCalendar(2007, 3, 1).getTimeInMillis()));
+		period.setEnddate(new Date(new GregorianCalendar(2007, 9, 31).getTimeInMillis()));
 		return period;
 	}
 
@@ -161,8 +230,10 @@ public class CourseDaoTest extends CourseDaoTestBase {
 		Institute institute = Institute.Factory.newInstance();
 		institute.setName("CourseDaoTest");
 		institute.setShortcut(testUtility.unique("shortcut"));
-		institute.setOwnername("ownername");
-		institute.setOwner(user);
+		institute.setOwnerName("ownername");
+		institute.setEnabled(true);
+		institute.setMembership(Membership.Factory.newInstance());
+		//institute.set(user);
 		institute.setEmail("email@institute");
 		return institute;
 	}
