79d866112fbedb7aa0a5431b2bd572051df3efce
kstettner <kstettner>
2007-08-05 20:14:07 +0000

    Merged Institute Indexer Test (Redesigned)
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1505 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/InstituteIndexerTest.java
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/InstituteIndexerTest.java
@@ -3,52 +3,84 @@ package org.openuss.lecture;
 import java.util.List;
 
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.log4j.Logger;
 import org.openuss.search.DomainResult;
 import org.openuss.security.User;
 import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 
+/**
+ * Test case for the spring indexing and searching of institutes.
+ * Tests:
+ * 	-	Normal Indexing and Searching
+ * 	- 	Updating of the index and new search
+ * Creating and Deleting of the index is testes via the Callback methods.
+ * 
+ * @author Ingo Dueppe
+ * @author Kai Stettner
+ */
 public class InstituteIndexerTest extends AbstractDependencyInjectionSpringContextTests {
 
+	private static final Logger logger = Logger.getLogger(InstituteIndexerTest.class);
+	
 	private InstituteIndexer instituteIndexer;
 	
 	private InstituteDao instituteDao = new InstituteDaoMock();
 	
 	private LectureSearcher lectureSearcher;
-							   	
+	
+	private Institute institute;
+						   	
 	@Override
 	protected void onSetUp() throws Exception {
 		super.onSetUp();
+		logger.debug("Method onSetUp: Started");
+		
 		instituteIndexer.setInstituteDao(instituteDao);
-	}
-
-	public void testIndexer() {
-		Institute institute = new LectureBuilder().createInstitute(User.Factory.newInstance()).getInstitute();
-
+		
+		institute = new LectureBuilder().createInstitute(User.Factory.newInstance()).getInstitute();
+		// set owner to common owner "Mustermann"
+		institute.setOwnername("Mustermann");
 		instituteDao.create(institute);
 		
 		instituteIndexer.setDomainObject(institute);
-		instituteIndexer.create();
 		
-		institute.setName("neuer name");
+		instituteIndexer.create();
+	}
+	
+	@Override
+	protected void onTearDown() throws Exception {
+		super.onTearDown();
+		logger.debug("Method onTearDown: Started");
+		instituteIndexer.delete();
+	}
+
+	
+	
+	public void testIndexingAndSearching() {
+		logger.debug("Method testIndexingAndSearching: Started");
+		
+		List<DomainResult> results = lectureSearcher.search("Mustermann");
+		DomainResult[] resultObjs = (DomainResult[]) results.toArray(new DomainResult[results.size()]);
+		logger.debug("--- RESULTS ---> "+ArrayUtils.toString(resultObjs));
+		logger.debug(" Result size is: "+results.size());
+		assertNotNull(results);
+		assertTrue(results.size() == 1);
+		
+	}
+	
+	public void testIndexerUpdatingAndSearching() {
+		logger.debug("Method testIndexerUpdatingAndSearching: Started");
+		// set owner to new owner "Musterfrau"
+		institute.setOwnername("Musterfrau");
 		
 		instituteIndexer.update();
 		
-		instituteIndexer.delete();
-	}
-	
-	public void testIndexingAndSearching() {
-		Institute institute = new LectureBuilder().createInstitute(User.Factory.newInstance()).getInstitute();
-		institute.setOwnerName("test owner name grob");
-		instituteDao.create(institute);
-		
-		instituteIndexer.setDomainObject(institute);
-		instituteIndexer.create();
-		
-		List<DomainResult> results = lectureSearcher.search("grob");
+		List<DomainResult> results = lectureSearcher.search("Musterfrau");
 		DomainResult[] resultObjs = (DomainResult[]) results.toArray(new DomainResult[results.size()]);
 		logger.debug("--- RESULTS ---> "+ArrayUtils.toString(resultObjs));
+		logger.debug(" Result size is: "+results.size());
 		assertNotNull(results);
-		assertTrue(results.size() >= 1);
+		assertTrue(results.size() == 1);
 		
 	}
 	
