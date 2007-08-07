627ba37093b48976412fc8d07a8cb6db15b85281
ronhaus <ronhaus>
2007-08-07 21:15:08 +0000

    Introduced CourseTypeInfo and CourseTypeService
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1527 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/lecture/CourseTypeDaoImpl.java
+++ openuss-plexus/plexus/plexus-core/src/main/java/org/openuss/lecture/CourseTypeDaoImpl.java
@@ -4,12 +4,77 @@
  * You can (and have to!) safely modify it by hand.
  */
 package org.openuss.lecture;
-
-
 /**
  * @see org.openuss.lecture.CourseType
- * @author Ingo Dueppe
  */
-public class CourseTypeDaoImpl extends org.openuss.lecture.CourseTypeDaoBase {
+public class CourseTypeDaoImpl
+    extends org.openuss.lecture.CourseTypeDaoBase
+{
+    /**
+     * @see org.openuss.lecture.CourseTypeDao#toCourseTypeInfo(org.openuss.lecture.CourseType, org.openuss.lecture.CourseTypeInfo)
+     */
+    public void toCourseTypeInfo(
+        org.openuss.lecture.CourseType sourceEntity,
+        org.openuss.lecture.CourseTypeInfo targetVO)
+    {
+        // @todo verify behavior of toCourseTypeInfo
+        super.toCourseTypeInfo(sourceEntity, targetVO);
+    }
+
+
+    /**
+     * @see org.openuss.lecture.CourseTypeDao#toCourseTypeInfo(org.openuss.lecture.CourseType)
+     */
+    public org.openuss.lecture.CourseTypeInfo toCourseTypeInfo(final org.openuss.lecture.CourseType entity)
+    {
+        // @todo verify behavior of toCourseTypeInfo
+        return super.toCourseTypeInfo(entity);
+    }
+
+
+    /**
+     * Retrieves the entity object that is associated with the specified value object
+     * from the object store. If no such entity object exists in the object store,
+     * a new, blank entity is created
+     */
+    private org.openuss.lecture.CourseType loadCourseTypeFromCourseTypeInfo(org.openuss.lecture.CourseTypeInfo courseTypeInfo)
+    {
+        // @todo implement loadCourseTypeFromCourseTypeInfo
+        throw new java.lang.UnsupportedOperationException("org.openuss.lecture.loadCourseTypeFromCourseTypeInfo(org.openuss.lecture.CourseTypeInfo) not yet implemented.");
+
+        /* A typical implementation looks like this:
+        org.openuss.lecture.CourseType courseType = this.load(courseTypeInfo.getId());
+        if (courseType == null)
+        {
+            courseType = org.openuss.lecture.CourseType.Factory.newInstance();
+        }
+        return courseType;
+        */
+    }
+
+    
+    /**
+     * @see org.openuss.lecture.CourseTypeDao#courseTypeInfoToEntity(org.openuss.lecture.CourseTypeInfo)
+     */
+    public org.openuss.lecture.CourseType courseTypeInfoToEntity(org.openuss.lecture.CourseTypeInfo courseTypeInfo)
+    {
+        // @todo verify behavior of courseTypeInfoToEntity
+        org.openuss.lecture.CourseType entity = this.loadCourseTypeFromCourseTypeInfo(courseTypeInfo);
+        this.courseTypeInfoToEntity(courseTypeInfo, entity, true);
+        return entity;
+    }
+
+
+    /**
+     * @see org.openuss.lecture.CourseTypeDao#courseTypeInfoToEntity(org.openuss.lecture.CourseTypeInfo, org.openuss.lecture.CourseType)
+     */
+    public void courseTypeInfoToEntity(
+        org.openuss.lecture.CourseTypeInfo sourceVO,
+        org.openuss.lecture.CourseType targetEntity,
+        boolean copyIfNull)
+    {
+        // @todo verify behavior of courseTypeInfoToEntity
+        super.courseTypeInfoToEntity(sourceVO, targetEntity, copyIfNull);
+    }
 
 }
---
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/CourseTypeServiceIntegrationTest.java
@@ -0,0 +1,14 @@
+// OpenUSS - Open Source University Support System
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package org.openuss.lecture;
+
+/**
+ * JUnit Test for Spring Hibernate CourseTypeService class.
+ * @see org.openuss.lecture.CourseTypeService
+ */
+public class CourseTypeServiceIntegrationTest extends CourseTypeServiceIntegrationTestBase {
+	
+}
