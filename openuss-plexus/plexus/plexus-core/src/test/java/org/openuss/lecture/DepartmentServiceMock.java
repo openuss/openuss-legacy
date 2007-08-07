3db72bd656d59c4fcab7a7a34e4055615c69cc01
m_stockmann <m_stockmann>
2007-08-07 11:49:29 +0000

    introduced Mock Object for DepartmentService (to be implemented)
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1519 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/DepartmentServiceMock.java
@@ -0,0 +1,38 @@
+package org.openuss.lecture;
+
+import java.util.List;
+
+public class DepartmentServiceMock implements DepartmentService {
+
+	public Long create(DepartmentInfo departmentInfo, Long ownerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public DepartmentInfo findDepartment(Long departmentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List findDepartmentsByUniversity(Long universityId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List findDepartmentsByUniversityAndEnabled(Long universityId,
+			Boolean enabled) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void removeDepartment(Long departmentId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void update(DepartmentInfo departmentInfo) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
