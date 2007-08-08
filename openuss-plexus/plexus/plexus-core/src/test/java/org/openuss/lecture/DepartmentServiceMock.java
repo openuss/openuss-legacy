0f669ad65cce8d184185a897025b36408d53b1bb
m_stockmann <m_stockmann>
2007-08-08 10:56:40 +0000

    added implementation of methods which are required for testing
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1531 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/DepartmentServiceMock.java
+++ openuss-plexus/plexus/plexus-core/src/test/java/org/openuss/lecture/DepartmentServiceMock.java
@@ -1,6 +1,10 @@
 package org.openuss.lecture;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class DepartmentServiceMock implements DepartmentService {
 
@@ -10,19 +14,49 @@ public class DepartmentServiceMock implements DepartmentService {
 	}
 
 	public DepartmentInfo findDepartment(Long departmentId) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.getMockDepartments().get(departmentId);
 	}
 
 	public List findDepartmentsByUniversity(Long universityId) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<Long, DepartmentInfo> allDepartments = this.getMockDepartments();
+		List departmentInfos = new ArrayList(); 
+		
+		if(universityId.equals(new Long(100))){
+			departmentInfos.add(allDepartments.get(new Long(1101)));
+			departmentInfos.add(allDepartments.get(new Long(1102)));
+			departmentInfos.add(allDepartments.get(new Long(1103)));
+		} else if(universityId.equals(new Long(101))){
+			departmentInfos.add(allDepartments.get(new Long(1201)));
+			departmentInfos.add(allDepartments.get(new Long(1201)));
+		} else if(universityId.equals(new Long(102))){
+			departmentInfos.add(allDepartments.get(new Long(1301)));
+			departmentInfos.add(allDepartments.get(new Long(1302)));
+		} else if(universityId.equals(new Long(103))){
+			departmentInfos.add(allDepartments.get(new Long(1401)));
+			departmentInfos.add(allDepartments.get(new Long(1402)));
+		} else if(universityId.equals(new Long(104))){
+			departmentInfos.add(allDepartments.get(new Long(1501)));
+			departmentInfos.add(allDepartments.get(new Long(1502)));
+		}
+		
+		return departmentInfos;
 	}
 
 	public List findDepartmentsByUniversityAndEnabled(Long universityId,
 			Boolean enabled) {
-		// TODO Auto-generated method stub
-		return null;
+		List departments = findDepartmentsByUniversity(universityId);
+		List result = new ArrayList(); 
+		DepartmentInfo tempDepartment;
+		
+		Iterator iterator = departments.iterator();
+		while(iterator.hasNext()){
+			tempDepartment = (DepartmentInfo) iterator.next();
+			if(tempDepartment.getEnabled()){
+				result.add(tempDepartment);
+			}
+		}
+		
+		return result;
 	}
 
 	public void removeDepartment(Long departmentId) {
@@ -33,6 +67,109 @@ public class DepartmentServiceMock implements DepartmentService {
 	public void update(DepartmentInfo departmentInfo) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	private Map<Long, DepartmentInfo> getMockDepartments(){
+		DepartmentInfo departmentTemp;
+		Map<Long, DepartmentInfo> departments = new HashMap<Long, DepartmentInfo>();
+		
+		// Uni 1
+		departmentTemp = new DepartmentInfo();
+		departmentTemp.setId((new Long(1101)));
+		departmentTemp.setName("Fachbereich 3 (Jura)");
+		departmentTemp.setShortcut("FB03");
+		departmentTemp.setUniversityId(new Long(100));
+		departmentTemp.setDepartmentType(org.openuss.lecture.DepartmentTypeEnum.OFFICIAL);
+		departmentTemp.setEnabled(true);
+		departments.put(departmentTemp.getId(), departmentTemp);
+		
+		departmentTemp = new DepartmentInfo();
+		departmentTemp.setId((new Long(1102)));
+		departmentTemp.setName("Fachbereich 4 (WiWi)");
+		departmentTemp.setShortcut("FB04");
+		departmentTemp.setUniversityId(new Long(100));
+		departmentTemp.setDepartmentType(org.openuss.lecture.DepartmentTypeEnum.OFFICIAL);
+		departmentTemp.setEnabled(true);
+		departments.put(departmentTemp.getId(), departmentTemp);
+		
+		departmentTemp = new DepartmentInfo();
+		departmentTemp.setId((new Long(1103)));
+		departmentTemp.setName("Lerngruppen FB 4");
+		departmentTemp.setShortcut("LG_FB04");
+		departmentTemp.setUniversityId(new Long(100));
+		departmentTemp.setDepartmentType(org.openuss.lecture.DepartmentTypeEnum.NONOFFICIAL);
+		departmentTemp.setEnabled(true);
+		departments.put(departmentTemp.getId(), departmentTemp);
+		
+		// Uni 2
+		departmentTemp = new DepartmentInfo();
+		departmentTemp.setId((new Long(1201)));
+		departmentTemp.setName("Design");
+		departmentTemp.setShortcut("Des");
+		departmentTemp.setUniversityId(new Long(101));
+		departmentTemp.setDepartmentType(org.openuss.lecture.DepartmentTypeEnum.OFFICIAL);
+		departments.put(departmentTemp.getId(), departmentTemp);
+		
+		departmentTemp = new DepartmentInfo();
+		departmentTemp.setId((new Long(1202)));
+		departmentTemp.setName("Pflege");
+		departmentTemp.setShortcut("Pfl");
+		departmentTemp.setUniversityId(new Long(101));
+		departmentTemp.setDepartmentType(org.openuss.lecture.DepartmentTypeEnum.OFFICIAL);
+		departments.put(departmentTemp.getId(), departmentTemp);
+		
+		// Uni 3
+		departmentTemp = new DepartmentInfo();
+		departmentTemp.setId((new Long(1301)));
+		departmentTemp.setName("Business");
+		departmentTemp.setShortcut("Bus");
+		departmentTemp.setUniversityId(new Long(102));
+		departmentTemp.setDepartmentType(org.openuss.lecture.DepartmentTypeEnum.OFFICIAL);
+		departments.put(departmentTemp.getId(), departmentTemp);
+		
+		departmentTemp = new DepartmentInfo();
+		departmentTemp.setId((new Long(1302)));
+		departmentTemp.setName("Law");
+		departmentTemp.setShortcut("L");
+		departmentTemp.setUniversityId(new Long(102));
+		departmentTemp.setDepartmentType(org.openuss.lecture.DepartmentTypeEnum.OFFICIAL);
+		departments.put(departmentTemp.getId(), departmentTemp);
+		
+		// Uni 4
+		departmentTemp = new DepartmentInfo();
+		departmentTemp.setId((new Long(1401)));
+		departmentTemp.setName("Fremdsprachen");
+		departmentTemp.setShortcut("FS");
+		departmentTemp.setUniversityId(new Long(103));
+		departmentTemp.setDepartmentType(org.openuss.lecture.DepartmentTypeEnum.OFFICIAL);
+		departments.put(departmentTemp.getId(), departmentTemp);
+		
+		departmentTemp = new DepartmentInfo();
+		departmentTemp.setId((new Long(1402)));
+		departmentTemp.setName("Pesonalführung");
+		departmentTemp.setShortcut("PF");
+		departmentTemp.setUniversityId(new Long(103));
+		departmentTemp.setDepartmentType(org.openuss.lecture.DepartmentTypeEnum.OFFICIAL);
+		departments.put(departmentTemp.getId(), departmentTemp);
+		
+		// Uni 5
+		departmentTemp = new DepartmentInfo();
+		departmentTemp.setId((new Long(1501)));
+		departmentTemp.setName("Fremdsprachen");
+		departmentTemp.setShortcut("FS");
+		departmentTemp.setUniversityId(new Long(104));
+		departmentTemp.setDepartmentType(org.openuss.lecture.DepartmentTypeEnum.OFFICIAL);
+		departments.put(departmentTemp.getId(), departmentTemp);
+		
+		departmentTemp = new DepartmentInfo();
+		departmentTemp.setId((new Long(1502)));
+		departmentTemp.setName("Management");
+		departmentTemp.setShortcut("Mgm");
+		departmentTemp.setUniversityId(new Long(104));
+		departmentTemp.setDepartmentType(org.openuss.lecture.DepartmentTypeEnum.OFFICIAL);
+		departments.put(departmentTemp.getId(), departmentTemp);
+		
+		return departments;
 	}
 
 }
