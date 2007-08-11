03f9d2cd29b8eca6b235e9119dff4e76c6cf384a
tianyuwang <tianyuwang>
2007-08-11 19:52:48 +0000

    konnect servicebean UniversityService  resolved
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1602 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/UniversityRegistrationController.java
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/UniversityRegistrationController.java
@@ -7,34 +7,41 @@ import org.apache.shale.tiger.view.View;
 import org.openuss.desktop.DesktopException;
 import org.openuss.lecture.LectureException;
 import org.openuss.lecture.UniversityInfo;
+import org.openuss.lecture.UniversityType;
 
 import org.openuss.web.Constants;
 
 
 @Bean(name=Constants.UNIVERSITY_REGISTRATION_CONTROLLER, scope=Scope.REQUEST)
 @View
-public class UniversityRegistrationController extends AbstractLecturePage{
+public class UniversityRegistrationController extends AbstractUniversityPage{
 
 	private static final Logger logger = Logger.getLogger(UniversityRegistrationController.class);
 	
 	public String start() {
-		/*
+		
 		logger.debug("start registration process");
 		university = new UniversityInfo();
 		setSessionBean(Constants.UNIVERSITY, university);
-		*/
+		
 		
 		return Constants.UNIVERSITY_REGISTRATION_STEP1_PAGE;
 	}
 	
 	public String registrate() throws DesktopException, LectureException {
 		
-		/*
+		
+		
+		
+		university.setOwnerName(user.getName());
+		university.setEnabled(true);
+	
+		university.setUniversityType(UniversityType.MISC);
 		universityService.create(university, user.getId());
 		//TODO send notification email
 		//FIXME this should be part of the business layer
 		//desktopService.linkUniversity(desktop.getId(), organisation.getId());
-		*/		
+				
 		return Constants.UNIVERSITY;
 	}
 	
