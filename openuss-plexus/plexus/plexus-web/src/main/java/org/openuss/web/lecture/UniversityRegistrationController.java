6d7f44b64ad01fe43eb9328cfc0b31f6f96e0096
kstettner <kstettner>
2007-08-18 20:11:43 +0000

    Added UniversityType combobox to university registration
    --> added UniversityType Converter to jsfcontrols
    
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1761 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/UniversityRegistrationController.java
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/UniversityRegistrationController.java
@@ -1,5 +1,13 @@
 package org.openuss.web.lecture;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.faces.el.ValueBinding;
+import javax.faces.model.SelectItem;
+
 import org.apache.log4j.Logger;
 import org.apache.shale.tiger.managed.Bean;
 import org.apache.shale.tiger.managed.Scope;
@@ -11,11 +19,40 @@ import org.openuss.lecture.UniversityType;
 import org.openuss.web.Constants;
 
 
+/**
+ * @author Kai Stettner
+ *
+ */
+
+
 @Bean(name=Constants.UNIVERSITY_REGISTRATION_CONTROLLER, scope=Scope.REQUEST)
 @View
 public class UniversityRegistrationController extends AbstractUniversityPage{
 
 	private static final Logger logger = Logger.getLogger(UniversityRegistrationController.class);
+	
+	private List<SelectItem> localeItems;
+	
+	private UniversityType universityType;
+	
+	public List<SelectItem> getSupportedOrganizationTypes() {
+		
+		ValueBinding binding = getFacesContext().getApplication().createValueBinding("#{visit.locale}");
+		String locale = (String)binding.getValue(getFacesContext());
+		ResourceBundle bundle = ResourceBundle.getBundle("resources", new Locale(locale));
+	
+		localeItems = new ArrayList<SelectItem>();
+	
+		SelectItem item1 = new SelectItem( universityType.UNIVERSITY, bundle.getString("organizationtype_university"));
+		SelectItem item2 = new SelectItem(universityType.COMPANY, bundle.getString("organizationtype_company"));
+		SelectItem item3 = new SelectItem(universityType.MISC, bundle.getString("organizationtype_misc"));
+		
+		localeItems.add(item1);
+		localeItems.add(item2);
+		localeItems.add(item3);
+			
+		return localeItems;
+	}
 	
 	public String start() {
 		
@@ -32,7 +69,6 @@ public class UniversityRegistrationController extends AbstractUniversityPage{
 		universityInfo.setOwnerName(user.getName());
 		universityInfo.setEnabled(true);
 	
-		universityInfo.setUniversityType(UniversityType.MISC);
 		Long universityId = universityService.createUniversity(universityInfo, user.getId());
 		universityInfo.setId(universityId);
 		//TODO send notification email
