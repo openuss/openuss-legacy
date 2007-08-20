6b35aeab9cad074217c0279134a95c7f94a497a2
m_stockmann <m_stockmann>
2007-08-20 18:23:10 +0000

    Bugfixing
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1811 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/DepartmentOptionsPage.java
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/DepartmentOptionsPage.java
@@ -1,23 +1,37 @@
 package org.openuss.web.lecture;
 
 import org.apache.shale.tiger.managed.Bean;
+import org.apache.shale.tiger.managed.Property;
 import org.apache.shale.tiger.managed.Scope;
 import org.apache.shale.tiger.view.Prerender;
 import org.apache.shale.tiger.view.View;
+import org.openuss.documents.DocumentService;
 import org.openuss.framework.jsfcontrols.breadcrumbs.BreadCrumb;
 import org.openuss.lecture.LectureException;
+import org.openuss.security.SecurityService;
 import org.openuss.web.Constants;
+import org.openuss.web.upload.UploadFileManager;
 
 /**
  * 
  * @author Kai Stettner
+ * @author Malte Stockmann
  */
 @Bean(name = "views$secured$lecture$departmentoptions", scope = Scope.REQUEST)
 @View
 public class DepartmentOptionsPage extends AbstractDepartmentPage {
 
 	private static final long serialVersionUID = -202799999652385870L;
-
+	
+	@Property (value="#{securityService}")
+	private SecurityService securityService;
+	
+	@Property (value="#{documentService}")
+	private DocumentService documentService;
+	
+	@Property(value = "#{uploadFileManager}")
+	private UploadFileManager uploadFileManager;
+	
 	@Prerender
 	public void prerender() throws LectureException {
 		super.prerender();
@@ -42,6 +56,30 @@ public class DepartmentOptionsPage extends AbstractDepartmentPage {
 		departmentService.update(departmentInfo);
 		addMessage(i18n("department_message_command_save_succeed"));
 		return Constants.SUCCESS;
+	}
+	
+	public DocumentService getDocumentService() {
+		return documentService;
+	}
+
+	public void setDocumentService(DocumentService documentService) {
+		this.documentService = documentService;
+	}
+
+	public UploadFileManager getUploadFileManager() {
+		return uploadFileManager;
+	}
+
+	public void setUploadFileManager(UploadFileManager uploadFileManager) {
+		this.uploadFileManager = uploadFileManager;
+	}
+
+	public SecurityService getSecurityService() {
+		return securityService;
+	}
+
+	public void setSecurityService(SecurityService securityService) {
+		this.securityService = securityService;
 	}
 
 }
