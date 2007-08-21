4e11580a0dbc3afe222b4f0354e89f915a4033e0
tianyuwang <tianyuwang>
2007-08-21 07:34:59 +0000

    add application process in institute registration
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1824 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/InstituteRegistrationController.java
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/InstituteRegistrationController.java
@@ -1,12 +1,22 @@
 package org.openuss.web.lecture;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.model.SelectItem;
+
 import org.apache.log4j.Logger;
 import org.apache.shale.tiger.managed.Bean;
 import org.apache.shale.tiger.managed.Scope;
 import org.apache.shale.tiger.view.View;
 import org.openuss.desktop.DesktopException;
+import org.openuss.lecture.DepartmentInfo;
+import org.openuss.lecture.DepartmentType;
 import org.openuss.lecture.InstituteInfo;
 import org.openuss.lecture.LectureException;
+import org.openuss.lecture.UniversityInfo;
 import org.openuss.web.Constants;
 
 
@@ -24,7 +34,14 @@ public class InstituteRegistrationController extends AbstractLecturePage{
 
 	private static final Logger logger = Logger.getLogger(InstituteRegistrationController.class);
 	
+	private List<SelectItem> universityItems;
+	private List<UniversityInfo> allUniversities;
+	
+	private List<SelectItem> departmentItems;
 
+	private List<DepartmentInfo> allDepartments;
+	
+	private DepartmentType departmentType;
 	public String start() {
 		logger.debug("start registration process2");
 
@@ -57,5 +74,65 @@ public class InstituteRegistrationController extends AbstractLecturePage{
 		desktopService2.linkInstitute(desktopInfo.getId(), instituteId);
 		return Constants.INSTITUTE;
 	}
+	
+	public List<SelectItem> getAllUniversities() {
+		
+		universityItems = new ArrayList<SelectItem>();
+		
+		allUniversities = universityService.findAllUniversities();
+		
+		Iterator<UniversityInfo> iter =  allUniversities.iterator();
+		UniversityInfo university;
+		
+		while (iter.hasNext()) {
+			university = iter.next();
+			SelectItem item = new SelectItem(university.getId(),university.getName());
+			universityItems.add(item);
+		}
+		
+		return universityItems;
+	}
+	
+	public List<SelectItem> getAllDepartments(){
+		departmentItems = new ArrayList<SelectItem>();
+		
+		Long universityId = universityInfo.getId();
+		allUniversities = universityService.findAllUniversities();
+		if (universityId == null)
+			universityId = allUniversities.get(0).getId();
+			
+		logger.info("universityId:"+universityInfo.getId());
+		allDepartments = departmentService.findDepartmentsByUniversity(universityId);
+		Iterator<DepartmentInfo> iter = allDepartments.iterator();
+		DepartmentInfo department;
+		
+		while (iter.hasNext()){
+			department = iter.next();
+			SelectItem item = new SelectItem(department.getId(), department.getName());
+			departmentItems.add(item);
+		}
+		
+		logger.info("DepartmentId:" + allDepartments.get(0).getId());
+		return departmentItems;
+	
+	}
+	
+
+	
+	public void processUniversitySelectChanged(ValueChangeEvent event) {
+		final Long universityId = (Long) event.getNewValue();
+		logger.info("universityId in event" + event.getNewValue().toString());
+		logger.info("universityId in processUniversitySelcetChanged:" + universityId);
+		universityInfo = universityService.findUniversity(universityId);
+		
+		setSessionBean(Constants.UNIVERSITY_INFO, universityInfo);
+		getAllDepartments();
+	}
+	
+	public void processDepartmentSelectChanged(ValueChangeEvent event) {
+		final Long departmentId = (Long) event.getNewValue();
+		departmentInfo = departmentService.findDepartment(departmentId);
+		setSessionBean(Constants.DEPARTMENT_INFO, departmentInfo);
+			}	
 	
 }
