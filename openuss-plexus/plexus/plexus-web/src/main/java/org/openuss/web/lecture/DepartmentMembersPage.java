9656b3197adcf462069273f5349adde9dad107f8
mac_chen <mac_chen>
2007-08-22 22:51:25 +0000

    add department authorisation backing bean
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1890 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/DepartmentMembersPage.java
@@ -0,0 +1,209 @@
+package org.openuss.web.lecture;
+
+
+import java.util.List;
+
+import javax.faces.event.ActionEvent;
+import org.apache.log4j.Logger;
+import org.apache.shale.tiger.managed.Bean;
+import org.apache.shale.tiger.managed.Property;
+import org.apache.shale.tiger.managed.Scope;
+import org.apache.shale.tiger.view.Preprocess;
+import org.apache.shale.tiger.view.Prerender;
+import org.apache.shale.tiger.view.View;
+import org.openuss.framework.jsfcontrols.breadcrumbs.BreadCrumb;
+import org.openuss.framework.web.jsf.model.AbstractPagedTable;
+import org.openuss.framework.web.jsf.model.DataPage;
+import org.openuss.lecture.LectureException;
+import org.openuss.lecture.OrganisationService;
+import org.openuss.security.GroupItem;
+import org.openuss.security.SecurityService;
+import org.openuss.security.UserInfo;
+import org.openuss.security.User;
+import org.openuss.web.Constants;
+
+/**
+ * Members page to define admin of a department
+ * 
+ * @author Tianyu Wang
+ * @author Weijun Chen
+ * @author Kai Stettner
+ */
+@Bean(name = "views$secured$lecture$auth$departmentmembers", scope = Scope.REQUEST)
+@View
+public class DepartmentMembersPage extends AbstractDepartmentPage {
+
+	private static final Logger logger = Logger.getLogger(DepartmentMembersPage.class);
+
+	@Property(value = "#{securityService}")
+	private SecurityService securityService;
+
+	@Property(value = "#{organisationService}")
+	private OrganisationService organisationService;
+
+	private MembersTable members = new MembersTable();
+
+	private String username;
+	
+
+	private List<GroupItem> departmentGroups;
+	
+
+	@Preprocess
+	public void preprocess() throws Exception {
+		super.preprocess();
+		getdepartmentGroups();
+		
+	}
+	@Prerender
+	public void prerender() throws LectureException {
+		super.prerender();
+		
+		addPageCrumb();
+	}
+
+	private void addPageCrumb() {
+		BreadCrumb crumb = new BreadCrumb();
+		crumb.setLink("");
+		crumb.setName(i18n("department_command_authorisations"));
+		crumb.setHint(i18n("department_command_authorisations"));
+		crumbs.add(crumb);
+		setSessionBean(Constants.BREADCRUMBS, crumbs);
+	}
+
+	public List<GroupItem> getdepartmentGroups() {
+	
+		if (departmentGroups == null) {
+			logger.debug("fetching available universtiy group informatiosn");
+			departmentGroups = organisationService.findGroupsByOrganisation(departmentInfo.getId());
+		}
+		return departmentGroups;
+	}
+	
+
+
+
+	public String removeMember() throws LectureException {
+
+		  logger.debug("remove member");
+		  
+		  UserInfo member = members.getRowData(); 
+		  logger.debug(member.getUsername());
+		  logger.debug("removeUserFromGroup");
+		  logger.debug(departmentGroups.get(0).getName());
+		  
+		  organisationService.removeUserFromGroup(member.getId(),departmentGroups.get(0).getId());
+		  logger.debug("return");
+		  return Constants.SUCCESS;
+		 
+		
+	}
+
+	/**
+	 * Lookup the user name and add the member
+	 * 
+	 * @param event
+	 * @throws LectureException
+	 */
+	public void addMember(ActionEvent event) throws LectureException {
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("add a member to university");
+		}
+		logger.debug(username);
+		user = securityService.getUserByName(username);
+		logger.debug(departmentInfo.getId());
+		logger.debug(user.getId());
+		organisationService.addMember(departmentInfo.getId(), user.getId());
+		logger.info(departmentGroups.size());
+		logger.info(departmentGroups.get(0).getId());
+		logger.debug(departmentGroups.get(0).getName());
+		organisationService.addUserToGroup(user.getId(), departmentGroups.get(0).getId());
+		
+
+	}
+
+	/**
+	 * Show profile of the selected member
+	 * 
+	 * @return outcome
+	 */
+	public String showProfile() {
+
+		UserInfo member = members.getRowData();
+		User user = securityService.getUser(member.getId());
+		setSessionBean(Constants.SHOW_USER_PROFILE, user);
+
+		return Constants.USER_PROFILE_VIEW_PAGE;
+	}
+
+	/**
+	 * @param startRow
+	 *            row to start from
+	 * @param pageSize
+	 *            number of rows on each page
+	 * @return
+	 */
+	private DataPage<UserInfo> fetchDataPage(int startRow, int pageSize) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("getDataPage(" + startRow + "," + pageSize + ")");
+		}
+		
+		 logger.info("universityId:" + departmentInfo.getId());
+		  
+		 List<UserInfo> members = organisationService.findAllMembers(departmentInfo.getId());
+		
+	
+		return new DataPage<UserInfo>(members.size(), 0, members);
+	}
+
+	/**
+	 * LocalDataModel of Universitry Members
+	 */
+	private class MembersTable extends AbstractPagedTable<UserInfo> {
+
+		private static final long serialVersionUID = 449438749521068451L;
+
+		@Override
+		public DataPage<UserInfo> getDataPage(int startRow, int pageSize) {
+			
+			return fetchDataPage(startRow, pageSize);
+		}
+	}
+
+	/* --------------------- properties -------------------------- */
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public SecurityService getSecurityService() {
+		return securityService;
+	}
+
+	public void setSecurityService(SecurityService securityService) {
+		this.securityService = securityService;
+	}
+
+	public OrganisationService getOrganisationService() {
+		return organisationService;
+	}
+
+	public void setOrganisationService(OrganisationService organisationService) {
+		this.organisationService = organisationService;
+	}
+
+	public MembersTable getMembers() {
+		return members;
+	}
+
+	public void setMembers(MembersTable members) {
+		this.members = members;
+	}
+
+
+}
