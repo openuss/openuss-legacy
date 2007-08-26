d9273802e6cd25bb760b46f71f43aecbc604daf2
julian_reimann <julian_reimann>
2007-08-26 14:57:35 +0000

    Fixing breadcrumbs for department page
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@1959 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/DepartmentPage.java
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/DepartmentPage.java
@@ -1,11 +1,12 @@
 package org.openuss.web.lecture;
 
 import org.apache.shale.tiger.managed.Bean;
+import org.apache.shale.tiger.managed.Scope;
 import org.apache.shale.tiger.view.View;
 import org.apache.shale.tiger.view.Prerender;
 import org.openuss.lecture.LectureException;
 
-@Bean(name = "views$public$department$department")
+@Bean(name = "views$public$department$department", scope = Scope.REQUEST)
 @View
 public class DepartmentPage extends AbstractDepartmentPage {
 
