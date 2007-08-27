fde3b9f5a7843fe361aaccd7c1f1fbb2fccbf18f
julian_reimann <julian_reimann>
2007-08-27 17:57:36 +0000

    Trying to fix commandLinks for shortcut/remove shortcut on departments page
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@2001 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/DepartmentPage.java
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/DepartmentPage.java
@@ -5,6 +5,7 @@ import org.apache.shale.tiger.managed.Scope;
 import org.apache.shale.tiger.view.View;
 import org.apache.shale.tiger.view.Prerender;
 import org.openuss.lecture.LectureException;
+import org.openuss.web.Constants;
 
 @Bean(name = "views$public$department$department", scope = Scope.REQUEST)
 @View
@@ -28,21 +29,31 @@ public class DepartmentPage extends AbstractDepartmentPage {
 		// TODO Ask service layer if the department is bookmarked
 	}
 	
-	public void shortcutDepartment()
+	/**
+	 * Adds a shortcut to the department
+	 * @return
+	 */
+	public String addShortcut()
 	{
 		try {
 			desktopService2.linkDepartment(desktopInfo.getId(), departmentInfo.getId());
 		} catch (Exception e) {
 			addError(i18n("DEPARTMENT_ERROR_SHORTCUT"), e.getMessage());
+			return Constants.FAILURE;
 		}
+		
+		return Constants.SUCCESS;
 	}
 	
-	public void removeShortcurDepartment()
+	public String removeShortcut()
 	{
 		try {
 			desktopService2.unlinkDepartment(desktopInfo.getId(), departmentInfo.getId());
 		} catch (Exception e) {
 			addError(i18n("department_error_remove_shortcut"), e.getMessage());
+			return Constants.FAILURE;
 		}
+		
+		return Constants.SUCCESS;
 	}
 }
