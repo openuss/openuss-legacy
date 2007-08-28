ca6200e84e6297dc81a0aaa5ca6d853db01db705
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-08-28 00:15:12 +0000

    in progress: migration
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@2016 60046477-0c10-0410-bf4b-e552a4ff1768

--- migration/migration-legacy20/src/main/java/org/openuss/migration/legacy/dao/LegacyDao.java
+++ migration/migration-legacy20/src/main/java/org/openuss/migration/legacy/dao/LegacyDao.java
@@ -1,10 +1,15 @@
 package org.openuss.migration.legacy.dao;
 
-import java.util.Collection;
+import java.util.List;
 
 import org.openuss.migration.legacy.domain.Assistant2;
+import org.openuss.migration.legacy.domain.Assistantenrollment2;
+import org.openuss.migration.legacy.domain.Assistantfaculty2;
 import org.openuss.migration.legacy.domain.Faculty2;
 import org.openuss.migration.legacy.domain.Student2;
+import org.openuss.migration.legacy.domain.Studentenrollment2;
+import org.openuss.migration.legacy.domain.Studentfaculty2;
+import org.openuss.migration.legacy.domain.Studentsubject2;
 
 /**
  * @author Ingo Dueppe
@@ -13,14 +18,24 @@ public interface LegacyDao {
 
 	public abstract Assistant2 loadAssistant(String id);
 
-	public abstract Collection<Assistant2> loadAllAssistants();
+	public abstract List<Assistant2> loadAllAssistants();
 	
 	public abstract Student2 loadStudent(String id);
 	
-	public abstract Collection<Student2> loadAllStudents();
+	public abstract List<Student2> loadAllStudents();
 	
 	public abstract Faculty2 loadFaculty(String id);
 	
-	public abstract Collection<Faculty2> loadAllInstitutes();
+	public abstract List<Faculty2> loadAllInstitutes();
+
+	public abstract List<Assistantenrollment2> loadAllAssistantEnrollments();
+
+	public abstract List<Assistantfaculty2> loadAllAssistantFaculty();
+
+	public abstract List<Studentenrollment2> loadAllStudentEnrollments();
+
+	public abstract List<Studentfaculty2> loadAllStudentFaculty();
+
+	public abstract List<Studentsubject2> loadAllStudentSubject();
 
 }
