62e7feff4d7c2075c6f1aa70de03ac0436edc88b
m_stockmann <m_stockmann>
2007-08-30 13:55:48 +0000

    added logo upload functionality for institutes
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@2091 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/InstituteOptionsPage.java
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/InstituteOptionsPage.java
@@ -1,17 +1,32 @@
 package org.openuss.web.lecture;
 
+import java.io.IOException;
+
+import javax.faces.event.ActionEvent;
+
 import org.apache.shale.tiger.managed.Bean;
+import org.apache.shale.tiger.managed.Property;
 import org.apache.shale.tiger.managed.Scope;
 import org.apache.shale.tiger.view.Prerender;
 import org.apache.shale.tiger.view.View;
+import org.openuss.documents.DocumentApplicationException;
+import org.openuss.documents.DocumentService;
+import org.openuss.documents.FileInfo;
+import org.openuss.documents.FolderInfo;
 import org.openuss.framework.jsfcontrols.breadcrumbs.BreadCrumb;
 import org.openuss.lecture.LectureException;
+import org.openuss.security.Roles;
+import org.openuss.security.SecurityService;
+import org.openuss.security.acl.LectureAclEntry;
 import org.openuss.web.Constants;
+import org.openuss.web.upload.UploadFileManager;
+import org.openuss.web.upload.UploadedDocument;
 
 /**
  * 
  * @author Ingo Dueppe
  * @author Kai Stettner
+ * @author Malte Stockmann
  */
 @Bean(name = "views$secured$lecture$instituteoptions", scope = Scope.REQUEST)
 @View
@@ -19,6 +34,15 @@ public class InstituteOptionsPage extends AbstractLecturePage {
 
 	private static final long serialVersionUID = -202776319652385870L;
 
+	@Property (value="#{securityService}")
+	private SecurityService securityService;
+	
+	@Property (value="#{documentService}")
+	private DocumentService documentService;
+	
+	@Property(value = "#{uploadFileManager}")
+	private UploadFileManager uploadFileManager;
+	
 	@Prerender
 	public void prerender() throws LectureException {
 		super.prerender();
@@ -43,11 +67,78 @@ public class InstituteOptionsPage extends AbstractLecturePage {
 	 * Save institute options.
 	 * @return outcome
 	 * @throws LectureException
+	 * @throws DocumentApplicationException 
+	 * @throws IOException 
 	 */
-	public String saveInstitute() throws LectureException {
+	public String saveInstitute() throws LectureException, DocumentApplicationException, IOException {
+		// fetch uploaded files and remove it from upload manager
+		UploadedDocument uploaded = (UploadedDocument) getSessionBean(Constants.UPLOADED_FILE);
+		if (uploaded != null) {
+			if (instituteInfo.getImageId() != null) {
+				documentService.removeFolderEntry(instituteInfo.getImageId());
+			}
+			FileInfo imageFile = new FileInfo();
+				
+			imageFile.setName(Constants.ORGANISATION_IMAGE_NAME);
+			imageFile.setFileName(uploaded.getFileName());
+			imageFile.setContentType(uploaded.getContentType());
+			imageFile.setFileSize(uploaded.getFileSize());
+			imageFile.setInputStream(uploaded.getInputStream());
+			
+			FolderInfo folder = documentService.getFolder(instituteInfo);
+			documentService.createFileEntry(imageFile, folder);
+			
+			permitRolesImageReadPermission(imageFile);
+			
+			instituteInfo.setImageId(imageFile.getId());
+
+			removeSessionBean(Constants.UPLOADED_FILE);
+			uploadFileManager.removeDocument(uploaded);
+		}
+		
 		instituteService.update(instituteInfo);
 		addMessage(i18n("institute_message_command_save_succeed"));
 		return Constants.SUCCESS;
 	}
+	
+	private void permitRolesImageReadPermission(FileInfo imageFile) {
+		// TODO should be done within the business layer
+		securityService.setPermissions(Roles.ANONYMOUS, imageFile, LectureAclEntry.READ);
+		securityService.setPermissions(Roles.USER, imageFile, LectureAclEntry.READ);
+	}
+	
+	public void removeImage(ActionEvent event) throws DocumentApplicationException {
+		if (instituteInfo.getImageId() != null) {
+			Long fileId = instituteInfo.getImageId();
+			instituteInfo.setImageId(null);
+			documentService.removeFolderEntry(fileId);
+		}
+		instituteService.update(instituteInfo);
+		setSessionBean(Constants.LAST_VIEW, Constants.USER_PROFILE_VIEW_PAGE);
+	}
 
+	public SecurityService getSecurityService() {
+		return securityService;
+	}
+
+	public void setSecurityService(SecurityService securityService) {
+		this.securityService = securityService;
+	}
+
+	public DocumentService getDocumentService() {
+		return documentService;
+	}
+
+	public void setDocumentService(DocumentService documentService) {
+		this.documentService = documentService;
+	}
+
+	public UploadFileManager getUploadFileManager() {
+		return uploadFileManager;
+	}
+
+	public void setUploadFileManager(UploadFileManager uploadFileManager) {
+		this.uploadFileManager = uploadFileManager;
+	}
+	
 }
