5fb9eace6b3214d313813cdd3b2c73b5cc865cd6
tianyuwang <tianyuwang>
2007-09-06 14:12:32 +0000

    add backing bean for departmentinstiutes.xhtml
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/solarplexus@2337 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ openuss-plexus/plexus/plexus-web/src/main/java/org/openuss/web/lecture/DepartmentInstitutesPage.java
@@ -0,0 +1,51 @@
+package org.openuss.web.lecture;
+
+import java.util.List;
+import java.util.ArrayList;
+import org.apache.shale.tiger.managed.Bean;
+import org.apache.shale.tiger.managed.Scope;
+import org.apache.shale.tiger.view.Prerender;
+import org.apache.shale.tiger.view.View;
+import org.openuss.framework.jsfcontrols.breadcrumbs.BreadCrumb;
+import org.openuss.framework.web.jsf.model.AbstractPagedTable;
+import org.openuss.framework.web.jsf.model.DataPage;
+import org.openuss.lecture.DepartmentInfo;
+import org.openuss.lecture.InstituteInfo;
+import org.openuss.lecture.LectureException;
+import org.openuss.web.Constants;
+import org.openuss.lecture.ApplicationInfo;
+
+
+/**
+ * 
+ * @author Kai Stettner
+ */
+@Bean(name = "views$secured$lecture$departmentinstitutes", scope = Scope.REQUEST)
+@View
+public class DepartmentInstitutesPage extends AbstractDepartmentPage {
+	
+	private static final long serialVersionUID = -202786789652385870L;
+	
+
+	@Prerender
+	public void prerender() throws LectureException {
+		super.prerender();
+		addPageCrumb();
+	}
+	
+	private void addPageCrumb() {
+		BreadCrumb crumb = new BreadCrumb();
+		crumb.setLink("");
+		crumb.setName(i18n("department_command_institutes"));
+		crumb.setHint(i18n("department_command_institutes"));
+		
+		breadcrumbs.loadDepartmentCrumbs(departmentInfo);
+		breadcrumbs.addCrumb(crumb);
+		
+		// TODO Remove old crumb code
+		crumbs.add(crumb);
+		setSessionBean(Constants.BREADCRUMBS, crumbs);
+	}	
+	
+
+}
