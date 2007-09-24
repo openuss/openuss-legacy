aefaa3242923f9d393e7036c762c85d59937fc83
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-09-24 12:36:53 +0000

    fixed http://teamopenuss.uni-muenster.de/jira/browse/USS-789
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@2586 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/lecture/validators/CalendarDateValidator.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/lecture/validators/CalendarDateValidator.java
@@ -2,7 +2,6 @@ package org.openuss.web.lecture.validators;
 
 import java.util.Date;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.component.UIOutput;
@@ -20,39 +19,37 @@ import org.openuss.lecture.UniversityServiceException;
  * 
  * @author Kai Stettner
  */
-@FacesValidator(value="calendarDateValidator")
+@FacesValidator(value = "calendarDateValidator")
 public class CalendarDateValidator extends BaseBean implements Validator {
-	
+
 	private static final Logger logger = Logger.getLogger(UniversityShortcutValidator.class);
 
 	public static final String SHORTCUT_MESSAGE_ID = "error_shortcut_already_exists";
 
 	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
 		try {
-			
-			Date startDate = (Date)((UIOutput) component.findComponent("startdate")).getValue();
-			Date endDate = (Date)value;
-			
-			if(startDate != null)
-			{
-				if(endDate == null)
-				{
+			// FIXME startdate should be configurable through validator tag attribute 
+			Date startDate = (Date) ((UIOutput) component.getParent().findComponent("startdate")).getValue();
+			Date endDate = (Date) value;
+
+			if (startDate != null) {
+				if (endDate == null) {
 					((UIInput) component).setValid(false);
-					context.addMessage(null, new FacesMessage("Das Enddatum darf nicht null sein.") );
+					addError(component.getClientId(context), "Enddate", i18n("validate_error_need_to_define_end_date"));
 				}
-					
+
 				logger.debug(startDate.getClass().toString());
 				logger.debug(endDate.getClass().toString());
 				logger.debug(startDate);
 				logger.debug(endDate);
 				logger.debug(startDate.after(endDate));
-				
+
 				if (startDate.after(endDate)) {
 					((UIInput) component).setValid(false);
-					context.addMessage(null, new FacesMessage("Das Enddatum muss nach dem Startdatum liegen.") );
+					addError(component.getClientId(context), "Enddate", i18n("validate_error_end_date_after_start_date"));
 				}
 			}
-			
+
 		} catch (UniversityServiceException e) {
 			logger.error(e);
 			((UIInput) component).setValid(false);
