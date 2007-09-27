8f52194fb9cc28c4a9d063d0a6c51f4f5be28ed7
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-09-27 13:44:39 +0000

    code polishing and bug fixing.
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@2600 60046477-0c10-0410-bf4b-e552a4ff1768

--- openuss-plexus/framework/jsfcontrols/src/main/java/org/openuss/framework/jsfcontrols/components/flexlist/ListeDAO.java
+++
@@ -1,7 +0,0 @@
-package org.openuss.framework.jsfcontrols.components.flexlist;
-
-public class ListeDAO {
-	private String title;
-	private String hideButtonTitle;
-	private String showButtonTitle;
-}
