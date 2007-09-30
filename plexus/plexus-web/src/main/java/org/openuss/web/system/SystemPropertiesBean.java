7188c5160eea11cda85dcf91e0bb748540f7f6cc
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-09-30 10:59:26 +0000

    bugfixing and code polishing
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@2623 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/system/SystemPropertiesBean.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/system/SystemPropertiesBean.java
@@ -7,16 +7,37 @@ import org.openuss.system.SystemProperties;
 import org.openuss.system.SystemService;
 import org.openuss.web.Constants;
 
+/**
+ * @author Ingo Dueppe
+ *
+ */
 @Bean(name = Constants.SYSTEM_PROPERTIES, scope = Scope.REQUEST)
 public class SystemPropertiesBean {
 	
 	@Property(value="#{systemService}")
 	private SystemService systemService;
 	
-	public String getCommunityCourseId() {
-		return systemService.getProperty(SystemProperties.COMMUNITY_COURSE_ID).getValue();
+	
+	public String getDOCUMENTATION_URL() {
+		return systemService.getProperty(SystemProperties.DOCUMENTATION_URL).getValue();
+	}
+
+	public String getSUPPORT_URL() {
+		return systemService.getProperty(SystemProperties.SUPPORT_URL).getValue();
+	}
+
+	public String getBUGTRACKING_URL() {
+		return systemService.getProperty(SystemProperties.BUGTRACKING_URL).getValue();
+	}
+
+	public String getIMPRESSUM_TEXT() {
+		return systemService.getProperty(SystemProperties.IMPRESSUM_TEXT).getValue();
 	}
 	
+	public String getPROVIDER_URL() {
+		return systemService.getProperty(SystemProperties.PROVIDER_URL).getValue();
+	}
+
 	public void setSystemService(SystemService systemService) {
 		this.systemService = systemService;
 	}
