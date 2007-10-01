17c3e8c42a7e704c10a1c6281205ab3bdfac13e5
Ingo Düppe <ingo.dueppe@crowdcode.io>
2007-10-01 10:06:47 +0000

    bugfixing
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@2634 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/course/CourseMainPage.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/course/CourseMainPage.java
@@ -1,4 +1,4 @@
-package org.openuss.web.course; 
+package org.openuss.web.course;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,22 +13,28 @@ import org.apache.shale.tiger.managed.Bean;
 import org.apache.shale.tiger.managed.Scope;
 import org.apache.shale.tiger.view.Prerender;
 import org.apache.shale.tiger.view.View;
+import org.openuss.desktop.DesktopException;
 import org.openuss.framework.jsfcontrols.breadcrumbs.BreadCrumb;
 import org.openuss.lecture.CourseApplicationException;
 import org.openuss.lecture.CourseMemberInfo;
 import org.openuss.lecture.CourseMemberType;
 import org.openuss.web.Constants;
 
+/**
+ * 
+ * @author Ingo Dueppe
+ * 
+ */
 @Bean(name = "views$secured$course$main", scope = Scope.REQUEST)
 @View
-public class CourseMainPage extends AbstractCoursePage{
-	
+public class CourseMainPage extends AbstractCoursePage {
+
 	private static final Logger logger = Logger.getLogger(CourseMainPage.class);
-	
-	private String password; 
+
+	private String password;
 
 	private List<CourseMemberInfo> assistants = new ArrayList<CourseMemberInfo>();
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	@Prerender
@@ -37,13 +43,13 @@ public class CourseMainPage extends AbstractCoursePage{
 		if (courseInfo != null) {
 			assistants = courseService.getAssistants(courseInfo);
 		}
-	
+
 		BreadCrumb newCrumb = new BreadCrumb();
 		newCrumb.setName(i18n("course_command_overview"));
 		newCrumb.setHint(i18n("course_command_overview"));
 		breadcrumbs.addCrumb(newCrumb);
 	}
-	
+
 	public void validatePassword(FacesContext context, UIComponent toValidate, Object value) {
 		String password = (String) value;
 		if (!StringUtils.equalsIgnoreCase(password, courseInfo.getPassword())) {
@@ -51,24 +57,62 @@ public class CourseMainPage extends AbstractCoursePage{
 			addError(toValidate.getClientId(context), i18n("message_error_password_is_not_correct"), null);
 		}
 	}
-	
-	public String applyWithPassword() throws CourseApplicationException{
+
+	public String applyWithPassword() throws CourseApplicationException {
 		logger.debug("course entry with password applied");
 		courseService.applyUserByPassword(password, courseInfo, user);
 		addMessage(i18n("message_course_password_accepted"));
 		return Constants.SUCCESS;
 	}
-	
-	public String apply() throws CourseApplicationException{
+
+	public String apply() throws CourseApplicationException {
 		logger.debug("course entry applied");
 		courseService.applyUser(courseInfo, user);
 		addMessage(i18n("message_course_send_application"));
 		return Constants.SUCCESS;
 	}
-	
+
 	public boolean isAspirant() {
 		CourseMemberInfo info = courseService.getMemberInfo(courseInfo, user);
 		return (info != null) && (info.getMemberType() == CourseMemberType.ASPIRANT);
+	}
+
+	public Boolean getBookmarked() {
+		try {
+			return desktopService2.isCourseBookmarked(courseInfo.getId(), user.getId());
+		} catch (Exception e) {
+			logger.error(e);
+			return false;
+		}
+	}
+	
+	/**
+	 * Bookmarks the selected course on the MyUni Page.
+	 * @return Outcome
+	 */
+	public String shortcutCourse() {
+		//courseInfo = courseData.getRowData();
+		try {
+			desktopService2.linkCourse(desktopInfo.getId(), courseInfo.getId());
+			addMessage(i18n("desktop_command_add_course_succeed"));
+			return Constants.SUCCESS;
+		} catch (DesktopException e) {
+			logger.error(e);
+			addError(i18n(e.getMessage()));
+			return Constants.FAILURE;
+		}
+	}
+
+	public String removeCourseShortcut() {
+		try {
+			desktopService2.unlinkCourse(desktopInfo.getId(), courseInfo.getId());
+		} catch (Exception e) {
+			addError(i18n("institute_error_remove_shortcut"), e.getMessage());
+			return Constants.FAILURE;
+		}
+		
+		addMessage(i18n("institute_success_remove_shortcut"));
+		return Constants.SUCCESS;
 	}
 
 	public String getPassword() {
@@ -82,6 +126,5 @@ public class CourseMainPage extends AbstractCoursePage{
 	public List<CourseMemberInfo> getAssistants() {
 		return assistants;
 	}
-
 
 }
