c6bfbf7aa80947ab591acd8083741417a5c31a76
tobi_brockmann <tobi_brockmann>
2007-11-28 15:36:57 +0000

    New: Discussion Search
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-diplom-ws0708@2827 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-api/src/main/java/org/openuss/discussion/DiscussionSearcher.java
@@ -0,0 +1,19 @@
+package org.openuss.discussion;
+
+import java.util.List;
+
+import org.openuss.search.DiscussionSearchDomainResult;
+
+/**
+ * Discussion Search Interface
+ *  
+ * @author Thomas Jansing
+ * @author Juergen de Braaf
+ * @author Peter Schuh
+ * @author Tobias Brockmann 
+ */
+public interface DiscussionSearcher {
+	
+	public List<DiscussionSearchDomainResult> search(String textToSearch, Long courseId, boolean onlyInTitle);
+
+}
