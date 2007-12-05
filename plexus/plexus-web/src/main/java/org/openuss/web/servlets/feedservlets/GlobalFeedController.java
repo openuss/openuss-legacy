c4359a87bb33a3384571ebd0b6ecf8818e76fe54
max-b <max-b>
2007-12-05 21:00:51 +0000

    Code of WarmUp-Task: RSS for (global) OpenUSS-News
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-bachelor-ws0708@2871 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/servlets/feedservlets/GlobalFeedController.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/servlets/feedservlets/GlobalFeedController.java
@@ -1,42 +1,61 @@
 package org.openuss.web.servlets.feedservlets;
 
 import java.text.DateFormat;
-import java.text.ParseException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.text.ParseException;
+
 import org.apache.commons.lang.time.DateFormatUtils;
 import org.apache.log4j.Logger;
-import org.openuss.framework.web.jsf.util.AcegiUtils;
-import org.openuss.security.acl.LectureAclEntry;
 import org.openuss.web.feeds.FeedWrapper;
+import org.openuss.web.feeds.GlobalFeed;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
-public abstract class AbstractFeedServlet{
+/**
+ * 
+ * @author Ingo Dueppe
+ * @author Sebastian Roekens
+ */
+public class GlobalFeedController extends AbstractFeedServlet implements Controller{
 
+	private GlobalFeed globalFeed;
+	
 	private static final Logger logger = Logger.getLogger(AbstractFeedServlet.class);
 	
-	public static final String IF_MODIFIED_SINCE = "If-Modified-Since";
+	public GlobalFeed getGlobalFeed() {
+		return globalFeed;
+	}
 
-	public static final String APPLICATION_RSS_XML = "application/rss+xml";
+	public void setGlobalFeed(GlobalFeed globalFeed) {
+		this.globalFeed = globalFeed;
+	}
 
-	public static final String LAST_MODIFIED = "Last-Modified";
-
-	public static final String DATE_FORMAT = "EEE, dd MMM yyyy hh:mm:ss zzz";
 	
+	protected FeedWrapper getFeedWrapper() {
+		return globalFeed.getFeed();
+	}
+
+	@Override
+	protected String domainParameterName() {
+		return "global";
+	}
+
+	@Override
+	public boolean hasPermissions(Long domainIdentifier) {
+		return true; // anonymous access allowed
+	}
+	
+	@Override
 	public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		Long domainIdentifier = Long.parseLong(req.getParameter(domainParameterName()));
+		
 		String modifiedSince = req.getParameter(IF_MODIFIED_SINCE);
 
-		if (domainIdentifier != null) {
-			if (!hasPermissions(domainIdentifier)) {
-				res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-				return null;
-			}
 		
 
-			FeedWrapper feedWrapper = getFeedWrapper(domainIdentifier);
+			FeedWrapper feedWrapper = getFeedWrapper();
 
 			if (feedWrapper == null) {
 				res.sendError(HttpServletResponse.SC_NOT_FOUND);
@@ -63,32 +82,11 @@ public abstract class AbstractFeedServlet{
 			}
 			return null;
 		}
-		res.sendError(HttpServletResponse.SC_NOT_FOUND);
+
+	@Override
+	protected FeedWrapper getFeedWrapper(Long domainId) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
-	
-	/**
-	 * Hook method to define the domain object request parameter name
-	 * @return name of the parameter
-	 */
-	protected abstract String domainParameterName();
-
-	/**
-	 * Hook method to retrieve the feed wrapper
-	 * @param domainId - unique domain identifier
-	 * @return
-	 */
-	protected abstract FeedWrapper getFeedWrapper(Long domainId);
-	
-
-	/**
-	 * Has the current user the permission to view the rss feed?
-	 * @param domainIdentifier
-	 * @return true - user may access the rss feed
-	 */
-	public boolean hasPermissions(Long domainIdentifier){
-		return AcegiUtils.hasPermission(domainIdentifier, new Integer[] { LectureAclEntry.READ });
-	}
-	
 }
