ca8f7888c1a238d66005d7a8aca22c6b545a9c53
chbeer <chbeer>
2008-01-22 10:28:52 +0000

    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-bachelor-kollaboration@2964 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/course/papersubmission/SubmissionSelection.java
+++
@@ -1,9 +0,0 @@
-package org.openuss.web.papersubmission;
-
-import org.openuss.web.documents.Selection;
-
-public class SubmissionSelection  extends Selection<SubmissionInfo> {
-
-	private static final long serialVersionUID = -6223370414221361756L;
-
-}
