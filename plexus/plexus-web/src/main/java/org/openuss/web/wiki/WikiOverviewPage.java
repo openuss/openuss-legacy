72d0257b2c91a721a0916bf4c20866f606754427
dyck <dyck>
2008-02-01 10:17:57 +0000

    Implemented runnable version of WikiOverviewPage.
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-bachelor-kollaboration@3071 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/wiki/WikiOverviewPage.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/wiki/WikiOverviewPage.java
@@ -3,14 +3,19 @@ package org.openuss.web.wiki;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.apache.shale.tiger.managed.Bean;
 import org.apache.shale.tiger.managed.Property;
+import org.apache.shale.tiger.managed.Scope;
 import org.apache.shale.tiger.view.Prerender;
+import org.apache.shale.tiger.view.View;
 import org.openuss.framework.web.jsf.model.AbstractPagedTable;
 import org.openuss.framework.web.jsf.model.DataPage;
 import org.openuss.web.course.AbstractCoursePage;
 import org.openuss.wiki.WikiService;
 import org.openuss.wiki.WikiSiteInfo;
 
+@Bean(name = "views$secured$wiki$wikioverview", scope = Scope.REQUEST)
+@View
 public class WikiOverviewPage extends AbstractWikiPage {
 	
 	private static final Logger LOGGER = Logger.getLogger(WikiEditLinksPage.class);
