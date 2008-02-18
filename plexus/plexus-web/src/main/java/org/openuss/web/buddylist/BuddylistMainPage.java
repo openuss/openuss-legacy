219e75b2b0bd36ecfa5296462476471814073e6c
ralf-p <ralf-p>
2008-02-18 11:26:26 +0000

    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-diplom-ws0708@3205 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/buddylist/BuddylistMainPage.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/buddylist/BuddylistMainPage.java
@@ -10,15 +10,18 @@ import javax.faces.context.FacesContext;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.shale.tiger.managed.Bean;
+import org.apache.shale.tiger.managed.Property;
 import org.apache.shale.tiger.managed.Scope;
 import org.apache.shale.tiger.view.Prerender;
 import org.apache.shale.tiger.view.View;
+import org.openuss.buddylist.*;
 import org.openuss.desktop.DesktopException;
 import org.openuss.framework.jsfcontrols.breadcrumbs.BreadCrumb;
 import org.openuss.groups.GroupApplicationException;
 import org.openuss.lecture.CourseApplicationException;
 import org.openuss.lecture.CourseMemberInfo;
 import org.openuss.lecture.CourseMemberType;
+import org.openuss.news.NewsService;
 import org.openuss.web.Constants;
 import org.openuss.web.BreadCrumbs;
 import org.openuss.web.BasePage;
@@ -26,13 +29,17 @@ import org.openuss.web.BasePage;
 /**
  * 
  * @author Thomas Jansing
+ * @author Ralf Plattfaut
  *
  */
-@Bean(name = "views$secured$openuss4us$buddylist", scope = Scope.REQUEST)
+@Bean(name = "views$secured$buddylist$buddylist", scope = Scope.REQUEST)
 @View
 public class BuddylistMainPage extends BasePage {
 	
 	private static final Logger logger = Logger.getLogger(BuddylistMainPage.class);
+	
+	@Property(value = "#{buddyService}")
+	private BuddyService buddyService;
 	
 	@Prerender
 	public void prerender() throws Exception {	
@@ -48,5 +55,14 @@ public class BuddylistMainPage extends BasePage {
 		breadcrumbs.loadOpenuss4usCrumbs();
 		breadcrumbs.addCrumb(crumb);
 	}	
+	
+	public List<BuddyInfo> getBuddys(){
+		logger.debug("read buddys");
+		return buddyService.getBuddyList();
+	}
+
+	public void setBuddyService(BuddyService buddyService) {
+		this.buddyService = buddyService;
+	}
 	
 }
