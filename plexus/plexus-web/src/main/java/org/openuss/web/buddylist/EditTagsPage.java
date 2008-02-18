9dbdac0cf7decb41d17ca6aa7a9ce56f0e72e98b
ralf-p <ralf-p>
2008-02-18 18:26:07 +0000

    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-diplom-ws0708@3215 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-web/src/main/java/org/openuss/web/buddylist/EditTagsPage.java
@@ -0,0 +1,99 @@
+package org.openuss.web.buddylist;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.log4j.Logger;
+import org.apache.shale.tiger.managed.Bean;
+import org.apache.shale.tiger.managed.Property;
+import org.apache.shale.tiger.managed.Scope;
+import org.apache.shale.tiger.view.Prerender;
+import org.apache.shale.tiger.view.View;
+import org.openuss.buddylist.*;
+import org.openuss.framework.jsfcontrols.breadcrumbs.BreadCrumb;
+import org.openuss.framework.web.jsf.model.AbstractPagedTable;
+import org.openuss.framework.web.jsf.model.DataPage;
+import org.openuss.security.User;
+import org.openuss.web.Constants;
+import org.openuss.web.BasePage;
+
+/**
+ * 
+ * @author Thomas Jansing
+ * @author Ralf Plattfaut
+ *
+ */
+@Bean(name = "views$secured$buddylist$edittags", scope = Scope.REQUEST)
+@View
+public class EditTagsPage extends BasePage {
+	@Property(value= "#{"+Constants.SHOW_USER_PROFILE+"}")
+	public User profile;
+	
+	@Property(value= "#{newTag}")
+	private String newTag;
+	
+	private static final Logger logger = Logger.getLogger(EditTagsPage.class);
+	
+	@Property(value = "#{buddyService}")
+	private BuddyService buddyService;
+	
+	@Property(value = "#{buddyInfo}")
+	private BuddyInfo buddyInfo;
+	
+	public BuddyInfo getBuddyInfo() {
+		return buddyInfo;
+	}
+
+	public void setBuddyInfo(BuddyInfo buddyInfo) {
+		this.buddyInfo = buddyInfo;
+	}
+
+	@Prerender
+	public void prerender() throws Exception {	
+		super.prerender();
+		buddyInfo = (BuddyInfo)getSessionAttribute(Constants.OPENUSS4US_EDITTAGS);
+		addPageCrumb();
+	}
+
+	private void addPageCrumb() {
+		BreadCrumb crumb = new BreadCrumb();
+		crumb.setLink("");
+		crumb.setName(i18n("openuss4us_command_buddylist"));
+		crumb.setHint(i18n("openuss4us_command_buddylist"));
+		breadcrumbs.loadOpenuss4usCrumbs();
+		breadcrumbs.addCrumb(crumb);
+	}	
+	
+	public String addTag(){
+		buddyService.addTag(buddyInfo, newTag);
+		return Constants.OPENUSS4US_CALENDAR;
+	}
+	
+	public String linkProfile(){
+		logger.debug("started");
+		profile.setId(profile.getId());
+		logger.debug("loading user profile: " + profile.getId());
+		setSessionAttribute(Constants.SHOW_USER_PROFILE, profile);
+		return Constants.USER_PROFILE_VIEW_PAGE;
+	}
+
+	public void setBuddyService(BuddyService buddyService) {
+		this.buddyService = buddyService;
+	}
+
+	public User getProfile() {
+		return profile;
+	}
+
+	public void setProfile(User profile) {
+		this.profile = profile;
+	}
+
+	public String getNewTag() {
+		return newTag;
+	}
+
+	public void setNewTag(String newTag) {
+		this.newTag = newTag;
+	}
+	
+}
