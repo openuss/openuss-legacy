ae69f5f76c1f51f608942bab7714c1aeb50ff9c4
SirMythos <SirMythos>
2008-02-26 13:40:14 +0000

    fixed group-create groupmemberspage groupmoderatorspage
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-diplom-ws0708@3414 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/groups/components/GroupMemberPage.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/groups/components/GroupMemberPage.java
@@ -1,100 +1,146 @@
 package org.openuss.web.groups.components;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import javax.faces.event.ValueChangeEvent;
 
 import org.apache.log4j.Logger;
 import org.apache.shale.tiger.managed.Bean;
 import org.apache.shale.tiger.managed.Scope;
-import org.apache.shale.tiger.view.Prerender;
 import org.apache.shale.tiger.view.View;
-import org.openuss.framework.jsfcontrols.breadcrumbs.BreadCrumb;
 import org.openuss.framework.web.jsf.model.AbstractPagedTable;
 import org.openuss.framework.web.jsf.model.DataPage;
+import org.openuss.groups.UserGroupMemberInfo;
 import org.openuss.security.User;
-import org.openuss.security.UserInfo;
 import org.openuss.web.Constants;
-
 
 /**
  * 
  * @author Lutz D. Kramer
  * @author Thomas Jansing
  */
-@Bean(name = "views$secured$groups$groupmembers", scope = Scope.REQUEST)
+@Bean(name = "views$secured$groups$components$groupmembers", scope = Scope.REQUEST)
 @View
 public class GroupMemberPage extends AbstractGroupPage {
-	
-	private static final Logger logger = Logger.getLogger(GroupMemberPage.class);
 
-	private ParticipantDataProvider data = new ParticipantDataProvider();
+	private static final Logger logger = Logger
+			.getLogger(GroupMemberPage.class);
 
-	public String save() {
-		logger.debug("Group member page - saved");
-		return Constants.SUCCESS;
-	}
+	private GroupsDataProvider data = new GroupsDataProvider();
+	private DataPage<MemberInfo> page;
 
-	public void changedMember(ValueChangeEvent event){
-		logger.debug("changed group members");
-	}
+	/* ----- private classes ----- */
 
-	@Prerender
-	public void prerender() throws Exception {
-		super.prerender();
-		addPageCrumb();
-	}
-	
-	//TODO - Lutz: Properties anpassen
-	private void addPageCrumb() {
-		BreadCrumb crumb = new BreadCrumb();
-		crumb.setName(i18n("group_command_options_participants"));
-		crumb.setHint(i18n("group_command_options_participants"));
-		breadcrumbs.addCrumb(crumb);
-	}
-	
-	private class ParticipantDataProvider extends AbstractPagedTable<UserInfo> {
+	private class MemberInfo {
 
-		private static final long serialVersionUID = -1918372320518667092L;
+		private Long id;
+		private String username;
+		private String firstName;
+		private String lastName;
+		private boolean moderator;
+
+		/* ----- getter and setter ----- */
+
+		public MemberInfo(Long id, String username, String firstName, String lastName,
+				boolean moderator) {
+			this.id = id;
+			this.username = username;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.moderator = moderator;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public boolean isModerator() {
+			return moderator;
+		}
+
+		public void setModerator(boolean moderator) {
+			this.moderator = moderator;
+		}
+
 		
-		private DataPage<UserInfo> memberPage;
+		public Long getId() {
+			return id;
+		}
+
+		
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+	}
+
+	private class GroupsDataProvider extends AbstractPagedTable<MemberInfo> {
+
+		private static final long serialVersionUID = -5342817757466323535L;
 
 		@Override
-		public DataPage<UserInfo> getDataPage(int startRow, int pageSize) {
-			if (memberPage == null) {
-				List<UserInfo> member = groupService.getMembers(groupInfo);
-				memberPage = new DataPage<UserInfo>(member.size(), 0, member);
-				sort(member);
+		public DataPage<MemberInfo> getDataPage(int startRow, int pageSize) {
+			if (page == null) {
+				logger.debug("fetching group list");
+				List<UserGroupMemberInfo> membersInfo = groupService
+						.getAllMembers(groupInfo);
+				List<MemberInfo> members = new ArrayList<MemberInfo>();
+				for (UserGroupMemberInfo member : membersInfo) {
+					members.add(new MemberInfo(member.getUserId(), member.getUsername(), member
+							.getFirstName(), member.getLastName(), groupService
+							.isModerator(groupInfo, member.getUserId())));
+				}
+				page = new DataPage<MemberInfo>(members.size(), 0, members);
+				sort(members);
 			}
-			return memberPage;
+			return page;
 		}
 	}
 
-	public String showProfile() {
-		UserInfo member = data.getRowData();
-		User user = User.Factory.newInstance();
-		user.setId(member.getId());
-		setSessionBean(Constants.SHOW_USER_PROFILE, user);
+	/* ----- business logic ----- */
+	
+	public String linkProfile(){
+		User profile = User.Factory.newInstance();
+		profile.setId(this.data.getRowData().getId());
+		setSessionAttribute(Constants.SHOW_USER_PROFILE, profile);
 		return Constants.USER_PROFILE_VIEW_PAGE;
 	}
 
-	public String delete() {
-		logger.info("group member deleted");
-		UserInfo member = data.getRowData();
-		// groupService.removeMember(member.getId());
-		// TODO - Lutz: Properties anpassen
-		addMessage(i18n("message_group_removed_participant",member.getUsername()));
-		return Constants.SUCCESS;
-	}
+	/* ----- getter and setter ----- */
 
-	// ----------------------- PROPERTIES ---------------------------------
-
-	public ParticipantDataProvider getData() {
+	public GroupsDataProvider getData() {
 		return data;
 	}
 
-	public void setData(ParticipantDataProvider data) {
+	public void setData(GroupsDataProvider data) {
 		this.data = data;
 	}
 
+	public DataPage<MemberInfo> getPage() {
+		return page;
+	}
+
+	public void setPage(DataPage<MemberInfo> page) {
+		this.page = page;
+	}
+	
 }
--- plexus/plexus-web/src/main/java/org/openuss/web/groups/components/GroupModeratorsPage.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/groups/components/GroupModeratorsPage.java
@@ -1,205 +1,200 @@
 package org.openuss.web.groups.components;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-
-import javax.faces.event.ValueChangeEvent;
-import javax.faces.model.SelectItem;
 
 import org.apache.log4j.Logger;
 import org.apache.shale.tiger.managed.Bean;
-import org.apache.shale.tiger.managed.Property;
 import org.apache.shale.tiger.managed.Scope;
-import org.apache.shale.tiger.view.Prerender;
 import org.apache.shale.tiger.view.View;
-import org.openuss.framework.jsfcontrols.breadcrumbs.BreadCrumb;
 import org.openuss.framework.web.jsf.model.AbstractPagedTable;
 import org.openuss.framework.web.jsf.model.DataPage;
-import org.openuss.security.SecurityService;
+import org.openuss.groups.UserGroupMemberInfo;
 import org.openuss.security.User;
-import org.openuss.security.UserInfo;
 import org.openuss.web.Constants;
-
 
 /**
  * 
  * @author Lutz D. Kramer
- *
+ * 
  */
-@Bean(name = "views$secured$groups$groupassistants", scope = Scope.REQUEST)
+@Bean(name = "views$secured$groups$components$groupmoderators", scope = Scope.REQUEST)
 @View
 public class GroupModeratorsPage extends AbstractGroupPage {
-	
-	private static final Logger logger = Logger.getLogger(GroupModeratorsPage.class);
 
-	private ModeratorDataProvider data = new ModeratorDataProvider();
+	private static final Logger logger = Logger
+			.getLogger(GroupModeratorsPage.class);
 
-	@Property(value = "#{securityService}")
-	protected SecurityService securityService;
-	
-	private Long userId;
+	private GroupsDataProvider data = new GroupsDataProvider();
+	private DataPage<MemberInfo> page;
+	List<MemberInfo> members;
 
-	List<UserInfo> moderators;
-	Set<Long> moderatorsUserIds;
-	List<SelectItem> instituteMembers;
-	
-	String username;
-	
-	private DataPage<UserInfo> page;
+	/* ----- private classes ----- */
 
+	private class MemberInfo {
 
-	@Prerender
-	public void prerender() throws Exception {
-		super.prerender();
-		addPageCrumb();
+		private Long id;
+		private String username;
+		private String firstName;
+		private String lastName;
+		private String eMail;
+		private boolean moderator;
+
+		/* ----- getter and setter ----- */
+
+		public MemberInfo(Long id, String username, String firstName,
+				String lastName, String eMail, boolean moderator) {
+			this.id = id;
+			this.username = username;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.eMail = eMail;
+			this.moderator = moderator;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public boolean isModerator() {
+			return moderator;
+		}
+
+		public void setModerator(boolean moderator) {
+			this.moderator = moderator;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getEMail() {
+			return eMail;
+		}
+
+		public void setEMail(String mail) {
+			eMail = mail;
+		}
+
 	}
-	
-	// TODO - Lutz: Properties anpassen
-	private void addPageCrumb() {
-		BreadCrumb crumb = new BreadCrumb();
-		crumb.setLink("");
-		crumb.setName(i18n("group_command_options_assistants"));
-		crumb.setHint(i18n("group_command_options_assistants"));
-		breadcrumbs.addCrumb(crumb);
-	}	
-	
-	private class ModeratorDataProvider extends AbstractPagedTable<UserInfo> {
-		
+
+	private class GroupsDataProvider extends AbstractPagedTable<MemberInfo> {
+
 		private static final long serialVersionUID = -5342817757466323535L;
 
 		@Override
-		public DataPage<UserInfo> getDataPage(int startRow, int pageSize) {
+		public DataPage<MemberInfo> getDataPage(int startRow, int pageSize) {
 			if (page == null) {
-				logger.debug("fetching group assistant list");
-				List<UserInfo> moderators = getModerators();
-				page = new DataPage<UserInfo>(moderators.size(), 0, moderators);
-				sort(moderators);
+				logger.debug("fetching group list");
+				List<UserGroupMemberInfo> membersInfo = groupService
+						.getAllMembers(groupInfo);
+				members = new ArrayList<MemberInfo>();
+				for (UserGroupMemberInfo member : membersInfo) {
+					members.add(new MemberInfo(member.getUserId(), member
+							.getUsername(), member.getFirstName(), member
+							.getLastName(), member.getEMail(), groupService
+							.isModerator(groupInfo, member.getUserId())));
+				}
+				page = new DataPage<MemberInfo>(members.size(), 0, members);
+				sort(members);
 			}
 			return page;
 		}
 	}
 
-	private List<UserInfo> getModerators() {
-		if (moderators == null) {
-			moderators = groupService.getModerators(groupInfo);
-		}
-		return moderators;
-	}
+	/* ----- business logic ----- */
 
-	private Set<Long> getModeratorsUserIdMap() {
-		if (moderatorsUserIds == null) {
-			moderatorsUserIds = new HashSet<Long>();
-			for (UserInfo moderators : getModerators()) {
-				moderatorsUserIds.add(moderators.getId());
-			}
-		}
-		return moderatorsUserIds;
-	}
-
-	public String showProfile() {
-		UserInfo memberInfo = data.getRowData();
-		User user = User.Factory.newInstance();
-		user.setId(memberInfo.getId());
-		setSessionBean("showuser", user);
+	public String linkProfile() {
+		User profile = User.Factory.newInstance();
+		profile.setId(this.data.getRowData().getId());
+		setSessionAttribute(Constants.SHOW_USER_PROFILE, profile);
 		return Constants.USER_PROFILE_VIEW_PAGE;
 	}
 
-	public String delete() {
-		logger.debug("course member deleted");
-		UserInfo moderator = data.getRowData();
-		groupService.removeModerator(groupInfo, moderator.getId());
-		// TODO - Lutz: Properties anpassen
-		addMessage(i18n("message_group_removed_assistant", moderator.getUsername()));
-		resetCachedData();
-		return Constants.SUCCESS;
-	}
-
-	public String addModerator() {
-		logger.debug("group moderator aspirant added");
-		if (userId != null){
-			User user = User.Factory.newInstance();
-			user.setId(userId);
-			groupService.addModerator(groupInfo, user.getId());
-			// TODO - Lutz: Properties anpassen
-			addMessage(i18n("message_group_add_assistant"));
-			resetCachedData();
-			userId  = null;
-			return Constants.SUCCESS;
-		} else {
-			return addMember();
-		}
-	}
-
-	private void resetCachedData() {
-		moderators = null;
-		moderatorsUserIds = null;
-		page = null;
-	}
-
-	public String addMember() {
-		logger.debug("course assistant aspirant added");
-		User user = User.Factory.newInstance();
-		if(securityService.getUserByName(username) != null){
-		user = securityService.getUserByName(username);	
-		} else {
-			user = securityService.getUserByEmail(username);
-		}
-		try {
-			groupService.addModerator(groupInfo, user.getId());
-			// TODO - Lutz: Properties anpassen
-			addMessage(i18n("message_group_add_assistant"));
-			resetCachedData();
-		} catch (Exception e) {
-			logger.debug(e.getMessage());
-			// TODO - Lutz: Properties anpassen
-			addError(i18n("groups_error_apply_member_at_group"));
-			return Constants.FAILURE;
-		}
-		username = "";
-		return Constants.SUCCESS;
-	}
-	
 	public String save() {
-		logger.debug("Course assistants page - saved");
+		MemberInfo member = data.getRowData();
+		if (!groupService.isCreator(groupInfo, member.getId())) {
+			if (!groupService.isModerator(groupInfo, member.getId())
+					&& member.moderator) {
+				groupService.addModerator(groupInfo, member.getId());
+			}
+			if (groupService.isModerator(groupInfo, member.getId())
+					&& !member.moderator) {
+				groupService.removeModerator(groupInfo, member.getId());
+			}
+		} else {
+			if (!groupService.isModerator(groupInfo, member.getId())
+					&& member.moderator) {
+				groupService.addModerator(groupInfo, member.getId());
+			}
+			if (member.getId().compareTo(user.getId()) == 0){
+				if (groupService.isModerator(groupInfo, member.getId())
+						&& !member.moderator) {
+					groupService.removeModerator(groupInfo, member.getId());
+				}
+			}else{
+				// TODO - Lutz: Error anpassen
+				addError("group_remove_creator");
+			}
+		}
 		return Constants.SUCCESS;
 	}
 
-	public void changedModerator(ValueChangeEvent event) {
-		logger.debug("changed group moderator");
+	public String removeMember() {
+		MemberInfo member = data.getRowData();
+		if (!groupService.isCreator(groupInfo, member.getId())) {
+			groupService.removeMember(groupInfo, member.getId());
+		} else {
+			if (member.getId().compareTo(user.getId()) == 0){
+				groupService.removeMember(groupInfo, member.getId());
+			}else{
+				// TODO - Lutz: Error anpassen
+				addError("group_delete_creator");
+			}
+		}
+		return Constants.SUCCESS;
 	}
 
-	public ModeratorDataProvider getData() {
+	/* ----- getter and setter ----- */
+
+	public GroupsDataProvider getData() {
 		return data;
 	}
 
-	public void setData(ModeratorDataProvider data) {
+	public void setData(GroupsDataProvider data) {
 		this.data = data;
 	}
 
-	public Long getUserId() {
-		return userId;
+	public DataPage<MemberInfo> getPage() {
+		return page;
 	}
 
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
-
-	public SecurityService getSecurityService() {
-		return securityService;
-	}
-
-	public void setSecurityService(SecurityService securityService) {
-		this.securityService = securityService;
-	}
-	
-	
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
+	public void setPage(DataPage<MemberInfo> page) {
+		this.page = page;
 	}
 
 }
