d7a15a49a9cf6d9caca8c28d4a3643c917efebd4
roekens <roekens>
2008-02-26 19:30:51 +0000

    resolution for uss-906 Datatrunc Exception http://teamopenuss.uni-muenster.de/jira/browse/USS-906
    couldn't reproduce datatrunc exception under windows with too long filenames, filenames were always too short to cause exception.
    but FolderEntryDaoImp, FolderDaoImpl and FileEntryDaoImpl are updated, so that too long names for files and folders will get shortened if names are too long.
    
    where it was not possible to define too long filenames on a windows os, it was possible to define too long descriptions.
    maxlength property for textArea components does not work!!!
    descriptions are also shortened now, 
    
    datatruncation exception should not occur any more.
    
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk/openuss-plexus@3439 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-core/src/main/java/org/openuss/documents/FolderEntryDaoImpl.java
+++ plexus/plexus-core/src/main/java/org/openuss/documents/FolderEntryDaoImpl.java
@@ -5,6 +5,8 @@
  */
 package org.openuss.documents;
 
+import org.apache.commons.lang.StringUtils;
+
 /**
  * @see org.openuss.documents.FolderEntry
  * @author ingo dueppe
@@ -57,6 +59,21 @@ public class FolderEntryDaoImpl extends org.openuss.documents.FolderEntryDaoBase
 		return entry;
 	}
 
+	private String shortenInMiddle(String str, int maxLength){
+		if (str==null){
+			return null;
+		}
+		if (str.length()<maxLength){
+			return str;
+		}
+		int halfLength = maxLength / 2;
+		String beginning = "";
+		String ending = "";
+		ending = str.substring(str.length()-halfLength);
+		beginning = str.substring(0, str.length()-halfLength);
+		return StringUtils.abbreviate(beginning, maxLength-halfLength)+ending;
+	}
+	
 	/**
 	 * @see org.openuss.documents.FolderEntryDao#folderEntryInfoToEntity(org.openuss.documents.FolderEntryInfo)
 	 */
@@ -64,6 +81,8 @@ public class FolderEntryDaoImpl extends org.openuss.documents.FolderEntryDaoBase
 			org.openuss.documents.FolderEntryInfo folderEntryInfo) {
 		// @todo verify behavior of folderEntryInfoToEntity
 		org.openuss.documents.FolderEntry entity = this.loadFolderEntryFromFolderEntryInfo(folderEntryInfo);
+		folderEntryInfo.setName(shortenInMiddle(folderEntryInfo.getName(), 255));
+		folderEntryInfo.setDescription(shortenInMiddle(folderEntryInfo.getDescription(), 1000));
 		this.folderEntryInfoToEntity(folderEntryInfo, entity, true);
 		return entity;
 	}
@@ -75,6 +94,8 @@ public class FolderEntryDaoImpl extends org.openuss.documents.FolderEntryDaoBase
 	public void folderEntryInfoToEntity(org.openuss.documents.FolderEntryInfo sourceVO,
 			org.openuss.documents.FolderEntry targetEntity, boolean copyIfNull) {
 		// @todo verify behavior of folderEntryInfoToEntity
+		sourceVO.setName(shortenInMiddle(sourceVO.getName(), 255));
+		sourceVO.setDescription(shortenInMiddle(sourceVO.getDescription(), 1000));
 		super.folderEntryInfoToEntity(sourceVO, targetEntity, copyIfNull);
 	}
 
