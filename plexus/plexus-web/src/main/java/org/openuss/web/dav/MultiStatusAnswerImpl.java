22a492b33e385067cd1ef658176cb64cb0f75e3c
phihag <phihag>
2008-02-27 16:56:59 +0000

    Implementation of MultiStatusAnswer
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-bachelor-ws0708@3465 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-web/src/main/java/org/openuss/web/dav/MultiStatusAnswerImpl.java
@@ -0,0 +1,97 @@
+package org.openuss.web.dav;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openuss.webdav.MultiStatusAnswer;
+import org.openuss.webdav.MultiStatusResponse;
+import org.openuss.webdav.WebDAVConstants;
+import org.openuss.webdav.WebDAVStatusCodes;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+/**
+ * Implements {@link MultiStatusAnswer}
+ */
+public class MultiStatusAnswerImpl implements MultiStatusAnswer  {
+	/**
+	 * The list of individual responses in this object.
+	 * Each element corresponds to a &lt;propstat&gt; element in the final outputted document. 
+	 */
+	protected List<MultiStatusResponse> responses;
+	/**
+	 * Additional inform(ation)al data. null for no information. 
+	 */
+	protected String description;
+	
+	public MultiStatusAnswerImpl() {
+		responses = new ArrayList<MultiStatusResponse>();
+		description = null;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.openuss.webdav.MultiStatusAnswer#addResponse(org.openuss.webdav.MultiStatusResponse)
+	 */
+	public void addResponse(MultiStatusResponse response) {
+		responses.add(response);
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.openuss.webdav.MultiStatusAnswer#getResponses()
+	 */
+	public List<MultiStatusResponse> getResponses() {
+		return responses;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.openuss.webdav.MultiStatusAnswer#getDescription()
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openuss.webdav.MultiStatusAnswer#setDescription(java.lang.String)
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openuss.webdav.WebDAVAnswer#getContentType()
+	 */
+	public String getContentType() {
+		return WebDAVConstants.MIMETYPE_XML;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openuss.webdav.WebDAVAnswer#getMessage()
+	 */
+	public String getMessage() {
+		Document doc = WebDAVUtils.newDocument();
+		
+		Node rootNode = doc.createElementNS(WebDAVConstants.NAMESPACE_WEBDAV_URI, WebDAVConstants.XML_MULTISTATUS);
+		doc.appendChild(rootNode);
+		
+		for (MultiStatusResponse response : getResponses()) {
+			response.addToXML(rootNode);
+		}
+		
+		// append description, if set
+		if (description != null) {
+			Node descNode = doc.createElementNS(WebDAVConstants.NAMESPACE_WEBDAV_URI, WebDAVConstants.XML_RESPONSEDESCRIPTION);
+			Node descNodeText = doc.createTextNode(description);
+			descNode.appendChild(descNodeText);
+			rootNode.appendChild(descNode);
+		}
+		
+		return WebDAVUtils.documentToString(doc);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openuss.webdav.WebDAVAnswer#getStatusCode()
+	 */
+	public int getStatusCode() {
+		return WebDAVStatusCodes.SC_MULTI_STATUS;
+	}
+}
