fd7da5afe15538ba974d898b94eb97ed457f0fae
juergendb <juergendb>
2008-02-29 15:34:05 +0000

    + renamed LdapConfigurerService --> LdapConfigurationNotifyService
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-diplom-ws0708-integrationsteam@3583 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-core/src/main/java/org/openuss/security/ldap/LdapConfigurationNotifyServiceImpl.java
@@ -0,0 +1,25 @@
+// OpenUSS - Open Source University Support System
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package org.openuss.security.ldap;
+
+/**
+ * @see org.openuss.security.ldap.LdapConfigurationNotifyService
+ */
+public class LdapConfigurationNotifyServiceImpl
+    extends org.openuss.security.ldap.LdapConfigurationNotifyServiceBase
+{
+
+    /**
+     * @see org.openuss.security.ldap.LdapConfigurationNotifyService#reconfigure()
+     */
+    protected void handleReconfigure()
+        throws java.lang.Exception
+    {
+        // @todo implement protected void handleReconfigure()
+        throw new java.lang.UnsupportedOperationException("org.openuss.security.ldap.LdapConfigurationNotifyService.handleReconfigure() Not implemented!");
+    }
+
+}
---
+++ plexus/plexus-core/src/test/java/org/openuss/security/ldap/LdapConfigurationNotifyServiceIntegrationTest.java
@@ -0,0 +1,14 @@
+// OpenUSS - Open Source University Support System
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package org.openuss.security.ldap;
+
+/**
+ * JUnit Test for Spring Hibernate LdapConfigurationNotifyService class.
+ * @see org.openuss.security.ldap.LdapConfigurationNotifyService
+ */
+public class LdapConfigurationNotifyServiceIntegrationTest extends LdapConfigurationNotifyServiceIntegrationTestBase {
+	
+}
