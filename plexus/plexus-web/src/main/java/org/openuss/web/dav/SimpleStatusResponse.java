f30f45619f8a114f58cf1a44279abd3d492ce124
stefan1984 <stefan1984>
2008-03-03 15:49:28 +0000

    +SimpleStatusResponse implementation of MultiStatusResponse
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-bachelor-ws0708@3653 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-web/src/main/java/org/openuss/web/dav/SimpleStatusResponse.java
@@ -0,0 +1,59 @@
+package org.openuss.web.dav;
+
+import org.openuss.webdav.MultiStatusResponse;
+import org.openuss.webdav.WebDAVConstants;
+import org.openuss.webdav.WebDAVStatusCodes;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class SimpleStatusResponse implements MultiStatusResponse{
+	
+	/**
+	 * The (absolute or relative) path to the resource that is represented in this response. 
+	 */
+	protected String href;
+	
+	/**
+	 * The status code
+	 */
+	protected int statusCode;
+	
+	/**
+	 * Constructor.
+	 * @param href The href of the resource this response is associated with.
+	 * @param responseDescription The description of the resource. 
+	 */
+	public SimpleStatusResponse(String href, int statusCode) {
+		this.href = href;
+		this.statusCode = statusCode;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openuss.webdav.MultiStatusResponse#addToXML(org.w3c.dom.Element)
+	 */
+	public void addToXML(Element el) {
+		Document doc = el.getOwnerDocument();
+		
+		// create response element
+		Element responseEl = doc.createElementNS(WebDAVConstants.NAMESPACE_WEBDAV_URI, WebDAVConstants.XML_RESPONSE);
+		el.appendChild(responseEl);
+		
+		// append href
+		Element hrefElement = doc.createElementNS(WebDAVConstants.NAMESPACE_WEBDAV_URI, WebDAVConstants.XML_HREF);
+		hrefElement.setTextContent(getHref());
+		responseEl.appendChild(hrefElement);
+		
+		// Status code
+		Element statusEl = doc.createElementNS(WebDAVConstants.NAMESPACE_WEBDAV_URI, WebDAVConstants.XML_STATUS);
+		statusEl.setTextContent(WebDAVStatusCodes.getStatusLine(statusCode));
+		responseEl.appendChild(statusEl);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openuss.webdav.MultiStatusResponse#getHref()
+	 */
+	public String getHref() {
+		return href;
+	}
+
+}
