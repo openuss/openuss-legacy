75e94ef4b6ff4b751e03124eafdd12503003c141
m_liet01 <m_liet01>
2008-03-05 09:51:57 +0000

    Ouput format modified in seminarpoolRegistrationStep3.xhtml
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-bachelor-ws0708@3727 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-web/src/main/java/org/openuss/web/seminarpool/add/SeminarpoolDateConverter.java
@@ -0,0 +1,41 @@
+package org.openuss.web.seminarpool.add;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+
+import org.apache.shale.tiger.register.FacesConverter;
+
+
+@FacesConverter(value = "seminarpoolDateConverter")
+public class SeminarpoolDateConverter implements Converter {
+
+	private static final String datePattern = "dd.MM.yyyy HH:mm";
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat(datePattern);
+
+	
+	public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2)
+			throws ConverterException {
+		try {
+			return dateFormat.parse(arg2);
+		} catch (ParseException e) {
+			throw new ConverterException();
+		}
+	}
+
+	public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2)
+			throws ConverterException {
+		if (arg2 instanceof Date) {
+			return dateFormat.format((Date)arg2);	
+		}
+		return null;
+	}
+
+
+
+}
