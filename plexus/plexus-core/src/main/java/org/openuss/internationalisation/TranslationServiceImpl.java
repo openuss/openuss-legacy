9a24a0e0b88dd08c8b3c2d4cd1f86e8e40454452
doenerking98 <doenerking98>
2008-03-07 16:19:33 +0000

    für gerrit
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-diplom-ws0708@3876 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-core/src/main/java/org/openuss/internationalisation/TranslationServiceImpl.java
+++ plexus/plexus-core/src/main/java/org/openuss/internationalisation/TranslationServiceImpl.java
@@ -5,91 +5,140 @@
  */
 package org.openuss.internationalisation;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import net.sf.cglib.proxy.Factory;
+
 /**
  * @see org.openuss.internationalisation.TranslationService
  */
-public class TranslationServiceImpl
-    extends org.openuss.internationalisation.TranslationServiceBase
-{
+public class TranslationServiceImpl extends
+		org.openuss.internationalisation.TranslationServiceBase {
 
-    /**
-     * @see org.openuss.internationalisation.TranslationService#addLanguage(org.openuss.internationalisation.LanguageInfo)
-     */
-    protected void handleAddLanguage(org.openuss.internationalisation.LanguageInfo languageInfo)
-        throws java.lang.Exception
-    {
-        // @todo implement protected void handleAddLanguage(org.openuss.internationalisation.LanguageInfo languageInfo)
-        throw new java.lang.UnsupportedOperationException("org.openuss.internationalisation.TranslationService.handleAddLanguage(org.openuss.internationalisation.LanguageInfo languageInfo) Not implemented!");
-    }
+	/**
+	 * @see org.openuss.internationalisation.TranslationService#addLanguage(org.openuss.internationalisation.LanguageInfo)
+	 */
+	protected void handleAddLanguage(String languageCode)
+			throws java.lang.Exception {
+		getLanguageDao().create(languageCode);
+	}
 
-    /**
-     * @see org.openuss.internationalisation.TranslationService#addTranslationtext(org.openuss.internationalisation.TranslationTextInfo)
-     */
-    protected void handleAddTranslationtext(org.openuss.internationalisation.TranslationTextInfo translationTextInfo)
-        throws java.lang.Exception
-    {
-        // @todo implement protected void handleAddTranslationtext(org.openuss.internationalisation.TranslationTextInfo translationTextInfo)
-        throw new java.lang.UnsupportedOperationException("org.openuss.internationalisation.TranslationService.handleAddTranslationtext(org.openuss.internationalisation.TranslationTextInfo translationTextInfo) Not implemented!");
-    }
+	/**
+	 * @see org.openuss.internationalisation.TranslationService#addTranslationtext(org.openuss.internationalisation.TranslationTextInfo)
+	 */
+	protected void handleAddTranslationText(
+			org.openuss.internationalisation.TranslationTextInfo translationTextInfo,
+			String languageCode) throws java.lang.Exception {
 
-    /**
-     * @see org.openuss.internationalisation.TranslationService#removeTranslationtext(org.openuss.internationalisation.TranslationTextInfo)
-     */
-    protected void handleRemoveTranslationtext(org.openuss.internationalisation.TranslationTextInfo translationTextInfo)
-        throws java.lang.Exception
-    {
-        // @todo implement protected void handleRemoveTranslationtext(org.openuss.internationalisation.TranslationTextInfo translationTextInfo)
-        throw new java.lang.UnsupportedOperationException("org.openuss.internationalisation.TranslationService.handleRemoveTranslationtext(org.openuss.internationalisation.TranslationTextInfo translationTextInfo) Not implemented!");
-    }
+		// get translations to the given language
+		Language lang = getLanguageDao().load(languageCode);
+		Set<TranslationText> transTexts = lang.getTranslationTexts();
+				
+		 // are there existing translations for the same domain idfer and 		 subkey?
+		 for (TranslationText textIt : transTexts) {
+			 if ( (textIt.getDomainIdentifier() ==
+			 translationTextInfo.getDomainIdentifier())
+			 && (textIt.getSubKey() == translationTextInfo.getSubKey()) ) {
+			 throw new TranslationApplicationException("translation is already existing");
+			 }
+		 }
+		getTranslationTextDao().create(
+				translationTextInfo.getDomainIdentifier(), lang,
+				translationTextInfo.getSubKey(), translationTextInfo.getText());
+	}
 
-    /**
-     * @see org.openuss.internationalisation.TranslationService#removeLanguage(org.openuss.internationalisation.LanguageInfo)
-     */
-    protected void handleRemoveLanguage(org.openuss.internationalisation.LanguageInfo languageInfo)
-        throws java.lang.Exception
-    {
-        // @todo implement protected void handleRemoveLanguage(org.openuss.internationalisation.LanguageInfo languageInfo)
-        throw new java.lang.UnsupportedOperationException("org.openuss.internationalisation.TranslationService.handleRemoveLanguage(org.openuss.internationalisation.LanguageInfo languageInfo) Not implemented!");
-    }
+	/**
+	 * @see org.openuss.internationalisation.TranslationService#removeTranslationtext(org.openuss.internationalisation.TranslationTextInfo)
+	 */
+	protected void handleRemoveTranslationText(
+			org.openuss.internationalisation.TranslationTextInfo translationTextInfo)
+			throws java.lang.Exception {
+		// @todo implement
+		throw new java.lang.UnsupportedOperationException(
+				"org.openuss.internationalisation.TranslationService.handleRemoveTranslationtext(org.openuss.internationalisation.TranslationTextInfo translationTextInfo) Not implemented!");
+	}
 
-    /**
-     * @see org.openuss.internationalisation.TranslationService#getTranslation(java.lang.Long, java.lang.String)
-     */
-    protected java.lang.String handleGetTranslation(java.lang.Long domainIdentifier, java.lang.String subKey)
-        throws java.lang.Exception
-    {
-        // @todo implement protected java.lang.String handleGetTranslation(java.lang.Long domainIdentifier, java.lang.String subKey)
-        return null;
-    }
+	/**
+	 * @see org.openuss.internationalisation.TranslationService#removeLanguage(org.openuss.internationalisation.LanguageInfo)
+	 */
+	protected void handleRemoveLanguage(String languageCode)
+			throws java.lang.Exception {
+		Language language = getLanguageDao().load(languageCode);
+		if (language != null) {
+			getLanguageDao().remove(languageCode);
+		} else {
+			throw new TranslationApplicationException(
+					"No language definition found with the given name.");
+		}
+	}
 
-    /**
-     * @see org.openuss.internationalisation.TranslationService#getLanguages()
-     */
-    protected java.util.List handleGetLanguages()
-        throws java.lang.Exception
-    {
-        // @todo implement protected java.util.List handleGetLanguages()
-        return null;
-    }
+	/**
+	 * @see org.openuss.internationalisation.TranslationService#getTranslation(java.lang.Long,
+	 *      java.lang.String)
+	 */
+	protected java.lang.String handleGetTranslation(
+			java.lang.Long domainIdentifier, java.lang.String subKey, java.lang.String languageCode)
+			throws java.lang.Exception {
+		Language lang = getLanguageDao().load(languageCode);
+		TranslationTextSearchCriteria criteria = new TranslationTextSearchCriteria();
+		criteria.setDomainIdentifier(domainIdentifier);
+		criteria.setSubKey(subKey);
+		List<TranslationText> transTexts = getTranslationTextDao()
+				.findByCriteria(criteria);
+		List<TranslationTextInfo> returnList = new ArrayList<TranslationTextInfo>();
+		for (TranslationText textIt : transTexts) {
+			if (textIt.getLanguage() == lang) returnList.add(getTranslationTextDao().toTranslationTextInfo(textIt));
+		}
+		if (returnList.size() != 0) {
+			return returnList.get(0).getText();
+		} else {
+			throw new NoTranslationFoundException("No Translation found");
+		}
 
-    /**
-     * @see org.openuss.internationalisation.TranslationService#findBySubKey(java.lang.String)
-     */
-    protected java.util.List handleFindBySubKey(java.lang.String subKey)
-        throws java.lang.Exception
-    {
-        // @todo implement protected java.util.List handleFindBySubKey(java.lang.String subKey)
-        return null;
-    }
+	}
 
-    /**
-     * @see org.openuss.internationalisation.TranslationService#updateTranslationtext(org.openuss.internationalisation.TranslationTextInfo)
-     */
-    protected void handleUpdateTranslationtext(org.openuss.internationalisation.TranslationTextInfo translationTextInfo)
-        throws java.lang.Exception
-    {
-        // @todo implement protected void handleUpdateTranslationtext(org.openuss.internationalisation.TranslationTextInfo translationTextInfo)
-        throw new java.lang.UnsupportedOperationException("org.openuss.internationalisation.TranslationService.handleUpdateTranslationtext(org.openuss.internationalisation.TranslationTextInfo translationTextInfo) Not implemented!");
-    }
+	/**
+	 * @see org.openuss.internationalisation.TranslationService#getLanguages()
+	 */
+	protected java.util.List handleGetLanguages() throws java.lang.Exception {
+		List<Language> languages = (List) getLanguageDao().loadAll();
+		List<String> languageStrings = new ArrayList<String>();
+		for (Language lang : languages) {
+			languageStrings.add(lang.getLanguageCode());
+		}
+		return languageStrings;
+	}
 
+	/**
+	 * @see org.openuss.internationalisation.TranslationService#updateTranslationtext(org.openuss.internationalisation.TranslationTextInfo)
+	 */
+	protected void handleUpdateTranslationText(
+			org.openuss.internationalisation.TranslationTextInfo translationTextInfo)
+			throws java.lang.Exception {
+		TranslationText transText = getTranslationTextDao().load(
+				translationTextInfo.getId());
+		transText.setSubKey(translationTextInfo.getSubKey());
+		transText.setText(translationTextInfo.getText());
+		// TODO TranslationServiceImpl: handleUpdateTranslationText() :
+		// implement update of domainidentifiert and language if necessary
+		getTranslationTextDao().update(transText);
+	}
+
+	@Override
+	protected List handleGetTranslationTexts(String languageCode,
+			Long domainIdentifier) throws Exception {
+		Language lang = getLanguageDao().load(languageCode);
+		TranslationTextSearchCriteria criteria = new TranslationTextSearchCriteria(
+				domainIdentifier, null);
+		List<TranslationText> transTextsByCriteria = getTranslationTextDao()
+				.findByCriteria(criteria);
+		List<TranslationTextInfo> returnList = new ArrayList<TranslationTextInfo>();
+		for (TranslationText textIt : transTextsByCriteria) {
+			if (textIt.getLanguage() == lang) returnList.add(getTranslationTextDao().toTranslationTextInfo(textIt));
+		}
+		return returnList;
+	}
 }
--- plexus/plexus-core/src/test/java/org/openuss/internationalisation/TranslationServiceIntegrationTest.java
+++ plexus/plexus-core/src/test/java/org/openuss/internationalisation/TranslationServiceIntegrationTest.java
@@ -5,10 +5,75 @@
  */
 package org.openuss.internationalisation;
 
+import java.util.List;
+
+import org.openuss.TestUtility;
+import org.openuss.calendar.AppointmentTypeInfo;
+import org.openuss.foundation.DefaultDomainObject;
+import org.openuss.foundation.DomainObject;
+import org.openuss.security.SecurityService;
+
 /**
  * JUnit Test for Spring Hibernate TranslationService class.
+ * 
  * @see org.openuss.internationalisation.TranslationService
  */
-public class TranslationServiceIntegrationTest extends TranslationServiceIntegrationTestBase {
+public class TranslationServiceIntegrationTest extends
+		TranslationServiceIntegrationTestBase {
 	
+	SecurityService securityService;
+
+	public void testLanguageAdministration() {
+		try {
+			translationService.addLanguage("deDE");
+			List<String> languages = translationService.getLanguages();
+			assertNotNull(languages);
+			assertEquals(1, languages.size());
+			assertEquals("deDE", languages.get(0).toString());
+			translationService.removeLanguage("deDE");
+			List<String> languages2 = translationService.getLanguages();
+			assertEquals(0, languages2.size());
+		} catch (TranslationApplicationException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	public void testTranslationAdministration() {
+		TranslationTextInfo transInfo = new TranslationTextInfo();
+		transInfo.setDomainIdentifier(1L);
+		transInfo.setSubKey("street");
+		transInfo.setText("Straﬂe");
+		TranslationTextInfo transInfo2 = new TranslationTextInfo();
+		transInfo2.setDomainIdentifier(1L);
+		transInfo2.setSubKey("description");
+		transInfo2.setText("Bezeichnung");
+		try {
+			translationService.addLanguage("deDE");
+			translationService.addTranslationText(transInfo, "deDE");
+			translationService.addTranslationText(transInfo2, "deDE");
+			String result = translationService.getTranslation(1L, "street", "deDE");
+			assertEquals("Straﬂe", result);
+			String result2 = translationService.getTranslation(1L, "description", "deDE");
+			assertEquals("Bezeichnung", result2);
+			try {
+				translationService.getTranslation(2L, "notFound", "deDE");
+				fail();
+			} catch (NoTranslationFoundException e) {
+				e.printStackTrace();
+			}
+			// TODO Implement Update of Translation
+			List<TranslationTextInfo> newTranslationInfos = translationService.getTranslationTexts("deDE", 1L);
+			TranslationTextInfo newTranslationInfo = newTranslationInfos.get(1);
+			newTranslationInfo.setText("Beschreibung");
+			translationService.updateTranslationText(newTranslationInfo);
+			String result3 = translationService.getTranslation(1L, "description", "deDE");
+			assertNotNull(result3);
+			assertEquals("Beschreibung", result3);
+		} catch (TranslationApplicationException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
 }
