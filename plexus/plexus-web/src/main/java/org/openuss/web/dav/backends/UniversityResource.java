06b06b4f0ebd9e87f54f75c66ca5a52f4fdfbd2e
phihag <phihag>
2008-03-07 16:43:26 +0000

    Implementation of UniversityResource
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-bachelor-ws0708@3880 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/dav/backends/UniversityResource.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/dav/backends/UniversityResource.java
@@ -1,11 +1,17 @@
 package org.openuss.web.dav.backends;
 
-import java.util.AbstractCollection; // TODOs
+import java.util.AbstractCollection; // TODO security
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
+import org.openuss.lecture.DepartmentInfo;
+import org.openuss.lecture.DepartmentService;
 import org.openuss.lecture.UniversityInfo;
+import org.openuss.web.Constants;
+import org.openuss.webdav.WebDAVConstants;
 import org.openuss.webdav.WebDAVPath;
 import org.openuss.webdav.WebDAVResource;
 import org.springframework.web.context.WebApplicationContext;
@@ -14,23 +20,54 @@ import org.springframework.web.context.WebApplicationContext;
  * A backend resource that represents a UniversityResource.
  */
 public class UniversityResource extends AbstractOrganisationResource {
+	protected DepartmentService departmentService;
+	protected Collection<DepartmentInfo> subDepartments = null;
 	protected final UniversityInfo ui;
 	
 	public UniversityResource(WebApplicationContext wac, WebDAVPath path, UniversityInfo ui) {
 		super(wac, path, ui.getId());
 		this.ui = ui;
+		
+		departmentService = (DepartmentService) getWAC().getBean(Constants.DEPARTMENT_SERVICE, DepartmentService.class);
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.openuss.web.dav.SimpleWebDAVResource#getChild(long, java.lang.String, org.openuss.webdav.WebDAVPath)
+	 */
 	@Override
 	protected WebDAVResource getChild(long id, String name, WebDAVPath path) {
-		// TODO Auto-generated method stub
+		DepartmentInfo di = null;
+		
+		if (id != ID_NONE) {
+			di = departmentService.findDepartment(id);
+		} else {
+			for (DepartmentInfo adi : getSubDepartments()) {
+				if (name.equals(sanitizeName(adi.getShortName()))) {
+					di = adi;
+					break;
+				}
+			}
+		}
+		
+		if (di != null) {
+			return new DepartmentResource(getWAC(), path, di);
+		}
+		
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.openuss.web.dav.SimpleWebDAVResource#getRawChildNames()
+	 */
 	@Override
 	protected Map<Long, String> getRawChildNames() {
-		// TODO Auto-generated method stub
-		return null;
+		Map<Long,String> resMap = new TreeMap<Long, String>();
+		
+		for (DepartmentInfo di : getSubDepartments()) {
+			resMap.put(di.getId(), di.getShortName());
+		}
+		
+		return resMap;
 	}
 
 	/* (non-Javadoc)
@@ -39,8 +76,8 @@ public class UniversityResource extends AbstractOrganisationResource {
 	@Override
 	protected Map<String, String> simpleGetProperties(Set<String> propNames) {
 		Map<String,String> res = new HashMap<String, String>();
-		if ((propNames == null) || (propNames.contains("displayname"))) {
-			res.put("displayname", ui.getName());
+		if ((propNames == null) || (propNames.contains(WebDAVConstants.XML_DISPLAYNAME))) {
+			res.put(WebDAVConstants.XML_DISPLAYNAME, ui.getName());
 		}
 		
 		return res;
@@ -50,8 +87,19 @@ public class UniversityResource extends AbstractOrganisationResource {
 	 * @see org.openuss.webdav.WebDAVResource#isReadable()
 	 */
 	public boolean isReadable() {
-		// TODO Check it
+		// TODO Security
 		return true;
 	}
-
+	
+	/**
+	 * @return All departments in this university
+	 */
+	@SuppressWarnings("unchecked")
+	protected Collection<DepartmentInfo> getSubDepartments() {
+		if (subDepartments == null) {
+			subDepartments = departmentService.findDepartmentsByUniversityAndEnabled(getId(), true);
+		}
+		
+		return subDepartments;
+	}
 }
