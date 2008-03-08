16c852049229e0bfa8aa432bbadf540b2f01ca38
dyck <dyck>
2008-03-08 17:41:45 +0000

    Resources cleanup, new handling for creation try of already existing sites.
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-plexus-usability@3928 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/wiki/WikiCreateNewSitePage.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/wiki/WikiCreateNewSitePage.java
@@ -3,7 +3,6 @@ package org.openuss.web.wiki;
 import org.apache.log4j.Logger;
 import org.apache.shale.tiger.managed.Bean;
 import org.apache.shale.tiger.managed.Scope;
-import org.apache.shale.tiger.view.Prerender;
 import org.apache.shale.tiger.view.View;
 import org.openuss.web.Constants;
 
@@ -20,31 +19,48 @@ public class WikiCreateNewSitePage extends AbstractWikiPage {
 	
 	private String createNewSiteName;
 	
-	@Override
-	@Prerender
-	public void prerender() throws Exception {
-		super.prerender();
-	}
-	
 	/**
-	 * Opens the Create or Edit Site Page.
-	 * @return Wiki Edit Page.
+	 * Opens the Create or View Site Page.
+	 * @return Wiki Edit Page or Wiki Main Page.
 	 */
 	public String createNewSite() {
 		this.siteVersionInfo = this.wikiService.findWikiSiteContentByDomainObjectAndName(this.courseInfo.getId(), createNewSiteName);
 		
 		if (this.siteVersionInfo == null) {
-			LOGGER.debug("Site '" + createNewSiteName + "' does not exist. Opening Create New Site Page.");			
-			this.siteName = createNewSiteName;
+			return handleSiteDoesNotExist();
 		} else {
-			LOGGER.debug("Site '" + createNewSiteName + "' does already exist. Opening Edit Site Page.");
-			this.addMessage("!!HARDCODE!! Site exists.");
+			return handleSiteAlreadyExists();
 		}
+	}
+	
+	/**
+	 * Returns Wiki Edit Page.
+	 * @return Wiki Edit Page.
+	 */
+	private String handleSiteDoesNotExist() {
+		LOGGER.debug("Site '" + createNewSiteName + "' does not exist. Opening Create New Site Page.");			
+		
+		this.siteName = createNewSiteName;
 		
 		setSessionBean(Constants.WIKI_CURRENT_SITE_VERSION, this.siteVersionInfo);
 		setSessionBean(Constants.WIKI_NEW_SITE_NAME, createNewSiteName);
 		
 		return Constants.WIKI_EDIT_PAGE;
+	}
+	
+	/**
+	 * Returns Wiki Main Page.
+	 * @return Wiki Main Page.
+	 */
+	private String handleSiteAlreadyExists() {
+		LOGGER.debug("Site '" + createNewSiteName + "' does already exist. Opening Edit Site Page.");
+		
+		this.addMessage(i18n("wiki_new_site_already_exists_message", createNewSiteName));
+		
+		setSessionBean(Constants.WIKI_CURRENT_SITE_VERSION, this.siteVersionInfo);
+		setSessionBean(Constants.WIKI_NEW_SITE_NAME, createNewSiteName);
+		
+		return Constants.WIKI_MAIN_PAGE;
 	}
 
 	public String getCreateNewSiteName() {
