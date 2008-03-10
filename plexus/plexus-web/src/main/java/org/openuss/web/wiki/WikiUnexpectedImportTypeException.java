6acd7cab3b12c186c9db6382220fe979196499be
chbeer <chbeer>
2008-03-10 16:51:57 +0000

    new
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-plexus-usability@4061 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-web/src/main/java/org/openuss/web/wiki/WikiUnexpectedImportTypeException.java
@@ -0,0 +1,24 @@
+package org.openuss.web.wiki;
+
+public class WikiUnexpectedImportTypeException extends RuntimeException {
+	
+	private static final long serialVersionUID = -2267311467887662408L;
+
+	public WikiUnexpectedImportTypeException() {
+		super();
+	}
+
+	public WikiUnexpectedImportTypeException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public WikiUnexpectedImportTypeException(String arg0) {
+		super(arg0);
+	}
+
+	public WikiUnexpectedImportTypeException(Throwable arg0) {
+		super(arg0);
+	}
+	
+	
+}
