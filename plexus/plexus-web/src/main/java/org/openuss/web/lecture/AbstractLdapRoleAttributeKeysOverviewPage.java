0921470561f483108302dde3db16635fbd925e1c
peterschuh <peterschuh>
2008-03-10 21:08:39 +0000

    # Refactored. Sorting works.
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-diplom-ws0708-integrationsteam@4113 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/lecture/AbstractLdapRoleAttributeKeysOverviewPage.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/lecture/AbstractLdapRoleAttributeKeysOverviewPage.java
@@ -1,18 +1,12 @@
 package org.openuss.web.lecture;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 import org.apache.log4j.Logger;
 import org.apache.shale.tiger.managed.Property;
 import org.apache.shale.tiger.view.Prerender;
-import org.openuss.framework.web.jsf.model.AbstractPagedTable;
-import org.openuss.framework.web.jsf.model.DataPage;
+import org.openuss.framework.jsfcontrols.breadcrumbs.BreadCrumb;
 import org.openuss.security.ldap.LdapConfigurationService;
-import org.openuss.security.ldap.RoleAttributeKeyInfo;
 import org.openuss.web.BasePage;
-import org.openuss.web.Constants;
+import org.openuss.web.PageLinks;
 
 /**
  * 
@@ -27,111 +21,36 @@ public abstract class AbstractLdapRoleAttributeKeysOverviewPage extends BasePage
 
 	protected static final Logger logger = Logger.getLogger(AbstractLdapRoleAttributeKeysOverviewPage.class);
 
-	protected RoleAttributeKeyTable roleAttributeKeys = new RoleAttributeKeyTable();
-
 	@Property(value = "#{ldapConfigurationService}")
 	protected LdapConfigurationService ldapConfigurationService;
 
 	@Prerender
-	public void prerender() throws Exception {
-	}
-
-	protected RoleAttributeKeyInfo currentRoleAttributeKey() {
-		RoleAttributeKeyInfo roleAttributeKey = roleAttributeKeys.getRowData();
-		return roleAttributeKey;
-	}
-
-	/**
-	 * Store the selected roleAttributeKey into session scope and go to roleAttributeKey
-	 * edit page.
-	 * 
-	 * @return Outcome
-	 */
-	public String selectRoleAttributeKeyAndEdit() {
-		RoleAttributeKeyInfo roleAttributeKey = currentRoleAttributeKey();
-		setSessionBean(Constants.ROLEATTRIBUTEKEY_INFO, roleAttributeKey);		
-		return Constants.LDAP_ROLEATTRIBUTEKEY_REGISTRATION_STEP1_PAGE;
-	}
-
-	/**
-	 * Store the selected roleAttributeKey into session scope and go to roleAttributeKey
-	 * remove confirmation page.
-	 * 
-	 * @return Outcome
-	 */
-	public String selectRoleAttributeKeyAndConfirmRemove() throws Exception {
-		logger.debug("Starting method selectRoleAttributeKeyAndConfirmRemove");		
-		setSessionBean(Constants.ROLEATTRIBUTEKEY_INFO, currentRoleAttributeKey());
-		
-		return Constants.ROLEATTRIBUTEKEY_CONFIRM_REMOVE_PAGE;
-	}	
-	
-
-
-	public String removeRoleAttributeKey() throws Exception {
+	public void prerender() {
 		try {
-			logger.debug("Starting method selectRoleAttributeKeyAndRemove");
-			RoleAttributeKeyInfo currentRoleAttributeKey = (RoleAttributeKeyInfo) getSessionBean(Constants.ROLEATTRIBUTEKEY_INFO);
-			if (currentRoleAttributeKey.getAttributeMappingIds() == null || currentRoleAttributeKey.getAttributeMappingIds().size()==0) {
-				ldapConfigurationService.deleteRoleAttributeKey(currentRoleAttributeKey);
-				setSessionBean(Constants.ROLEATTRIBUTEKEY_INFO, null);
-				return Constants.LDAP_ROLEATTRIBUTEKEY_PAGE;
-			} else {
-				addMessage(i18n("message_ldap_roleattributekey_still_in_use_cannot_be_removed"));
-				return Constants.LDAP_ROLEATTRIBUTEKEY_PAGE;
-			  }
-			}
-			catch (Exception e) {
-				addMessage(i18n("message_ldap_roleattributekey_cannot_be_removed"));
-				return Constants.LDAP_ROLEATTRIBUTEKEY_PAGE;
-			}
+			super.prerender();
+			addBreadCrumbs();
+		} catch (Exception e) {
+			
+		}	
 	}
 	
-	protected DataPage<RoleAttributeKeyInfo> dataPage;
+	/**
+	 * Adds an additional BreadCrumb.
+	 */
+	 private void addBreadCrumbs() {		 
+		 breadcrumbs.init();	
+		 
+		 BreadCrumb myBreadCrumb = new BreadCrumb();		 
+		 myBreadCrumb.setLink(PageLinks.ADMIN_LDAP_INDEX);
+		 myBreadCrumb.setName(i18n("ldap_index"));
+		 myBreadCrumb.setHint(i18n("ldap_index"));
+		 breadcrumbs.addCrumb(myBreadCrumb);
+		 
+		 myBreadCrumb = new BreadCrumb();
+		 myBreadCrumb.setLink(PageLinks.ADMIN_LDAP_ROLEATTRIBUTEKEY);
+		 myBreadCrumb.setName(i18n("ldap_roleattributekey"));
+		 myBreadCrumb.setHint(i18n("ldap_roleattributekey_hint"));
+		 breadcrumbs.addCrumb(myBreadCrumb);
+	 }
 
-	public abstract DataPage<RoleAttributeKeyInfo> fetchDataPage(int startRow,
-			int pageSize);
-
-	protected void sort(List<RoleAttributeKeyInfo> roleAttributeKeyList) {
-				Collections.sort(roleAttributeKeyList, new NameComparator());		
-	}
-
-	public LdapConfigurationService getLdapConfigurationService() {
-		return ldapConfigurationService;
-	}
-
-	public void setLdapConfigurationService(LdapConfigurationService ldapConfigurationService) {
-		this.ldapConfigurationService = ldapConfigurationService;
-	}
-
-	protected class NameComparator implements Comparator<RoleAttributeKeyInfo> {
-		public int compare(RoleAttributeKeyInfo f1, RoleAttributeKeyInfo f2) {
-			if (roleAttributeKeys.isAscending()) {
-				return f1.getName().compareToIgnoreCase(f2.getName());
-			} else {
-				return f2.getName().compareToIgnoreCase(f1.getName());
-			}
-		}
-	}
-
-	
-	public String confirmRemoveRoleAttributeKey() {
-		RoleAttributeKeyInfo roleAttributeKeyInfo = currentRoleAttributeKey();
-		setSessionBean(Constants.ROLEATTRIBUTEKEY, roleAttributeKeyInfo);
-		return "removed";
-	}
-
-	public RoleAttributeKeyTable getRoleAttributeKeys() {
-		return roleAttributeKeys;
-	}
-	
-	
-	protected class RoleAttributeKeyTable extends AbstractPagedTable<RoleAttributeKeyInfo> {
-
-		@Override
-		public DataPage<RoleAttributeKeyInfo> getDataPage(int startRow, int pageSize) {
-			return fetchDataPage(startRow, pageSize);
-		}
-
-	}
 }
--- plexus/plexus-web/src/main/java/org/openuss/web/system/LdapRoleAttributeKeysPage.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/system/LdapRoleAttributeKeysPage.java
@@ -1,16 +1,17 @@
 package org.openuss.web.system;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.apache.shale.tiger.managed.Bean;
 import org.apache.shale.tiger.managed.Scope;
-import org.apache.shale.tiger.view.Prerender;
 import org.apache.shale.tiger.view.View;
-import org.openuss.framework.jsfcontrols.breadcrumbs.BreadCrumb;
+import org.openuss.framework.web.jsf.model.AbstractPagedTable;
 import org.openuss.framework.web.jsf.model.DataPage;
-import org.openuss.lecture.DepartmentInfo;
+import org.openuss.security.ldap.LdapConfigurationService;
 import org.openuss.security.ldap.RoleAttributeKeyInfo;
-import org.openuss.web.PageLinks;
+import org.openuss.web.Constants;
 import org.openuss.web.lecture.AbstractLdapRoleAttributeKeysOverviewPage;
 
 /** 
@@ -24,52 +25,105 @@ import org.openuss.web.lecture.AbstractLdapRoleAttributeKeysOverviewPage;
 
 public class LdapRoleAttributeKeysPage extends AbstractLdapRoleAttributeKeysOverviewPage{
 	
-	@Prerender
-	public void prerender() {
-		try {
-			super.prerender();
-			addBreadCrumbs();
-		} catch (Exception e) {
-			
-		}	
-	}
+	protected RoleAttributeKeyTable roleAttributeKeys = new RoleAttributeKeyTable();
 	
-	/**
-	 * Adds an additional BreadCrumb.
-	 */
-	 private void addBreadCrumbs() {		 
-		 breadcrumbs.init();	
-		 
-		 BreadCrumb myBreadCrumb = new BreadCrumb();		 
-		 myBreadCrumb.setLink(PageLinks.ADMIN_LDAP_INDEX);
-		 myBreadCrumb.setName(i18n("ldap_index"));
-		 myBreadCrumb.setHint(i18n("ldap_index"));
-		 breadcrumbs.addCrumb(myBreadCrumb);
-		 
-		 myBreadCrumb = new BreadCrumb();
-		 myBreadCrumb.setLink(PageLinks.ADMIN_LDAP_ROLEATTRIBUTEKEY);
-		 myBreadCrumb.setName(i18n("ldap_roleattributekey"));
-		 myBreadCrumb.setHint(i18n("ldap_roleattributekey_hint"));
-		 breadcrumbs.addCrumb(myBreadCrumb);
-	 }
-	
-	public DataPage<RoleAttributeKeyInfo> fetchDataPage(int startRow, int pageSize) {
-		if (dataPage == null) {
-			if (logger.isDebugEnabled()) {
-				logger.debug("fetch roleattributekeys data page at " + startRow + ", "+ pageSize+" sorted by "+roleAttributeKeys.getSortColumn());
-			}
-			List<RoleAttributeKeyInfo> roleAttributeKeyList = ldapConfigurationService.getAllRoleAttributeKeys();
-			
 
-			logger.info("RoleAttributeKeys:"+roleAttributeKeyList);
-			if (roleAttributeKeyList != null) {
-				logger.info("Size:"+roleAttributeKeyList.size());
+	protected RoleAttributeKeyInfo currentRoleAttributeKey() {
+		RoleAttributeKeyInfo roleAttributeKey = roleAttributeKeys.getRowData();
+		return roleAttributeKey;
+	}
+
+	/**
+	 * Store the selected roleAttributeKey into session scope and go to roleAttributeKey
+	 * edit page.
+	 * 
+	 * @return Outcome
+	 */
+	public String selectRoleAttributeKeyAndEdit() {
+		RoleAttributeKeyInfo roleAttributeKey = currentRoleAttributeKey();
+		setSessionBean(Constants.ROLEATTRIBUTEKEY_INFO, roleAttributeKey);		
+		return Constants.LDAP_ROLEATTRIBUTEKEY_REGISTRATION_STEP1_PAGE;
+	}
+
+	/**
+	 * Store the selected roleAttributeKey into session scope and go to roleAttributeKey
+	 * remove confirmation page.
+	 * 
+	 * @return Outcome
+	 */
+	public String selectRoleAttributeKeyAndConfirmRemove() throws Exception {
+		logger.debug("Starting method selectRoleAttributeKeyAndConfirmRemove");		
+		setSessionBean(Constants.ROLEATTRIBUTEKEY_INFO, currentRoleAttributeKey());
+		
+		return Constants.ROLEATTRIBUTEKEY_CONFIRM_REMOVE_PAGE;
+	}	
+	
+
+
+	public String removeRoleAttributeKey() throws Exception {
+		try {
+			logger.debug("Starting method selectRoleAttributeKeyAndRemove");
+			RoleAttributeKeyInfo currentRoleAttributeKey = (RoleAttributeKeyInfo) getSessionBean(Constants.ROLEATTRIBUTEKEY_INFO);
+			if (currentRoleAttributeKey.getAttributeMappingIds() == null || currentRoleAttributeKey.getAttributeMappingIds().size()==0) {
+				ldapConfigurationService.deleteRoleAttributeKey(currentRoleAttributeKey);
+				setSessionBean(Constants.ROLEATTRIBUTEKEY_INFO, null);
+				return Constants.LDAP_ROLEATTRIBUTEKEY_PAGE;
+			} else {
+				addMessage(i18n("message_ldap_roleattributekey_still_in_use_cannot_be_removed"));
+				return Constants.LDAP_ROLEATTRIBUTEKEY_PAGE;
+			  }
+			}
+			catch (Exception e) {
+				addMessage(i18n("message_ldap_roleattributekey_cannot_be_removed"));
+				return Constants.LDAP_ROLEATTRIBUTEKEY_PAGE;
+			}
+	}
+	
+	
+	public LdapConfigurationService getLdapConfigurationService() {
+		return ldapConfigurationService;
+	}
+
+	public void setLdapConfigurationService(LdapConfigurationService ldapConfigurationService) {
+		this.ldapConfigurationService = ldapConfigurationService;
+	}
+
+	
+	public String confirmRemoveRoleAttributeKey() {
+		RoleAttributeKeyInfo roleAttributeKeyInfo = currentRoleAttributeKey();
+		setSessionBean(Constants.ROLEATTRIBUTEKEY, roleAttributeKeyInfo);
+		return "removed";
+	}
+
+	public RoleAttributeKeyTable getRoleAttributeKeys() {
+		return roleAttributeKeys;
+	}
+	
+	
+	protected class RoleAttributeKeyTable extends AbstractPagedTable<RoleAttributeKeyInfo> {
+		
+		private DataPage<RoleAttributeKeyInfo> dataPage;
+
+		@Override
+		public DataPage<RoleAttributeKeyInfo> getDataPage(int startRow, int pageSize) {
+			if (dataPage == null) {
+				if (logger.isDebugEnabled()) {
+					logger.debug("fetch roleattributekeys data page at " + startRow + ", "+ pageSize+" sorted by "+roleAttributeKeys.getSortColumn());
+				}
+				List<RoleAttributeKeyInfo> roleAttributeKeyList = ldapConfigurationService.getAllRoleAttributeKeys();
+				
+
+				logger.info("RoleAttributeKeys:"+roleAttributeKeyList);
+				if (roleAttributeKeyList != null) {
+					logger.info("Size:"+roleAttributeKeyList.size());
+				}
+				
+				sort(roleAttributeKeyList);
+				dataPage = new DataPage<RoleAttributeKeyInfo>(roleAttributeKeyList.size(),0,roleAttributeKeyList);
 			}
 			
-//			sort(roleAttributeKeyList);
-			dataPage = new DataPage<RoleAttributeKeyInfo>(roleAttributeKeyList.size(),0,roleAttributeKeyList);
+			return dataPage;
 		}
-		
-		return dataPage;
-	}
+
+	}	
 }
