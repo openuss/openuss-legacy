ec39d04f891321c1833449915c51555a1e7b6d5a
chbeer <chbeer>
2008-03-11 12:21:34 +0000

    fixed usability-groups issue: "wenn man bei eurer exams-erstellung ein datum in der vergangenheit als abgabe wählt und auf absenden drückt, kommt die normale addError-Meldung über der Seite. Besser wärs, wenns als rote Fehlermeldung unter der Komponente mit dem Fehler stehen würde"
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-plexus-usability@4144 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-web/src/main/java/org/openuss/web/papersubmission/validators/CalendarDateAfterNowValidator.java
@@ -0,0 +1,37 @@
+package org.openuss.web.papersubmission.validators;
+
+import java.util.Date;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.component.UIOutput;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import org.apache.log4j.Logger;
+import org.apache.shale.tiger.register.FacesValidator;
+import org.openuss.framework.web.jsf.controller.BaseBean;
+import org.openuss.lecture.UniversityServiceException;
+import org.openuss.web.Constants;
+
+/**
+ * Checks if the entered date is correct.
+ * 
+ * @author Team Collaboration (PS im WS 2007/08)
+ */
+@FacesValidator(value = "calendarDateAfterNowValidator")
+public class CalendarDateAfterNowValidator extends BaseBean implements Validator {
+
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		Date date = (Date) value;
+		
+		Date currentDate = new Date();
+		if (date.before(currentDate)) {
+			addError(i18n("papersubmission_illegal_exam_message"));
+			((UIInput) component).setValid(false);
+			addError(component.getClientId(context), "Deadline", i18n("papersubmission_validate_error_deadline_before_today"));
+		}
+	}
+
+}
