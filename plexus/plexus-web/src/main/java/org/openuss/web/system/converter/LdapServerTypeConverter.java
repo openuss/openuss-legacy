46b26b87b2a83836ce2eb2ea3ce2e5324d891b0b
juergendb <juergendb>
2008-03-13 13:54:36 +0000

    + added converter and validator to LDAP configuration
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-diplom-ws0708-integrationsteam@4353 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-web/src/main/java/org/openuss/web/system/converter/LdapServerTypeConverter.java
@@ -0,0 +1,34 @@
+package org.openuss.web.system.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+
+import org.apache.shale.tiger.register.FacesConverter;
+import org.openuss.security.ldap.LdapServerType;
+
+/**
+ * @author Juergen de Braaf
+ */
+@FacesConverter(value = "ldapServerTypeConverter")
+public class LdapServerTypeConverter implements Converter {
+
+	public Object getAsObject(FacesContext context, UIComponent component, String value) throws ConverterException {
+		int literal = Integer.parseInt(value);
+		return LdapServerType.fromInteger(literal);
+	}
+
+	public String getAsString(FacesContext context, UIComponent component, Object value) throws ConverterException {
+		
+		if (value instanceof LdapServerType){
+			LdapServerType ldapServerType = (LdapServerType) value;
+			return ldapServerType.getValue().toString();
+		}  else if (value instanceof Integer) {
+			return String.valueOf(value);
+		} else if (value instanceof String) {
+			return (String) value;
+		}
+		return null;
+	}
+}
---
+++ plexus/plexus-web/src/main/java/org/openuss/web/system/validator/ChangePasswordUrlValidator.java
@@ -0,0 +1,35 @@
+package org.openuss.web.system.validator;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.shale.tiger.register.FacesValidator;
+import org.openuss.framework.web.jsf.controller.BaseBean;
+import org.openuss.security.ldap.LdapConfigurationService;
+import org.openuss.security.ldap.LdapServerInfo;
+
+/**
+ * Validates changePasswordUrl.
+ * 
+ * @author Juergen de Braaf
+ *
+ */
+@FacesValidator(value="changePasswordUrlValidator")
+public class ChangePasswordUrlValidator extends BaseBean implements Validator {
+
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		String changePasswordUrl = (String) value;
+		if (StringUtils.isNotEmpty(changePasswordUrl)) {
+	    	LdapConfigurationService ldapConfigurationService = (LdapConfigurationService) getBean("ldapConfigurationService");
+	    	boolean valid = ldapConfigurationService.isValidURL(new String[]{"http", "https"}, changePasswordUrl);
+	    	if (!valid) {
+	    		((UIInput)component).setValid(false);
+	    		addError(component.getClientId(context), i18n("error_provider_url_invalid"), null);
+	    	}
+	    }
+	}
+}
