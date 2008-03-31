a3af742fe553517e826b2b10aa2f7ac553c5dafe
Ingo Düppe <ingo.dueppe@crowdcode.io>
2008-03-31 18:24:54 +0000

    add missing file after merge
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-plexus-3.2@4640 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-core/src/test/java/org/openuss/lecture/PeriodTest.java
@@ -0,0 +1,62 @@
+package org.openuss.lecture;
+
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.lang.time.DateUtils;
+
+public class PeriodTest extends TestCase {
+
+	public void testIsActive() {
+		Date today = new Date();
+		Date beforeYesterday = DateUtils.addDays(today, -2); 
+		Date yesterday = DateUtils.addDays(today, -1); 
+		Date tomorrow = DateUtils.addDays(today, 1);
+		Date afterTomorrow = DateUtils.addDays(today, 2); 
+		
+		Period period1 = new PeriodImpl();
+		period1.setStartdate(today);
+		period1.setEnddate(tomorrow);
+
+		assertTrue(period1.isActive());
+		
+		Period period2 = new PeriodImpl();
+		period2.setStartdate(beforeYesterday);
+		period2.setEnddate(yesterday);
+		assertFalse(period2.isActive());
+		
+		Period period3 = new PeriodImpl();
+		period3.setStartdate(tomorrow);
+		period3.setEnddate(afterTomorrow);
+		assertFalse(period3.isActive());
+
+		Period period4 = new PeriodImpl();
+		period4.setStartdate(yesterday);
+		period4.setEnddate(today);
+		assertTrue(period4.isActive());
+	}
+	
+	public void testAddCourse() {
+		Course course = Course.Factory.newInstance(120L);
+		Period period = Period.Factory.newInstance();
+		
+		period.add(course);
+		assertTrue(period.getCourses().contains(course));
+		try {
+			period.add(course);
+			fail("IllegalStateException expected");
+		} catch (IllegalStateException ise) {
+			assertNotNull(ise);
+		}
+		
+		try {
+			period.add(null);
+			fail("IllegalArgumentException expected");
+		} catch (IllegalArgumentException iae) {
+			assertNotNull(iae);
+		}
+	}
+	
+
+}
