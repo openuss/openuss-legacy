46359831f9e58a12367d52f33dfdac54846c7631
nick-it-simon <nick-it-simon>
2008-03-31 17:09:41 +0000

    parameter for server in webdav guide pages added
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-bachelor-ws0708@4635 60046477-0c10-0410-bf4b-e552a4ff1768

--- plexus/plexus-web/src/main/java/org/openuss/web/dav/GuideKdePage.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/dav/GuideKdePage.java
@@ -3,11 +3,14 @@ package org.openuss.web.dav;
 
 
 import org.apache.shale.tiger.managed.Bean;
+import org.apache.shale.tiger.managed.Property;
 import org.apache.shale.tiger.managed.Scope;
 import org.apache.shale.tiger.view.Prerender;
 import org.apache.shale.tiger.view.View;
 import org.openuss.framework.jsfcontrols.breadcrumbs.BreadCrumb;
 import org.openuss.web.BasePage;
+import org.openuss.web.Constants;
+import org.openuss.web.system.SystemPropertiesBean;
 
 
 
@@ -21,7 +24,14 @@ import org.openuss.web.BasePage;
 	@Bean(name = "views$secured$webdav$guide_kde", scope = Scope.REQUEST)
 	@View
 	public class GuideKdePage extends BasePage {		
-				
+		
+		@Property(value = "#{"+Constants.SYSTEM_PROPERTIES+"}")
+		private SystemPropertiesBean systemPropertiesBean;
+		
+		public String getWebdavGuideKde1() {
+			return i18n("webdav_guide_kde1", systemPropertiesBean.getOPENUSS_SERVER_URL().replaceAll("^http://", "webdav://"));
+		}
+
 		@Prerender
 		public void prerender() throws Exception {
 			breadcrumbs.init();	
@@ -29,5 +39,13 @@ import org.openuss.web.BasePage;
 			newCrumb.setName(i18n("webdav_guide_kde_header"));
 			newCrumb.setHint(i18n("webdav_guide_kde_header"));
 			breadcrumbs.addCrumb(newCrumb);	
+		}
+
+		public SystemPropertiesBean getSystemPropertiesBean() {
+			return systemPropertiesBean;
+		}
+
+		public void setSystemPropertiesBean(SystemPropertiesBean systemPropertiesBean) {
+			this.systemPropertiesBean = systemPropertiesBean;
 		}		
 	}
--- plexus/plexus-web/src/main/java/org/openuss/web/dav/GuideVistaPage.java
+++ plexus/plexus-web/src/main/java/org/openuss/web/dav/GuideVistaPage.java
@@ -3,11 +3,14 @@ package org.openuss.web.dav;
 
 
 import org.apache.shale.tiger.managed.Bean;
+import org.apache.shale.tiger.managed.Property;
 import org.apache.shale.tiger.managed.Scope;
 import org.apache.shale.tiger.view.Prerender;
 import org.apache.shale.tiger.view.View;
 import org.openuss.framework.jsfcontrols.breadcrumbs.BreadCrumb;
 import org.openuss.web.BasePage;
+import org.openuss.web.Constants;
+import org.openuss.web.system.SystemPropertiesBean;
 
 
 
@@ -21,7 +24,22 @@ import org.openuss.web.BasePage;
 	@Bean(name = "views$secured$webdav$guide_winvista", scope = Scope.REQUEST)
 	@View
 	public class GuideVistaPage extends BasePage {		
-				
+		
+		@Property(value = "#{"+Constants.SYSTEM_PROPERTIES+"}")
+		private SystemPropertiesBean systemPropertiesBean;
+		
+		public SystemPropertiesBean getSystemPropertiesBean() {
+			return systemPropertiesBean;
+		}
+
+		public void setSystemPropertiesBean(SystemPropertiesBean systemPropertiesBean) {
+			this.systemPropertiesBean = systemPropertiesBean;
+		}
+
+		public String getWebdavGuideWinVista5() {
+			return i18n("webdav_guide_win_vista_5", systemPropertiesBean.getOPENUSS_SERVER_URL());
+		}
+		
 		@Prerender
 		public void prerender() throws Exception {
 			breadcrumbs.init();	
