6124ae4f95523679a7e7ab4e9ca45f6d7a1a1ce5
Ingo Düppe <ingo.dueppe@crowdcode.io>
2008-05-13 14:50:55 +0000

    In progress: LDAP merge.
    git-svn-id: https://svn.code.sf.net/p/openuss/code/branches/openuss-plexus-3.1@4711 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ plexus/plexus-web/src/main/java/org/openuss/web/migration/CentralUserData.java
@@ -0,0 +1,89 @@
+package org.openuss.web.migration;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+
+import org.apache.shale.tiger.managed.Bean;
+import org.apache.shale.tiger.managed.Scope;
+import org.apache.shale.tiger.view.View;
+import org.openuss.framework.web.jsf.controller.BaseBean;
+
+/**
+ * Stores information about a centrally authenticated user. 
+ * @author Peter Schuh
+ *
+ */
+@Bean(name = "centralUserData", scope = Scope.SESSION)
+@View
+public class CentralUserData extends BaseBean {
+
+	private String username;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String authenticationDomainName="";
+	private Long authenticationDomainId;	
+	private boolean userAgreementAccepted = false;
+
+	/**
+	 * Validator to check wether the user has accepted the user agreement or not.
+	 * @param context
+	 * @param toValidate
+	 * @param value
+	 */
+	public void validateAcception(FacesContext context, UIComponent toValidate, Object value) {
+		boolean accept = (Boolean) value;
+		if (!accept) {
+			((UIInput)toValidate).setValid(false);
+			addError(toValidate.getClientId(context), i18n("error_useragreement_must_be_accepted"), null);
+		}
+	}
+
+	public boolean isUserAgreementAccepted() {
+		return userAgreementAccepted;
+	}
+
+	public void setUserAgreementAccepted(boolean userAgreementAccepted) {
+		this.userAgreementAccepted = userAgreementAccepted;
+	}	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getAuthenticationDomainId() {
+		return authenticationDomainId;
+	}
+	public void setAuthenticationDomainId(Long authenticationDomainId) {
+		this.authenticationDomainId = authenticationDomainId;
+	}
+
+	public String getAuthenticationDomainName() {
+		return authenticationDomainName;
+	}
+
+	public void setAuthenticationDomainName(String authenticationDomainName) {
+		this.authenticationDomainName = authenticationDomainName;
+	}	
+}
---
+++ plexus/plexus-web/src/main/java/org/openuss/web/migration/MigrationPage.java
@@ -0,0 +1,50 @@
+package org.openuss.web.migration;
+
+import org.apache.shale.tiger.managed.Bean;
+import org.apache.shale.tiger.managed.Property;
+import org.apache.shale.tiger.managed.Scope;
+import org.apache.shale.tiger.view.Prerender;
+import org.apache.shale.tiger.view.View;
+import org.openuss.framework.jsfcontrols.breadcrumbs.BreadCrumb;
+import org.openuss.web.BasePage;
+import org.openuss.web.PageLinks;
+
+/**
+ * @author Peter Schuh
+ *
+ */
+@Bean(name = "views$secured$migration$migration", scope = Scope.REQUEST)
+@View
+public class MigrationPage extends BasePage {
+	
+	@Property(value="#{centralUserData}")
+	CentralUserData centralUserData;
+ 	
+	@Prerender
+ 	public void prerender() throws Exception {
+		 super.prerender();
+		 addBreadCrumbs();
+ 	}
+ 
+	 /**
+	 * Adds an additional BreadCrumb to the course crumbs.
+	 */
+	 private void addBreadCrumbs() {
+		 breadcrumbs.setCrumbs(null);
+	
+		 final BreadCrumb migrationBreadCrumb = new BreadCrumb();
+		 migrationBreadCrumb.setLink(PageLinks.MIGRATION_PAGE);
+		 migrationBreadCrumb.setName(i18n("migration_navigation_header"));
+		 migrationBreadCrumb.setHint(i18n("migration_navigation_header"));
+		 breadcrumbs.addCrumb(migrationBreadCrumb);
+	 }
+
+	public CentralUserData getCentralUserData() {
+		return centralUserData;
+	}
+
+	public void setCentralUserData(CentralUserData centralUserData) {
+		this.centralUserData = centralUserData;
+	}
+
+}
