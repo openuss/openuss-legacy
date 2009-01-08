f2f209c5ba0a067996997fd52327b0132bbba2ce
roekens <roekens>
2009-01-08 14:33:24 +0000

    Add @XmlSchema configuration.
    git-svn-id: https://svn.code.sf.net/p/openuss/code/trunk@4897 60046477-0c10-0410-bf4b-e552a4ff1768

---
+++ openuss-plexus/plexus/plexus-remote/src/main/java/org/openuss/services/model/package-info.java
@@ -0,0 +1,5 @@
+@XmlSchema(elementFormDefault=XmlNsForm.QUALIFIED, namespace="http://www.openuss.org/services/DataTypes")
+package org.openuss.services.model;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
